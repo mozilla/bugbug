05db5c826b4910f148812b8468249b12d2f39651
Marco Castelluccio <mcastelluccio@mozilla.com>
2018-06-18 17:49:51 +0100

    Add more data and use it

--- run.py
+++ run.py
@@ -4,21 +4,34 @@ import bugbug
 
 
 with open('classes.csv', 'r') as f:
-    classes = [row for row in csv.reader(f)][1:]
+    classes = dict([row for row in csv.reader(f)][1:])
 
-bugs = get_bugs([int(bug_id) for bug_id, is_bug in classes])
+with open('classes_more.csv', 'r') as f:
+    classes_more = [row for row in csv.reader(f)][1:]
+
+for bug_id, category in classes_more:
+    if category == 'nobug':
+        is_bug = 'False'
+    else:
+        is_bug = 'True'
+
+    classes[bug_id] = is_bug
+
+bugs = get_bugs([int(bug_id) for bug_id in classes.keys()])
 
 true_positives = 0
 true_negatives = 0
 false_positives = 0
 false_negatives = 0
 
-for bug_id, is_bug in classes:
+for bug_id, is_bug in classes.items():
     assert is_bug == 'True' or is_bug == 'False'
     bug_id = int(bug_id)
     is_bug = True if is_bug == 'True' else False
 
     print(bug_id)
+    if bug_id not in bugs:
+        continue
     is_bug_pred = bugbug.is_bug(bugs[bug_id])
 
     if is_bug_pred and is_bug:
