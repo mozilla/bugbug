0b1077f18093db375e74e54d74d0bd765672c5c9
Marco Castelluccio <mcastelluccio@mozilla.com>
2018-09-21 16:45:24 +0200

    Fix flake8 issues in handwritten_rules_run.py
    Former-commit-id: 2f17f3d8ee3e3677bdc9b42ef86904ea112e8c8a

--- handwritten_rules_run.py
+++ handwritten_rules_run.py
@@ -1,5 +1,11 @@
-from get_bugs import get_bugs, get_labels
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import bugbug
+from get_bugs import get_bugs
+from get_bugs import get_labels
 
 classes = get_labels()
 
