8d3c7c3ba42c273622272f6a0d4f7eb3959c5236
Marco Castelluccio <mcastelluccio@mozilla.com>
2018-09-24 00:11:49 +0100

    Perform under-sampling of the majority class

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ from typing import Dict
 
 import numpy as np
 import xgboost
+from imblearn.under_sampling import RandomUnderSampler
 from sklearn import metrics
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -108,6 +109,9 @@ extraction_pipeline = Pipeline([
 ])
 
 X = extraction_pipeline.fit_transform(bugs)
+
+# Under-sample the 'bug' class, as there are too many compared to 'feature'.
+X, y = RandomUnderSampler().fit_sample(X, y)
 
 # Split dataset in training and test.
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=0)
