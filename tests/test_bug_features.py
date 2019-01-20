c7cd4c537cded8dc8f6fd060b89810a18cbb49b4
ayush1999 <ayush.shridhar1999@gmail.com>
2019-01-20 19:27:45 +0530

    more tests

--- tests/test_bug_features.py
+++ tests/test_bug_features.py
@@ -6,24 +6,89 @@
 import json
 import os
 
+from bugbug.bug_features import component
+from bugbug.bug_features import has_crash_signature
+from bugbug.bug_features import has_github_url
+from bugbug.bug_features import has_regression_range
 from bugbug.bug_features import has_str
+from bugbug.bug_features import has_url
+from bugbug.bug_features import has_w3c_url
+from bugbug.bug_features import is_coverity_issue
+from bugbug.bug_features import keywords
+from bugbug.bug_features import landings
+from bugbug.bug_features import patches
+from bugbug.bug_features import product
+from bugbug.bug_features import severity
+from bugbug.bug_features import title
+from bugbug.bug_features import whiteboard
 
 
-def read(path):
-    check = has_str()
-    if not os.path.exists(path):
-        return ()
+def read(filename, feature_extractor_class, expected_results):
+    path = os.path.join('tests/fixtures/bug_features', filename)
+    feature_extractor = feature_extractor_class()
+    assert os.path.exists(path)
 
-    data = []
     with open(path, 'r') as f:
-        for line in f:
-            data.append(check(json.loads(line)))
-
-    return data
+        results = (feature_extractor(json.loads(line)) for line in f)
+        for result, expected_result in zip(results, expected_results):
+            assert result == expected_result
 
 
 def test_has_str():
+    read('has_str.json', has_str, ['yes', None, 'no'])
 
-    bugs = read('tests/data/test_bugs.json')
-    results = ['yes', None, None, 'yes']
-    assert results == bugs
+
+def test_has_regression_range():
+    read('has_regression_range.json', has_regression_range, ['yes', None])
+
+
+def test_has_crash_signature():
+    read('has_crash_signature.json', has_crash_signature, [False, True])
+
+
+def test_keywords():
+    read('keywords.json', keywords, [['crash', 'intermittent-failure', 'stale-bug'], ['bulk-close-intermittents', 'crash', 'intermittent-failure']])
+
+
+def test_severity():
+    read('severity.json', severity, ['major', 'normal'])
+
+
+def test_is_coverity_issue():
+    read('is_coverity_issue.json', is_coverity_issue, [False, True])
+
+
+def test_has_url():
+    read('has_url.json', has_url, [True, False])
+
+
+def test_has_w3c_url():
+    read('has_w3c_url.json', has_w3c_url, [True, False])
+
+
+def test_has_github_url():
+    read('has_github_url.json', has_github_url, [True, False])
+
+
+def test_whiteboard():
+    read('whiteboard.json', whiteboard, [['memshrink'], []])
+
+
+def test_patches():
+    read('patches.json', patches, [1, 0])
+
+
+def test_landings():
+    read('landings.json', landings, [2, 1])
+
+
+def test_title():
+    read('title.json', title, [['fail'], []])
+
+
+def test_product():
+    read('product.json', product, ['Core', 'Firefox for Android'])
+
+
+def test_component():
+    read('component.json', component, ['Graphics', 'CSS Parsing and Computation'])
