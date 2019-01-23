cc7f3d015e5b2805e0e1c2b67a1d4a68d1613e41
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-01-23 11:51:06 +0100

    Retrieve mercurial commits in parallel using a pool of processes

--- bugbug/repository.py
+++ bugbug/repository.py
@@ -4,18 +4,48 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import argparse
+import concurrent.futures
+import os
 import re
 
 import hglib
+from parsepatch.patch import Patch
 
 from bugbug import db
 
 COMMITS_DB = 'data/commits.json'
 db.register(COMMITS_DB, 'https://www.dropbox.com/s/mz3afgncx0siijc/commits.json.xz?dl=1')
 
+BUG_PATTERN = re.compile('[\t ]*[Bb][Uu][Gg][\t ]*([0-9]+)')
+
 
 def get_commits():
     return db.read(COMMITS_DB)
+
+
+def _init(repo_dir):
+    global HG
+    HG = hglib.open(repo_dir)
+
+
+def _transform(commit):
+    desc = commit[5].decode('utf-8')
+
+    bug_id = None
+    bug_id_match = re.search(BUG_PATTERN, desc)
+    if bug_id_match:
+        bug_id = int(bug_id_match.group(1))
+
+    return {
+        # 'rev': commit[0].decode('utf-8'),
+        # 'node': commit[1].decode('utf-8'),
+        # 'tags': commit[2].decode('utf-8'),
+        # 'branch': commit[3].decode('utf-8'),
+        # 'author': commit[4].decode('utf-8'),
+        'desc': desc,
+        # 'date': str(commit[6]),
+        'bug_id': bug_id,
+    }
 
 
 def download_commits(repo_dir):
@@ -23,30 +53,13 @@ def download_commits(repo_dir):
 
     commits = hg.log()
 
-    bug_pattern = re.compile('[\t ]*[Bb][Uu][Gg][\t ]*([0-9]+)')
+    hg.close()
 
-    def transform(commit):
-        desc = commit[5].decode('utf-8')
+    commits = (tuple(commit) for commit in commits)
 
-        bug_id = None
-        bug_id_match = re.search(bug_pattern, desc)
-        if bug_id_match:
-            bug_id = int(bug_id_match.group(1))
-
-        return {
-            # 'rev': commit[0].decode('utf-8'),
-            # 'node': commit[1].decode('utf-8'),
-            # 'tags': commit[2].decode('utf-8'),
-            # 'branch': commit[3].decode('utf-8'),
-            # 'author': commit[4].decode('utf-8'),
-            'desc': desc,
-            # 'date': str(commit[6]),
-            'bug_id': bug_id,
-        }
-
-    commits = [transform(commit) for commit in reversed(commits)]
-
-    db.write(COMMITS_DB, commits)
+    with concurrent.futures.ProcessPoolExecutor(initializer=_init, initargs=(repo_dir,)) as executor:
+        commits = executor.map(_transform, commits, chunksize=256)
+        db.write(COMMITS_DB, commits)
 
 
 def get_commit_messages_map():
