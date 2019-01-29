42e603facc6fec41ba739cfcc538e8b1b4b9612d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-01-29 01:22:34 +0100

    Add a class extending ColumnTransformer to return a structured numpy array instead of a plain one

--- bugbug/utils.py
+++ bugbug/utils.py
@@ -4,7 +4,20 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import numpy as np
+from sklearn.compose import ColumnTransformer
 
 
 def numpy_to_dict(array):
     return {name: array[name].squeeze(axis=1) for name in array.dtype.names}
+
+
+class StructuredColumnTransformer(ColumnTransformer):
+    def _hstack(self, Xs):
+        result = super()._hstack(Xs)
+
+        transformer_names = (name for name, transformer, column in self.transformers_)
+        types = []
+        for i, (f, transformer_name) in enumerate(zip(Xs, transformer_names)):
+            types.append((transformer_name, result.dtype, (f.shape[1],)))
+
+        return result.view(np.dtype(types))
