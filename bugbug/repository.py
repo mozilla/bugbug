ca09204352c99ffb2e5decdb05ecb15359824ec5
Ayush Shridhar <ayush.shridhar1999@gmail.com>
2019-02-04 04:56:21 +0530

    Use a custom template for hg log (#148)
    This way we can change it when wanted and we can retrieve more info (e.g. author, or bug number, and so on).

--- bugbug/repository.py
+++ bugbug/repository.py
@@ -7,6 +7,7 @@ import argparse
 import concurrent.futures
 import os
 import re
+from datetime import datetime
 
 import hglib
 from parsepatch.patch import Patch
@@ -18,6 +19,20 @@ COMMITS_DB = 'data/commits.json'
 db.register(COMMITS_DB, 'https://www.dropbox.com/s/mz3afgncx0siijc/commits.json.xz?dl=1')
 
 BUG_PATTERN = re.compile('[\t ]*[Bb][Uu][Gg][\t ]*([0-9]+)')
+
+
+def hg_log(repo_dir):
+    hg = hglib.open(repo_dir)
+    args = hglib.util.cmdbuilder(b'log', template='{rev}\\0{node}\\0{tags}\\0{branch}\\0{author}\\0{desc}\\0{date}\\0')
+    x = hg.rawcommand(args)
+    out = x.split(b'\x00')[:-1]
+    revs = []
+    for rev in hglib.util.grouper(7, out):
+        posixtime = float(rev[6].split(b'.', 1)[0])
+        dt = datetime.fromtimestamp(posixtime)
+        revs.append((rev[0], rev[1], rev[2], rev[3], rev[4], rev[5], dt))
+    hg.close()
+    return revs
 
 
 def get_commits():
@@ -84,14 +99,8 @@ def _transform(commit):
 
 
 def download_commits(repo_dir):
-    hg = hglib.open(repo_dir)
-
-    commits = hg.log()
+    commits = hg_log(repo_dir)
     commits_num = len(commits)
-
-    hg.close()
-
-    commits = (tuple(commit) for commit in commits)
 
     with concurrent.futures.ProcessPoolExecutor(initializer=_init, initargs=(repo_dir,)) as executor:
         commits = executor.map(_transform, commits, chunksize=256)
