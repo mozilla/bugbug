7fe067daed1c6ba36daaba5f5c976eb5ad1b4d99
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-02-14 18:00:07 +0100

    Use bugs with 'feature' keyword as 'f' label for defect/feature/task model

--- bugbug/models/defect_feature_task.py
+++ bugbug/models/defect_feature_task.py
@@ -3,7 +3,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from bugbug import labels
 from bugbug.models.bug import BugModel
 
 
@@ -12,13 +11,7 @@ class DefectFeatureTaskModel(BugModel):
         BugModel.__init__(self, lemmatization)
 
     def get_labels(self):
-        classes = self.get_bugbug_labels('bug')
-
-        classes = {bug_id: 'd' for bug_id, label in classes.items() if label == 1}
-
-        for bug_id, label in labels.get_labels('defect_feature_task'):
-            assert label in ['d', 'f', 't']
-            classes[int(bug_id)] = label
+        classes = self.get_bugbug_labels('defect_feature_task')
 
         print('{} defects'.format(sum(1 for label in classes.values() if label == 'd')))
         print('{} features'.format(sum(1 for label in classes.values() if label == 'f')))
