ef83af2cfa3984232e5ffc946aa99e70120036f6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-02-15 11:52:24 +0100

    Add overwrite classes rules for defect/feature/task model

--- bugbug/models/defect_feature_task.py
+++ bugbug/models/defect_feature_task.py
@@ -18,3 +18,12 @@ class DefectFeatureTaskModel(BugModel):
         print('{} tasks'.format(sum(1 for label in classes.values() if label == 't')))
 
         return classes
+
+    def overwrite_classes(self, bugs, classes, probabilities):
+        for i, bug in enumerate(bugs):
+            if any(keyword in bug['keywords'] for keyword in ['regression', 'talos-regression']) or ('cf_has_regression_range' in bug and bug['cf_has_regression_range'] == 'yes'):
+                classes[i] = 'd' if not probabilities else [1., 0., 0.]
+            elif 'feature' in bug['keywords']:
+                classes[i] = 'f' if not probabilities else [0., 1., 0.]
+
+        return classes
