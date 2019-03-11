9ed8dabdc665933cfa86deddceb481d30c3d60b7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-03-11 18:18:40 +0100

    Use a pytest fixture to register a DB, to avoid some code duplication

--- tests/test_db.py
+++ tests/test_db.py
@@ -3,41 +3,59 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import pytest
+
 from bugbug import db
 
 
-def test_write_read(tmp_path):
-    db_path = tmp_path / 'prova.json'
+@pytest.fixture
+def mock_db(tmp_path):
+    def register_db(name):
+        db_path = tmp_path / name
+        db.register(db_path, 'https://alink', 1)
+        return db_path
 
-    db.register(db_path, 'https://alink', 1)
-
-    db.write(db_path, range(1, 8))
-
-    assert list(db.read(db_path)) == [1, 2, 3, 4, 5, 6, 7]
+    return register_db
 
 
-def test_write_read_gzip(tmp_path):
-    db_path = tmp_path / 'prova.json.gz'
-
-    db.register(db_path, 'https://alink', 1)
+def test_write_read(mock_db):
+    db_path = mock_db('prova.json')
 
     db.write(db_path, range(1, 8))
 
     assert list(db.read(db_path)) == [1, 2, 3, 4, 5, 6, 7]
 
 
-def test_write_read_zstd(tmp_path):
-    db_path = tmp_path / 'prova.json.zstd'
-
-    db.register(db_path, 'https://alink', 1)
+def test_write_read_gzip(mock_db):
+    db_path = mock_db('prova.json.gz')
 
     db.write(db_path, range(1, 8))
 
     assert list(db.read(db_path)) == [1, 2, 3, 4, 5, 6, 7]
 
 
-def test_append(tmp_path):
-    db_path = tmp_path / 'prova.json'
+def test_write_read_zstd(mock_db):
+    db_path = mock_db('prova.json.zstd')
+
+    db.write(db_path, range(1, 8))
+
+    assert list(db.read(db_path)) == [1, 2, 3, 4, 5, 6, 7]
+
+
+def test_append(mock_db):
+    db_path = mock_db('prova.json')
+
+    db.write(db_path, range(1, 4))
+
+    assert list(db.read(db_path)) == [1, 2, 3]
+
+    db.append(db_path, range(4, 8))
+
+    assert list(db.read(db_path)) == [1, 2, 3, 4, 5, 6, 7]
+
+
+def test_append_gzip(mock_db):
+    db_path = mock_db('prova.json.gz')
 
     db.register(db_path, 'https://alink', 1)
 
@@ -50,10 +68,8 @@ def test_append(tmp_path):
     assert list(db.read(db_path)) == [1, 2, 3, 4, 5, 6, 7]
 
 
-def test_append_gzip(tmp_path):
-    db_path = tmp_path / 'prova.json.gz'
-
-    db.register(db_path, 'https://alink', 1)
+def test_append_zstd(mock_db):
+    db_path = mock_db('prova.json.zstd')
 
     db.write(db_path, range(1, 4))
 
@@ -64,26 +80,8 @@ def test_append_gzip(tmp_path):
     assert list(db.read(db_path)) == [1, 2, 3, 4, 5, 6, 7]
 
 
-def test_append_zstd(tmp_path):
-    db_path = tmp_path / 'prova.json.zstd'
-
-    db.register(db_path, 'https://alink', 1)
-
-    db.write(db_path, range(1, 4))
-
-    assert list(db.read(db_path)) == [1, 2, 3]
-
-    db.append(db_path, range(4, 8))
-
-    assert list(db.read(db_path)) == [1, 2, 3, 4, 5, 6, 7]
-
-
-def test_delete(tmp_path):
-    db_path = tmp_path / 'prova.json'
-
-    print(db_path)
-
-    db.register(db_path, 'https://alink', 1)
+def test_delete(mock_db):
+    db_path = mock_db('prova.json')
 
     db.write(db_path, range(1, 9))
 
@@ -94,12 +92,8 @@ def test_delete(tmp_path):
     assert list(db.read(db_path)) == [1, 2, 3, 5, 6, 7, 8]
 
 
-def test_delete_gzip(tmp_path):
-    db_path = tmp_path / 'prova.json.gz'
-
-    print(db_path)
-
-    db.register(db_path, 'https://alink', 1)
+def test_delete_gzip(mock_db):
+    db_path = mock_db('prova.json.gz')
 
     db.write(db_path, range(1, 9))
 
@@ -110,10 +104,8 @@ def test_delete_gzip(tmp_path):
     assert list(db.read(db_path)) == [1, 2, 3, 5, 6, 7, 8]
 
 
-def test_delete_zstd(tmp_path):
-    db_path = tmp_path / 'prova.json.zstd'
-
-    print(db_path)
+def test_delete_zstd(mock_db):
+    db_path = mock_db('prova.json.zstd')
 
     db.register(db_path, 'https://alink', 1)
 
