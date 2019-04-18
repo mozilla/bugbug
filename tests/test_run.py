d27d5f8b2c354cbd9909d698e0110cbe7ff9c6ba
Marco <mcastelluccio@mozilla.com>
2019-04-18 14:01:25 +0200

    Add a mock DB for tests to avoid downloading the full DB (#273)
    * Rename test function in test_bug to reflect reality, and add more assertions
    
    * Add a mock bugs DB for tests
    
    * Don't download bugs DB anymore for running tests
    
    * Add a test for run.py basic functionality
    
    * Remove training test task, as the test is now a pytest

---
+++ tests/test_run.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import os
+import subprocess
+import sys
+
+RUNPY = os.path.join(os.path.dirname(os.path.dirname(__file__)), "run.py")
+
+
+def test_run():
+    # Test running the training for the bug model.
+    print([sys.executable, RUNPY, "--train", "--goal", "bug"])
+    subprocess.run([sys.executable, RUNPY, "--train", "--goal", "bug"], check=True)
+
+    # Test loading the trained model.
+    subprocess.run([sys.executable, RUNPY, "--goal", "bug"], check=True)
