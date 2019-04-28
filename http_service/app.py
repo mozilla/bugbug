5cf5b20adbf055c7809dae4aa338d19e464ce65b
Boris Feld <lothiraldan@gmail.com>
2019-04-28 18:34:10 +0200

    Start working on the bach API

--- http_service/app.py
+++ http_service/app.py
@@ -3,6 +3,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import json
 import logging
 import os
 
@@ -32,8 +33,8 @@ def get_model(model):
     return getattr(current_app, attribute, model)
 
 
-def classify_bug(model_name, bug_id):
-    bugs = bugzilla._download([bug_id])
+def classify_bug(model_name, bug_ids):
+    bugs = bugzilla._download(bug_ids)
     model = get_model(model_name)
     probs = model.classify(list(bugs.values()), True)
     indexes = probs.argmax(axis=-1)
@@ -59,6 +60,28 @@ def model_prediction(model_name, bug_id):
     else:
         LOGGER.info("Request with API TOKEN %r", auth)
 
-    data = classify_bug(model_name, bug_id)
+    data = classify_bug(model_name, [bug_id])
+
+    return jsonify(**data)
+
+
+@application.route("/<model_name>/predict/batch", methods=["POST"])
+def batch_prediction(model_name):
+    headers = request.headers
+
+    auth = headers.get(API_TOKEN)
+
+    if not auth:
+        return jsonify({"message": "Error, missing X-API-KEY"}), 401
+    else:
+        LOGGER.info("Request with API TOKEN %r", auth)
+
+    # TODO Check is JSON is valid and validate against a request schema
+    batch_body = json.loads(request.data)
+
+    print("BATCH BODY", request.data, batch_body)
+    bugs = batch_body["bugs"]
+
+    data = classify_bug(model_name, bugs)
 
     return jsonify(**data)
