369b44ea02cd72dbaacebb0532dbd93dcc2635ff
Boris Feld <lothiraldan@gmail.com>
2019-05-02 17:00:32 +0200

    Update the index URLs in bugbug (#328)
    * Update the index URLs in bugbug
    
    * Split the http service Docker image in two
    
    This way we can both:
    - Build the first half (code + dependencies) in the usual CI.
    - Build the second half at the end of the data pipeline with updated models.
    
    Taskboot build-compose doesn't support building all services except a
    specific one and it might be cumbersome to add this feature so move the second
    half of the Docker image to a separate docker-compose file.

--- http_service/download_models.py
+++ http_service/download_models.py
@@ -14,7 +14,7 @@ import requests
 logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger()
 
-BASE_URL = "https://index.taskcluster.net/v1/task/project.releng.services.project.testing.bugbug_train.latest/artifacts/public"
+BASE_URL = "https://index.taskcluster.net/v1/task/project.relman.bugbug.train_{}.latest/artifacts/public"
 
 MODELS_NAMES = ("defectenhancementtask", "component", "regression")
 MODELS_DIR = os.path.join(os.path.dirname(__file__), "models")
@@ -26,7 +26,8 @@ def retrieve_model(name):
     file_name = f"{name}model"
     file_path = os.path.join(MODELS_DIR, file_name)
 
-    model_url = f"{BASE_URL}/{file_name}.xz"
+    base_model_url = BASE_URL.format(name)
+    model_url = f"{base_model_url}/{file_name}.xz"
     LOGGER.info(f"Checking ETAG of {model_url}")
     r = requests.head(model_url, allow_redirects=True)
     r.raise_for_status()
