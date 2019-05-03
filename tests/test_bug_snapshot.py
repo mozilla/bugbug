3af587a36ccc9769fc2b60fd7799257f046646b7
Boris Feld <lothiraldan@gmail.com>
2019-05-03 12:29:19 +0200

    Use the standard interface to read the mock DB

--- tests/test_bug_snapshot.py
+++ tests/test_bug_snapshot.py
@@ -4,20 +4,13 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 
-import json
-
+from bugbug import bugzilla
 from bugbug.bug_snapshot import rollback
 
 
-def test_bug_snapshot(get_fixture_path):
-    mock_bugs_db_path = get_fixture_path("bugs.json")
+def test_bug_snapshot():
+    for i, bug in enumerate(bugzilla.get_bugs()):
+        print(bug["id"])
+        print(i)
 
-    with open(mock_bugs_db_path, "r") as mock_bug_db:
-
-        for i, line in enumerate(mock_bug_db):
-            bug = json.loads(line)
-
-            print(bug["id"])
-            print(i)
-
-            rollback(bug, None, False)
+        rollback(bug, None, False)
