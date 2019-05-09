b1c63758f8e00954dc074b5820dd3ede7ee61ec6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-05-10 01:26:43 +0200

    Move the experience calculation code in a separate function

--- bugbug/repository.py
+++ bugbug/repository.py
@@ -302,64 +302,8 @@ def get_directories(files):
     return list(directories)
 
 
-def download_commits(repo_dir, date_from):
-    hg = hglib.open(repo_dir)
-
-    revs = get_revs(hg)
-
-    commits_num = len(revs)
-
-    assert (
-        commits_num > 0
-    ), "There should definitely be more than 0 commits, something is wrong"
-
-    hg.close()
-
-    processes = multiprocessing.cpu_count()
-
-    print(f"Mining {commits_num} commits using {processes} processes...")
-
-    CHUNK_SIZE = 256
-    revs_groups = [revs[i : (i + CHUNK_SIZE)] for i in range(0, len(revs), CHUNK_SIZE)]
-
-    with concurrent.futures.ProcessPoolExecutor(
-        initializer=_init, initargs=(repo_dir,)
-    ) as executor:
-        commits = executor.map(_hg_log, revs_groups, chunksize=20)
-        commits = tqdm(commits, total=len(revs_groups))
-        commits = list(itertools.chain.from_iterable(commits))
-
-    # Don't analyze backouts.
-    backouts = set(
-        commit.backedoutby for commit in commits if commit.backedoutby != b""
-    )
-    commits = [commit for commit in commits if commit.node not in backouts]
-
-    # Don't analyze commits that are not linked to a bug.
-    commits = [commit for commit in commits if commit.bug != b""]
-
-    # Skip commits which are in .hg-annotate-ignore-revs (mostly consisting of very
-    # large and not meaningful formatting changes).
-    with open(os.path.join(repo_dir, ".hg-annotate-ignore-revs"), "r") as f:
-        ignore_revs = set(l[:40].encode("utf-8") for l in f)
-
-    commits = [commit for commit in commits if commit.node not in ignore_revs]
-
-    print("Downloading file->component mapping...")
-
-    global path_to_component
-    r = requests.get(
-        "https://index.taskcluster.net/v1/task/gecko.v2.mozilla-central.latest.source.source-bugzilla-info/artifacts/public/components.json"
-    )
-    r.raise_for_status()
-    path_to_component = r.json()
-    path_to_component = {
-        path: "::".join(component) for path, component in path_to_component.items()
-    }
-
-    commits_num = len(commits)
-
-    print(f"Analyzing {commits_num} patches...")
+def calculate_experiences(commits):
+    print(f"Analyzing experiences from {len(commits)} commits...")
 
     global experiences_by_commit
 
@@ -461,10 +405,70 @@ def download_commits(repo_dir, date_from):
 
         # TODO: We can delete anything older than 90 days at this point.
 
+
+def download_commits(repo_dir, date_from):
+    hg = hglib.open(repo_dir)
+
+    revs = get_revs(hg)
+
+    commits_num = len(revs)
+
+    assert (
+        commits_num > 0
+    ), "There should definitely be more than 0 commits, something is wrong"
+
+    hg.close()
+
+    processes = multiprocessing.cpu_count()
+
+    print(f"Mining {commits_num} commits using {processes} processes...")
+
+    CHUNK_SIZE = 256
+    revs_groups = [revs[i : (i + CHUNK_SIZE)] for i in range(0, len(revs), CHUNK_SIZE)]
+
+    with concurrent.futures.ProcessPoolExecutor(
+        initializer=_init, initargs=(repo_dir,)
+    ) as executor:
+        commits = executor.map(_hg_log, revs_groups, chunksize=20)
+        commits = tqdm(commits, total=len(revs_groups))
+        commits = list(itertools.chain.from_iterable(commits))
+
+    # Don't analyze backouts.
+    backouts = set(
+        commit.backedoutby for commit in commits if commit.backedoutby != b""
+    )
+    commits = [commit for commit in commits if commit.node not in backouts]
+
+    # Don't analyze commits that are not linked to a bug.
+    commits = [commit for commit in commits if commit.bug != b""]
+
+    # Skip commits which are in .hg-annotate-ignore-revs (mostly consisting of very
+    # large and not meaningful formatting changes).
+    with open(os.path.join(repo_dir, ".hg-annotate-ignore-revs"), "r") as f:
+        ignore_revs = set(l[:40].encode("utf-8") for l in f)
+
+    commits = [commit for commit in commits if commit.node not in ignore_revs]
+
+    print("Downloading file->component mapping...")
+
+    global path_to_component
+    r = requests.get(
+        "https://index.taskcluster.net/v1/task/gecko.v2.mozilla-central.latest.source.source-bugzilla-info/artifacts/public/components.json"
+    )
+    r.raise_for_status()
+    path_to_component = r.json()
+    path_to_component = {
+        path: "::".join(component) for path, component in path_to_component.items()
+    }
+
+    calculate_experiences(commits)
+
     # Exclude commits outside the range we care about.
     commits = [commit for commit in commits if commit.pushdate > date_from]
 
-    print(f"Mining commits using {multiprocessing.cpu_count()} processes...")
+    commits_num = len(commits)
+
+    print(f"Mining {commits_num} commits using {processes} processes...")
 
     with concurrent.futures.ProcessPoolExecutor(
         initializer=_init, initargs=(repo_dir,)
