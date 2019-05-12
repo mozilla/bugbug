fb6b62afec2b1d1793f71bbe80a5bd1278395149
Boris Feld <lothiraldan@gmail.com>
2019-05-12 22:08:15 +0200

    Add a better error message for load_model

--- bugbug/models/__init__.py
+++ bugbug/models/__init__.py
@@ -22,6 +22,10 @@ def load_model(model_name, model_dir=None):
     if model_dir is None:
         model_dir = "."
 
+    if model_name not in MODELS:
+        err_msg = f"Invalid name {model_name}, not in {list(MODELS.keys())}"
+        raise ValueError(err_msg)
+
     model_file_path = os.path.join(model_dir, f"{model_name}model")
     LOGGER.info(f"Lookup model in {model_file_path}")
     model = MODELS[model_name].load(model_file_path)
