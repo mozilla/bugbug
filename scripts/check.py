0a5e37439d629beea8f9ad0feaaac50998ae7ec7
Boris Feld <lothiraldan@gmail.com>
2019-05-16 15:34:38 +0200

    Add a central place where the models are defined (#398)
    * Add a central place where the models are defined
    
    Also add some helpers to load a model.
    
    * Add missing tensorflow dependency in extra-nn-requirements.txt

--- scripts/check.py
+++ scripts/check.py
@@ -4,7 +4,7 @@ import argparse
 import sys
 from logging import INFO, basicConfig, getLogger
 
-from bugbug.models.component import ComponentModel
+from bugbug.models import load_model
 
 basicConfig(level=INFO)
 logger = getLogger(__name__)
@@ -12,30 +12,14 @@ logger = getLogger(__name__)
 
 class ModelChecker:
     def go(self, model_name):
-        # TODO: Stop hard-coding them
-        valid_models = ["component"]
-
-        if model_name not in valid_models:
-            exception = f"Invalid model {model_name!r} name, use one of {valid_models!r} instead"
-            raise ValueError(exception)
-
-        # TODO: What is the standard file path of the models?
-        model_file_name = f"{model_name}model"
-
-        if model_name == "component":
-            model_class = ComponentModel
-        else:
-            # We shouldn't be here
-            raise Exception("valid_models is likely not up-to-date anymore")
-
         # Load the model
-        model = model_class.load(model_file_name)
+        model = load_model(model_name)
 
         # Then call the check method of the model
         success = model.check()
 
         if not success:
-            msg = f"Check of model {model_class!r} failed, check the output for reasons why"
+            msg = f"Check of model {model.__class__!r} failed, check the output for reasons why"
             logger.warning(msg)
             sys.exit(1)
 
