999cd3ef44356f66e45e78f5d7bf2a79690e9de5
Sladyn <gunnerforlife00@gmail.com>
2019-05-25 13:53:05 +0530

    test_stepstoreproduce.py: Add basic tests
    Added basic tests with respec to the
    stepstoreproduce model.
    
    Reference: https://github.com/mozilla/bugbug/issues/428

---
+++ tests/test_stepstoreproduce.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+from bugbug.models.stepstoreproduce import StepsToReproduceModel
+
+
+def test_get_labels():
+    model = StepsToReproduceModel()
+    classes,_ = model.get_labels()
+    assert not classes[1101825]
+    assert classes[1042096]
