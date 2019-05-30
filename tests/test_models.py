51e7297a89013987f0ea564198ad52a256fd17d6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-05-30 13:25:26 +0200

    Test type of component and backout models

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,6 +3,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from bugbug import model
 from bugbug.models import MODELS, get_model_class
 
 
@@ -14,3 +15,13 @@ def test_import_all_models():
     for model_name in MODELS.keys():
         print("Try loading model", model_name)
         get_model_class(model_name)
+
+
+def test_component_is_bugmodel():
+    model_class = get_model_class("component")
+    assert model.BugModel in model_class.__bases__
+
+
+def test_backout_is_commitmodel():
+    model_class = get_model_class("backout")
+    assert model.CommitModel in model_class.__bases__
