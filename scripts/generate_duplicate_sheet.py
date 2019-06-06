89c420515e48340b531420f8ea0e422811802208
ayush-1506 <ayush.shridhar1506@gmail.com>
2019-06-06 11:16:56 +0100

    changes

--- scripts/generate_duplicate_sheet.py
+++ scripts/generate_duplicate_sheet.py
@@ -2,6 +2,7 @@
 
 import csv
 import itertools
+import json
 from datetime import datetime, timedelta
 
 from bugbug import bugzilla
@@ -10,16 +11,23 @@ from bugbug.models.duplicate import DuplicateModel
 m = DuplicateModel.load("duplicatemodel")
 
 REPORTERS_TO_IGNORE = {"intermittent-bug-filer@mozilla.bugs", "wptsync@mozilla.bugs"}
-test_bugs = bugzilla.download_bugs_between(
-    datetime.now() - timedelta(days=21), datetime.now(), store=False
-)
 
-test_bugs = [bug for bug in test_bugs if not bug["creator"] in REPORTERS_TO_IGNORE]
 
-bug_tuples = itertools.combinations(test_bugs, 2)
+try:
+    with open("duplicate_test_bugs.json", "r") as f:
+        test_bugs = json.load(f)
+except FileNotFoundError:
+    test_bugs = bugzilla.download_bugs_between(
+        datetime.now() - timedelta(days=1), datetime.now(), store=False
+    )
+    test_bugs = [bug for bug in test_bugs if not bug["creator"] in REPORTERS_TO_IGNORE]
+    with open("duplicate_test_bugs.json", "w") as f:
+        json.dump(test_bugs, f)
+
+bug_tuples = list(itertools.combinations(test_bugs, 2))
 
 # Warning: Classifying all the test bugs takes a while
-probs = [m.classify(bug_tuples)]
+probs = m.classify(bug_tuples, probabilities=True)
 
 with open("duplicate_predictions.csv", "w") as csvfile:
     spamwriter = csv.writer(csvfile)
