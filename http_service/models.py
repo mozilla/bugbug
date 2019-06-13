7a7ae6475b92e8318a8384fe1a9334153ed5f285
Boris Feld <lothiraldan@gmail.com>
2019-06-13 18:00:01 +0200

    Enable the stepstoreproduce model in the HTTP service (#587)

--- http_service/models.py
+++ http_service/models.py
@@ -19,7 +19,7 @@ from bugbug.models import load_model as bugbug_load_model
 logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger()
 
-MODELS_NAMES = ["defectenhancementtask", "component", "regression"]
+MODELS_NAMES = ["defectenhancementtask", "component", "regression", "stepstoreproduce"]
 MODELS_DIR = os.path.join(os.path.dirname(__file__), "models")
 BASE_URL = "https://index.taskcluster.net/v1/task/project.relman.bugbug.train_{}.latest/artifacts/public"
 DEFAULT_EXPIRATION_TTL = 7 * 24 * 3600  # A week
