ad2546e1248913e98e3ec3b413a38e0c967e51c5
Ayush Shridhar <ayush.shridhar1506@gmail.com>
2019-07-08 03:24:13 -0700

    Add word2vec with WMD distance similarity (#666)

--- bugbug/similarity.py
+++ bugbug/similarity.py
@@ -3,9 +3,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import bisect
+import random
 import re
 from collections import defaultdict
 
+import numpy as np
+from pyemd import emd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neighbors import NearestNeighbors
 
@@ -17,7 +21,9 @@ OPT_MSG_MISSING = (
 
 try:
     import nltk
+    import gensim
     from gensim import models, similarities
+    from gensim.models import Word2Vec
     from gensim.corpora import Dictionary
     from nltk.corpus import stopwords
     from nltk.stem.porter import PorterStemmer
@@ -175,3 +181,120 @@ class NeighborsSimilarity(BaseSimilarity):
         return [
             self.bug_ids[ind] for ind in indices[0] if self.bug_ids[ind] != query["id"]
         ]
+
+
+class Word2VecWmdSimilarity(BaseSimilarity):
+    def __init__(self):
+        self.corpus = []
+        self.bug_ids = []
+        for bug in bugzilla.get_bugs():
+            self.corpus.append(text_preprocess(get_text(bug)))
+            self.bug_ids.append(bug["id"])
+
+        indexes = list(range(len(self.corpus)))
+        random.shuffle(indexes)
+        self.corpus = [self.corpus[idx] for idx in indexes]
+        self.bug_ids = [self.bug_ids[idx] for idx in indexes]
+
+        self.w2vmodel = Word2Vec(self.corpus, size=100, min_count=5)
+        self.w2vmodel.init_sims(replace=True)
+
+    # word2vec.wmdistance calculates only the euclidean distance. To get the cosine distance,
+    # we're using the function with a few subtle changes. We compute the cosine distances
+    # in the get_similar_bugs method and use this inside the wmdistance method.
+    def wmdistance(self, document1, document2, all_distances, distance_metric="cosine"):
+        model = self.w2vmodel
+        if len(document1) == 0 or len(document2) == 0:
+            print(
+                "At least one of the documents had no words that were in the vocabulary. Aborting (returning inf)."
+            )
+            return float("inf")
+
+        dictionary = gensim.corpora.Dictionary(documents=[document1, document2])
+        vocab_len = len(dictionary)
+
+        # Sets for faster look-up.
+        docset1 = set(document1)
+        docset2 = set(document2)
+
+        distance_matrix = np.zeros((vocab_len, vocab_len), dtype=np.double)
+
+        for i, t1 in dictionary.items():
+            for j, t2 in dictionary.items():
+                if t1 not in docset1 or t2 not in docset2:
+                    continue
+
+                if distance_metric == "euclidean":
+                    distance_matrix[i, j] = np.sqrt(
+                        np.sum((model.wv[t1] - model.wv[t2]) ** 2)
+                    )
+                elif distance_metric == "cosine":
+                    distance_matrix[i, j] = all_distances[model.wv.vocab[t2].index, i]
+
+        if np.sum(distance_matrix) == 0.0:
+            print("The distance matrix is all zeros. Aborting (returning inf).")
+            return float("inf")
+
+        def nbow(document):
+            d = np.zeros(vocab_len, dtype=np.double)
+            nbow = dictionary.doc2bow(document)
+            doc_len = len(document)
+            for idx, freq in nbow:
+                d[idx] = freq / float(doc_len)
+            return d
+
+        d1 = nbow(document1)
+        d2 = nbow(document2)
+
+        return emd(d1, d2, distance_matrix)
+
+    def get_similar_bugs(self, query):
+
+        words = text_preprocess(get_text(query))
+        words = [word for word in words if word in self.w2vmodel.wv.vocab]
+
+        all_distances = np.array(
+            1.0
+            - np.dot(
+                self.w2vmodel.wv.vectors_norm,
+                self.w2vmodel.wv.vectors_norm[
+                    [self.w2vmodel.wv.vocab[word].index for word in words]
+                ].transpose(),
+            ),
+            dtype=np.double,
+        )
+        distances = []
+        for i in range(len(self.corpus)):
+            indexes = [self.w2vmodel.wv.vocab[word].index for word in self.corpus[i]]
+            if len(indexes) != 0:
+                word_dists = all_distances[indexes]
+                rwmd = max(
+                    np.sum(np.min(word_dists, axis=0)),
+                    np.sum(np.min(word_dists, axis=1)),
+                )
+
+                distances.append((self.bug_ids[i], rwmd))
+
+        distances.sort(key=lambda v: v[1])
+
+        confirmed_distances_ids = []
+        confirmed_distances = []
+
+        for i, (doc_id, rwmd_distance) in enumerate(distances):
+
+            if (
+                len(confirmed_distances) >= 10
+                and rwmd_distance > confirmed_distances[10 - 1]
+            ):
+                break
+
+            doc_words_clean = self.corpus[self.bug_ids.index(doc_id)]
+            wmd = self.wmdistance(words, doc_words_clean, all_distances)
+
+            j = bisect.bisect(confirmed_distances, wmd)
+            confirmed_distances.insert(j, wmd)
+            confirmed_distances_ids.insert(j, doc_id)
+
+        similarities = zip(confirmed_distances_ids, confirmed_distances)
+
+        return [similar[0] for similar in sorted(similarities, key=lambda v: v[1])[:10]]
