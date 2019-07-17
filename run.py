c1dd687a461a97238346d72315f5a2e7f9dfaf6b
cklyyung <cklyyung@users.noreply.github.com>
2019-07-17 07:33:43 -0400

    Add option not to cleanup URLs when training the Duplicate model (#722)

--- run.py
+++ run.py
@@ -29,6 +29,13 @@ def parse_args(args):
         type=int,
         help="The size of the training set for the duplicate model",
     )
+    parser.add_argument(
+        "--disable-url-cleanup",
+        help="Don't cleanup urls when training the duplicate model",
+        dest="cleanup_urls",
+        default=True,
+        action="store_false",
+    )
     parser.add_argument("--train", help="Perform training", action="store_true")
     parser.add_argument(
         "--goal", help="Goal of the classifier", choices=MODELS.keys(), default="defect"
@@ -84,7 +91,9 @@ def main(args):
         if args.goal in historical_supported_tasks:
             model = model_class(args.lemmatization, args.historical)
         elif args.goal == "duplicate":
-            model = model_class(args.training_set_size, args.lemmatization)
+            model = model_class(
+                args.training_set_size, args.lemmatization, args.cleanup_urls
+            )
         else:
             model = model_class(args.lemmatization)
         model.train()
