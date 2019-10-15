mozilla/bugbug

commit c9f8613d794d0a0ad8d4d32023bcc1da0391d286

During training, don't build a list with all trainable elements, but use a generator
It could use too much memory if there are too many elements.

bugbug/model.py
@@ -25,7 +25,7 @@ from tabulate import tabulate
 
 from bugbug import bugzilla, repository
 from bugbug.nlp import SpacyVectorizer
-from bugbug.utils import split_tuple_iterator, to_array
+from bugbug.utils import split_tuple_generator, to_array
 
 
 def classification_report_imbalanced_values(
@@ -319,13 +319,13 @@ class Model:
         self.class_names = sort_class_names(self.class_names)
 
         # Get items and labels, filtering out those for which we have no labels.
-        X_iter, y_iter = split_tuple_iterator(self.items_gen(classes))
+        X_gen, y = split_tuple_generator(lambda: self.items_gen(classes))
 
         # Extract features from the items.
-        X = self.extraction_pipeline.fit_transform([item for item in X_iter])
+        X = self.extraction_pipeline.fit_transform(X_gen)
 
         # Calculate labels.
-        y = np.array(y_iter)
+        y = np.array(y)
 
         if limit:
             X = X[:limit]

bugbug/bug_features.py
@@ -643,7 +643,7 @@ class BugExtractor(BaseEstimator, TransformerMixin):
                     "comments": " ".join([c["text"] for c in bug["comments"]]),
                 }
 
-        for bug in bugs:
+        for bug in bugs():
             if isinstance(bug, dict):
                 results.append(apply_transform(bug))
             elif isinstance(bug, tuple):

bugbug/commit_features.py
@@ -250,14 +250,14 @@ class CommitExtractor(BaseEstimator, TransformerMixin):
     def fit(self, x, y=None):
         for feature in self.feature_extractors:
             if hasattr(feature, "fit"):
-                feature.fit(x)
+                feature.fit(x())
 
         return self
 
     def transform(self, commits):
         results = []
 
-        for commit in commits:
+        for commit in commits():
             data = {}
 
             for feature_extractor in self.feature_extractors:

bugbug/utils.py
@@ -3,7 +3,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import collections
 import json
 import os
 import time
@@ -22,15 +21,21 @@ from sklearn.preprocessing import OrdinalEncoder
 TASKCLUSTER_DEFAULT_URL = "https://taskcluster.net"
 
 
-def split_tuple_iterator(iterable):
-    q = collections.deque()
+def split_tuple_generator(generator):
+    second_filled = False
+    q = deque()
 
     def first_iter():
-        for first, second in iterable:
-            yield first
-            q.append(second)
+        nonlocal second_filled
 
-    return first_iter(), q
+        for first, second in generator():
+            yield first
+            if not second_filled:
+                q.append(second)
+
+        second_filled = True
+
+    return first_iter, q
 
 
 def numpy_to_dict(array):

tests/test_utils.py
@@ -6,6 +6,18 @@
 from bugbug import utils
 
 
+def test_split_tuple_iterator():
+    X, y = utils.split_tuple_generator(
+        lambda: [("val1", "label1"), ("val2", "label2"), ("val3", "label3")]
+    )
+
+    assert list(X()) == ["val1", "val2", "val3"]
+    assert list(y) == ["label1", "label2", "label3"]
+    assert list(y) == ["label1", "label2", "label3"]
+    assert list(X()) == ["val1", "val2", "val3"]
+    assert list(y) == ["label1", "label2", "label3"]
+
+
 def test_exp_queue():
     q = utils.ExpQueue(0, 4, 0)
     q[0] = 1

commit e230508a3076b8b24b6440f3f22a52c69890d6e3

Exctractor's transform argument must be a function returning an iterable
Regression from c9f8613d794d0a0ad8d4d32023bcc1da0391d286

bugbug/model.py
@@ -545,7 +545,7 @@ class Model:
 
         assert isinstance(items[0], dict) or isinstance(items[0], tuple)
 
-        X = self.extraction_pipeline.transform(items)
+        X = self.extraction_pipeline.transform(lambda: items)
         if probabilities:
             classes = self.clf.predict_proba(X)
         else: