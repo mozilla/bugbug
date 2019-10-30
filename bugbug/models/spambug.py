ae6dadffa3f6c41d1e2fc017eabd3ff30ba57591
ayush-1506 <ayush.shridhar1506@gmail.com>
2019-10-30 09:55:37 +0530

    remove commit map from spambug model

--- bugbug/models/spambug.py
+++ bugbug/models/spambug.py
@@ -3,15 +3,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from collections import defaultdict
-
 import xgboost
 from imblearn.under_sampling import RandomUnderSampler
 from sklearn.compose import ColumnTransformer
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.pipeline import Pipeline
 
-from bugbug import bug_features, bugzilla, feature_cleanup, repository
+from bugbug import bug_features, bugzilla, feature_cleanup
 from bugbug.model import BugModel
 
 
@@ -90,32 +88,13 @@ class SpamBugModel(BugModel):
         return classes, [0, 1]
 
     def items_gen(self, classes):
-        if not self.commit_data:
-            commit_map = None
-        else:
-            commit_map = defaultdict(list)
-
-            for commit in repository.get_commits():
-                bug_id = commit["bug_id"]
-                if not bug_id:
-                    continue
-
-                commit_map[bug_id].append(commit)
-
-            assert len(commit_map) > 0
-
-        for bug in bugzilla.get_bugs(include_invalid=True):
-            bug_id = bug["id"]
-            if bug_id not in classes:
-                continue
-
-            if self.commit_data:
-                if bug_id in commit_map:
-                    bug["commits"] = commit_map[bug_id]
-                else:
-                    bug["commits"] = []
-
-            yield bug, classes[bug_id]
+        # Overwriting this method to add include_invalid=True to get_bugs to
+        # include spam bugs.
+        return (
+            (bug, classes[bug["id"]])
+            for bug in bugzilla.get_bugs(include_invalid=True)
+            if bug["id"] in classes
+        )
 
     def get_feature_names(self):
         return self.extraction_pipeline.named_steps["union"].get_feature_names()
