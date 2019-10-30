fed216d1b2748fcd3ec45e5dc559d84dc79aca7f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-10-30 14:14:38 +0100

    Make commit feature names more readable
    Fixes #1044

--- bugbug/commit_features.py
+++ bugbug/commit_features.py
@@ -10,7 +10,6 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 EXPERIENCE_TIMESPAN = 90
 EXPERIENCE_TIMESPAN_TEXT = f"{EXPERIENCE_TIMESPAN}_days"
-EXPERIENCE_TIMESPAN_TEXT_USER = f"{EXPERIENCE_TIMESPAN} days"
 
 
 class files_modified_num(object):
@@ -28,26 +27,22 @@ class test_files_modified_num(object):
 
 
 class file_size(object):
-    name = "File size"
-
     def __call__(self, commit, **kwargs):
         return {
-            "sum": commit["total_file_size"],
-            "avg": commit["average_file_size"],
-            "max": commit["maximum_file_size"],
-            "min": commit["minimum_file_size"],
+            "Total file size": commit["total_file_size"],
+            "Average file size": commit["average_file_size"],
+            "Maximum file size": commit["maximum_file_size"],
+            "Minimum file size": commit["minimum_file_size"],
         }
 
 
 class test_file_size(object):
-    name = "Test file size"
-
     def __call__(self, commit, **kwargs):
         return {
-            "sum": commit["total_test_file_size"],
-            "avg": commit["average_test_file_size"],
-            "max": commit["maximum_test_file_size"],
-            "min": commit["minimum_test_file_size"],
+            "Total test file size": commit["total_test_file_size"],
+            "Average test file size": commit["average_test_file_size"],
+            "Maximum test file size": commit["maximum_test_file_size"],
+            "Minimum test file size": commit["minimum_test_file_size"],
         }
 
 
@@ -96,31 +91,31 @@ def get_exps(exp_type, commit):
         "avg backout": commit[f"touched_prev_total_{exp_type}_backout_sum"] / items_num
         if items_num > 0
         else 0,
-        f"sum {EXPERIENCE_TIMESPAN_TEXT_USER}": commit[
+        f"sum {EXPERIENCE_TIMESPAN_TEXT}": commit[
             f"touched_prev_{EXPERIENCE_TIMESPAN_TEXT}_{exp_type}_sum"
         ],
-        f"max {EXPERIENCE_TIMESPAN_TEXT_USER}": commit[
+        f"max {EXPERIENCE_TIMESPAN_TEXT}": commit[
             f"touched_prev_{EXPERIENCE_TIMESPAN_TEXT}_{exp_type}_max"
         ],
-        f"min {EXPERIENCE_TIMESPAN_TEXT_USER}": commit[
+        f"min {EXPERIENCE_TIMESPAN_TEXT}": commit[
             f"touched_prev_{EXPERIENCE_TIMESPAN_TEXT}_{exp_type}_min"
         ],
-        f"avg {EXPERIENCE_TIMESPAN_TEXT_USER}": commit[
+        f"avg {EXPERIENCE_TIMESPAN_TEXT}": commit[
             f"touched_prev_{EXPERIENCE_TIMESPAN_TEXT}_{exp_type}_sum"
         ]
         / items_num
         if items_num > 0
         else 0,
-        f"sum {EXPERIENCE_TIMESPAN_TEXT_USER} backout": commit[
+        f"sum {EXPERIENCE_TIMESPAN_TEXT} backout": commit[
             f"touched_prev_{EXPERIENCE_TIMESPAN_TEXT}_{exp_type}_backout_sum"
         ],
-        f"max {EXPERIENCE_TIMESPAN_TEXT_USER} backout": commit[
+        f"max {EXPERIENCE_TIMESPAN_TEXT} backout": commit[
             f"touched_prev_{EXPERIENCE_TIMESPAN_TEXT}_{exp_type}_backout_max"
         ],
-        f"min {EXPERIENCE_TIMESPAN_TEXT_USER} backout": commit[
+        f"min {EXPERIENCE_TIMESPAN_TEXT} backout": commit[
             f"touched_prev_{EXPERIENCE_TIMESPAN_TEXT}_{exp_type}_backout_min"
         ],
-        f"avg {EXPERIENCE_TIMESPAN_TEXT_USER} backout": commit[
+        f"avg {EXPERIENCE_TIMESPAN_TEXT} backout": commit[
             f"touched_prev_{EXPERIENCE_TIMESPAN_TEXT}_{exp_type}_backout_sum"
         ]
         / items_num
@@ -134,23 +129,53 @@ class author_experience(object):
 
     def __call__(self, commit, **kwargs):
         return {
-            "total": commit["touched_prev_total_author_sum"],
-            EXPERIENCE_TIMESPAN_TEXT_USER: commit[
+            "Author experience": commit["touched_prev_total_author_sum"],
+            "Recent author experience": commit[
                 f"touched_prev_{EXPERIENCE_TIMESPAN_TEXT}_author_sum"
             ],
-            "total backouts": commit["touched_prev_total_author_backout_sum"],
-            f"{EXPERIENCE_TIMESPAN_TEXT_USER} backouts": commit[
+            "Author backouts": commit["touched_prev_total_author_backout_sum"],
+            "Recent author backouts": commit[
                 f"touched_prev_{EXPERIENCE_TIMESPAN_TEXT}_author_backout_sum"
             ],
-            "seniority": commit["seniority_author"] / 86400,
+            "Author seniority": commit["seniority_author"] / 86400,
         }
 
 
 class reviewer_experience(object):
-    name = "Reviewer experience"
-
     def __call__(self, commit, **kwargs):
-        return get_exps("reviewer", commit)
+        exps = get_exps("reviewer", commit)
+        return {
+            "Total reviewer experience": exps["sum"],
+            "Maximum reviewer experience": exps["max"],
+            "Minimum reviewer experience": exps["min"],
+            "Average reviewer experience": exps["avg"],
+            "Total reviewer backouts": exps["sum backout"],
+            "Maximum reviewer backouts": exps["max backout"],
+            "Minimum reviewer backouts": exps["min backout"],
+            "Average reviewer backouts": exps["avg backout"],
+            "Total recent reviewer experience": exps[f"sum {EXPERIENCE_TIMESPAN_TEXT}"],
+            "Maximum recent reviewer experience": exps[
+                f"max {EXPERIENCE_TIMESPAN_TEXT}"
+            ],
+            "Minimum recent reviewer experience": exps[
+                f"min {EXPERIENCE_TIMESPAN_TEXT}"
+            ],
+            "Average recent reviewer experience": exps[
+                f"avg {EXPERIENCE_TIMESPAN_TEXT}"
+            ],
+            "Total recent reviewer backouts": exps[
+                f"sum {EXPERIENCE_TIMESPAN_TEXT} backout"
+            ],
+            "Maximum recent reviewer backouts": exps[
+                f"max {EXPERIENCE_TIMESPAN_TEXT} backout"
+            ],
+            "Minimum recent reviewer backouts": exps[
+                f"min {EXPERIENCE_TIMESPAN_TEXT} backout"
+            ],
+            "Average recent reviewer backouts": exps[
+                f"avg {EXPERIENCE_TIMESPAN_TEXT} backout"
+            ],
+        }
 
 
 class reviewers_num(object):
@@ -173,10 +198,44 @@ class components_modified_num(object):
 
 
 class component_touched_prev(object):
-    name = "# of times the components were touched before"
-
     def __call__(self, commit, **kwargs):
-        return get_exps("component", commit)
+        exps = get_exps("component", commit)
+        return {
+            "# of times these components have been touched before": exps["sum"],
+            "Maximum # of times these components have been touched before": exps["max"],
+            "Minimum # of times these components have been touched before": exps["min"],
+            "Average # of times these components have been touched before": exps["avg"],
+            "# of times patches in these components have been backed out": exps[
+                "sum backout"
+            ],
+            "Maximum # of backouts in these components": exps["max backout"],
+            "Minimum # of backouts in these components": exps["min backout"],
+            "Average # of backouts in these components": exps["avg backout"],
+            "# of times these components have recently been touched": exps[
+                f"sum {EXPERIENCE_TIMESPAN_TEXT}"
+            ],
+            "Maximum # of times these components have recently been touched": exps[
+                f"max {EXPERIENCE_TIMESPAN_TEXT}"
+            ],
+            "Minimum # of times these components have recently been touched": exps[
+                f"min {EXPERIENCE_TIMESPAN_TEXT}"
+            ],
+            "Average # of times these components have recently been touched": exps[
+                f"avg {EXPERIENCE_TIMESPAN_TEXT}"
+            ],
+            "# of recent backouts in these components": exps[
+                f"sum {EXPERIENCE_TIMESPAN_TEXT} backout"
+            ],
+            "Maximum # of recent backouts in these components": exps[
+                f"max {EXPERIENCE_TIMESPAN_TEXT} backout"
+            ],
+            "Minimum # of recent backouts in these components": exps[
+                f"min {EXPERIENCE_TIMESPAN_TEXT} backout"
+            ],
+            "Average # of recent backouts in these components": exps[
+                f"avg {EXPERIENCE_TIMESPAN_TEXT} backout"
+            ],
+        }
 
 
 class directories(object):
@@ -192,10 +251,50 @@ class directories_modified_num(object):
 
 
 class directory_touched_prev(object):
-    name = "# of times the directories were touched before"
-
     def __call__(self, commit, **kwargs):
-        return get_exps("directory", commit)
+        exps = get_exps("directory", commit)
+        return {
+            "# of times these directories have been touched before": exps["sum"],
+            "Maximum # of times these directories have been touched before": exps[
+                "max"
+            ],
+            "Minimum # of times these directories have been touched before": exps[
+                "min"
+            ],
+            "Average # of times these directories have been touched before": exps[
+                "avg"
+            ],
+            "# of times patches in these directories have been backed out": exps[
+                "sum backout"
+            ],
+            "Maximum # of backouts in these directories": exps["max backout"],
+            "Minimum # of backouts in these directories": exps["min backout"],
+            "Average # of backouts in these directories": exps["avg backout"],
+            "# of times these directories have recently been touched": exps[
+                f"sum {EXPERIENCE_TIMESPAN_TEXT}"
+            ],
+            "Maximum # of times these directories have recently been touched": exps[
+                f"max {EXPERIENCE_TIMESPAN_TEXT}"
+            ],
+            "Minimum # of times these directories have recently been touched": exps[
+                f"min {EXPERIENCE_TIMESPAN_TEXT}"
+            ],
+            "Average # of times these directories have recently been touched": exps[
+                f"avg {EXPERIENCE_TIMESPAN_TEXT}"
+            ],
+            "# of recent backouts in these directories": exps[
+                f"sum {EXPERIENCE_TIMESPAN_TEXT} backout"
+            ],
+            "Maximum # of recent backouts in these directories": exps[
+                f"max {EXPERIENCE_TIMESPAN_TEXT} backout"
+            ],
+            "Minimum # of recent backouts in these directories": exps[
+                f"min {EXPERIENCE_TIMESPAN_TEXT} backout"
+            ],
+            "Average # of recent backouts in these directories": exps[
+                f"avg {EXPERIENCE_TIMESPAN_TEXT} backout"
+            ],
+        }
 
 
 class files(object):
@@ -231,10 +330,44 @@ class files(object):
 
 
 class file_touched_prev(object):
-    name = "# of times the files were touched before"
-
     def __call__(self, commit, **kwargs):
-        return get_exps("file", commit)
+        exps = get_exps("component", commit)
+        return {
+            "# of times these files have been touched before": exps["sum"],
+            "Maximum # of times these files have been touched before": exps["max"],
+            "Minimum # of times these files have been touched before": exps["min"],
+            "Average # of times these files have been touched before": exps["avg"],
+            "# of times patches in these files have been backed out": exps[
+                "sum backout"
+            ],
+            "Maximum # of backouts in these files": exps["max backout"],
+            "Minimum # of backouts in these files": exps["min backout"],
+            "Average # of backouts in these files": exps["avg backout"],
+            "# of times these files have recently been touched": exps[
+                f"sum {EXPERIENCE_TIMESPAN_TEXT}"
+            ],
+            "Maximum # of times these files have recently been touched": exps[
+                f"max {EXPERIENCE_TIMESPAN_TEXT}"
+            ],
+            "Minimum # of times these files have recently been touched": exps[
+                f"min {EXPERIENCE_TIMESPAN_TEXT}"
+            ],
+            "Average # of times these files have recently been touched": exps[
+                f"avg {EXPERIENCE_TIMESPAN_TEXT}"
+            ],
+            "# of recent backouts in these files": exps[
+                f"sum {EXPERIENCE_TIMESPAN_TEXT} backout"
+            ],
+            "Maximum # of recent backouts in these files": exps[
+                f"max {EXPERIENCE_TIMESPAN_TEXT} backout"
+            ],
+            "Minimum # of recent backouts in these files": exps[
+                f"min {EXPERIENCE_TIMESPAN_TEXT} backout"
+            ],
+            "Average # of recent backouts in these files": exps[
+                f"avg {EXPERIENCE_TIMESPAN_TEXT} backout"
+            ],
+        }
 
 
 class types(object):
@@ -283,7 +416,7 @@ class CommitExtractor(BaseEstimator, TransformerMixin):
 
                 if isinstance(res, dict):
                     for key, value in res.items():
-                        data[sys.intern(f"{feature_extractor_name} ({key})")] = value
+                        data[sys.intern(key)] = value
                     continue
 
                 if isinstance(res, list):
