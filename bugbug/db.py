mozilla/bugbug

commit 8dbae2c290fe9b8cff0f99f731f15967b8fe3629

Support extracting .tar.zst support files
And extract them directly while decompressing, rather than decompressing
the zstandard file creating a tar file and then extracting the tar file.

bugbug/db.py
@@ -16,7 +16,7 @@ import requests
 import zstandard
 
 from bugbug import utils
-from bugbug.utils import zstd_decompress
+from bugbug.utils import extract_tar_zst, zstd_decompress
 
 DATABASES = {}
 
@@ -59,12 +59,14 @@ def is_old_version(path):
 
 
 def extract_file(path):
-    path, compression_type = os.path.splitext(path)
+    inner_path, _ = os.path.splitext(path)
 
-    if compression_type == ".zst":
-        zstd_decompress(path)
+    if path.endswith(".tar.zst"):
+        extract_tar_zst(inner_path)
+    elif path.endswith(".zst"):
+        zstd_decompress(inner_path)
     else:
-        assert False, f"Unexpected compression type: {compression_type}"
+        assert False, f"Unexpected compression type for {path}"
 
 
 def download_support_file(path, file_name):

bugbug/utils.py
@@ -219,6 +219,14 @@ def open_tar_zst(path):
                 yield tar
 
 
+def extract_tar_zst(path):
+    dctx = zstandard.ZstdDecompressor()
+    with open(f"{path}.zst", "rb") as f:
+        with dctx.stream_reader(f) as reader:
+            with tarfile.open(mode="r|", fileobj=reader) as tar:
+                tar.extractall()
+
+
 class CustomJsonEncoder(json.JSONEncoder):
     """ A custom Json Encoder to support Numpy types
     """

scripts/test_scheduling_history_retriever.py
@@ -4,7 +4,6 @@ import argparse
 import json
 import os
 import subprocess
-import tarfile
 from datetime import datetime
 from logging import INFO, basicConfig, getLogger
 
@@ -57,10 +56,6 @@ class Retriever(object):
         cache_path = os.path.splitext(ADR_CACHE_DB)[0]
         if not db.is_old_version(ADR_CACHE_DB):
             db.download(ADR_CACHE_DB)
-            if os.path.exists(ADR_CACHE_DB):
-                with tarfile.open(ADR_CACHE_DB, "r") as tar:
-                    tar.extractall()
-                assert os.path.exists(cache_path), "Decompressed adr cache exists"
 
         # Setup adr cache configuration.
         os.makedirs(os.path.expanduser("~/.config/adr"), exist_ok=True)

commit 0ad402a87f7aef204884f64654964fcb5aaa4a3a

Support PosixPath paths too
Fixes a test which started failing since 8dbae2c290fe9b8cff0f99f731f15967b8fe3629

Regression from 8dbae2c290fe9b8cff0f99f731f15967b8fe3629

bugbug/db.py
@@ -61,9 +61,9 @@ def is_old_version(path):
 def extract_file(path):
     inner_path, _ = os.path.splitext(path)
 
-    if path.endswith(".tar.zst"):
+    if str(path).endswith(".tar.zst"):
         extract_tar_zst(inner_path)
-    elif path.endswith(".zst"):
+    elif str(path).endswith(".zst"):
         zstd_decompress(inner_path)
     else:
         assert False, f"Unexpected compression type for {path}"