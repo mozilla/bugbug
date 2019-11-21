d8c62e9e0a30e9afa2ec9c48472341caa6a902a8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-11-21 12:53:55 +0100

    Increase the limit for considering commits as too large for test scheduling history generation
    We were skipping ~1500 pushes, from now on we will skip ~500.

--- scripts/test_scheduling_history_retriever.py
+++ scripts/test_scheduling_history_retriever.py
@@ -191,7 +191,7 @@ file = {{ driver = "file", path = "{os.path.abspath(cache_path)}" }}
                 #  - Improve shelve perf and go back to consider all files;
                 #  - Consider only files which appear with a given frequency, like the "files" feature in commit_features;
                 #  - Keep a limit of number of files.
-                if len(merged_commits["files"]) > 20:
+                if len(merged_commits["files"]) > 50:
                     skipped_too_big_commits += 1
                     continue
 
