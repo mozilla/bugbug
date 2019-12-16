06f79569ce865ee7f2171eb28d6703d47203f3cf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-12-16 16:44:31 +0100

    Rename definition file of the data pipeline hook

--- tests/test_hooks.py
+++ tests/test_hooks.py
@@ -18,7 +18,7 @@ with open(os.path.join("VERSION")) as f:
 
 parameters = [
     (os.path.realpath("infra/taskcluster-hook-annotate.json"), {}),
-    (os.path.realpath("infra/taskcluster-hook-pipeline-start.json"), {}),
+    (os.path.realpath("infra/taskcluster-hook-data-pipeline.json"), {}),
     (os.path.realpath("infra/taskcluster-hook-check-models-start.json"), {}),
     (os.path.realpath("infra/taskcluster-hook-classify-patch.json"), {"DIFF_ID": 123}),
     (os.path.realpath("infra/taskcluster-hook-test-select.json"), {"DIFF_ID": 123}),
