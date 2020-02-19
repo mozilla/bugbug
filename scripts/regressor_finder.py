mozilla/bugbug

commit 7c81a5ece9b60e5cb1a8efef705b572c53935d7c

Split regressor finder task in four separate tasks
A task to find commits to ignore, a task to classify commits between
bug-fixing vs not-bug-fixing, a task to find regressors using the
normal repo, and a task to find regressors using the tokenized
repo.

This way we can also find regressors for the two kinds of repos
in parallel.

Fixes #1273

Make the past bugs by function task depend on the task to classify
commits between bug-fixing and not-bug-fixing.

Fixes #1274

scripts/regressor_finder.py
@@ -55,33 +55,40 @@ db.register(
 class RegressorFinder(object):
     def __init__(
         self,
-        cache_root,
-        git_repo_url,
-        git_repo_dir,
-        tokenized_git_repo_url,
-        tokenized_git_repo_dir,
+        mercurial_repo_dir=None,
+        git_repo_url=None,
+        git_repo_dir=None,
+        tokenized_git_repo_url=None,
+        tokenized_git_repo_dir=None,
     ):
-        self.mercurial_repo_dir = os.path.join(cache_root, "mozilla-central")
+        self.mercurial_repo_dir = mercurial_repo_dir
         self.git_repo_url = git_repo_url
         self.git_repo_dir = git_repo_dir
         self.tokenized_git_repo_url = tokenized_git_repo_url
         self.tokenized_git_repo_dir = tokenized_git_repo_dir
 
         with ThreadPoolExecutorResult(max_workers=3) as executor:
-            logger.info(f"Cloning mercurial repository to {self.mercurial_repo_dir}...")
-            executor.submit(repository.clone, self.mercurial_repo_dir)
+            if self.mercurial_repo_dir is not None:
+                logger.info(
+                    f"Cloning mercurial repository to {self.mercurial_repo_dir}..."
+                )
+                executor.submit(repository.clone, self.mercurial_repo_dir)
 
-            logger.info(f"Cloning {self.git_repo_url} to {self.git_repo_dir}...")
-            executor.submit(self.clone_git_repo, self.git_repo_url, self.git_repo_dir)
+            if self.git_repo_url is not None:
+                logger.info(f"Cloning {self.git_repo_url} to {self.git_repo_dir}...")
+                executor.submit(
+                    self.clone_git_repo, self.git_repo_url, self.git_repo_dir
+                )
 
-            logger.info(
-                f"Cloning {self.tokenized_git_repo_url} to {self.tokenized_git_repo_dir}..."
-            )
-            executor.submit(
-                self.clone_git_repo,
-                self.tokenized_git_repo_url,
-                self.tokenized_git_repo_dir,
-            )
+            if self.tokenized_git_repo_url is not None:
+                logger.info(
+                    f"Cloning {self.tokenized_git_repo_url} to {self.tokenized_git_repo_dir}..."
+                )
+                executor.submit(
+                    self.clone_git_repo,
+                    self.tokenized_git_repo_url,
+                    self.tokenized_git_repo_dir,
+                )
 
         logger.info(f"Initializing mapping between git and mercurial commits...")
         self.init_mapping()
@@ -118,10 +125,11 @@ class RegressorFinder(object):
         vcs_map.download_mapfile()
         vcs_map.load_mapfile()
 
-        (
-            self.tokenized_git_to_mercurial,
-            self.mercurial_to_tokenized_git,
-        ) = microannotate_utils.get_commit_mapping(self.tokenized_git_repo_dir)
+        if self.tokenized_git_repo_url is not None:
+            (
+                self.tokenized_git_to_mercurial,
+                self.mercurial_to_tokenized_git,
+            ) = microannotate_utils.get_commit_mapping(self.tokenized_git_repo_dir)
 
     # TODO: Make repository module analyze all commits, even those to ignore, but add a field "ignore" or a function should_ignore that analyzes the commit data. This way we don't have to clone the Mercurial repository in this script.
     def get_commits_to_ignore(self):
@@ -183,8 +191,6 @@ class RegressorFinder(object):
         db.append(IGNORED_COMMITS_DB, commits_to_ignore)
         zstd_compress(IGNORED_COMMITS_DB)
         db.upload(IGNORED_COMMITS_DB)
-
-        return prev_commits_to_ignore + commits_to_ignore
 
     def find_bug_fixing_commits(self):
         logger.info("Downloading commits database...")
@@ -280,22 +286,23 @@ class RegressorFinder(object):
         zstd_compress(BUG_FIXING_COMMITS_DB)
         db.upload(BUG_FIXING_COMMITS_DB)
 
-        bug_fixing_commits = prev_bug_fixing_commits + bug_fixing_commits
-        return [
+    def find_bug_introducing_commits(self, repo_dir, tokenized):
+        logger.info("Download commits to ignore...")
+        assert db.download(IGNORED_COMMITS_DB)
+        commits_to_ignore = list(db.read(IGNORED_COMMITS_DB))
+
+        logger.info("Download bug-fixing classifications...")
+        assert db.download(BUG_FIXING_COMMITS_DB)
+        bug_fixing_commits = [
             bug_fixing_commit
-            for bug_fixing_commit in bug_fixing_commits
+            for bug_fixing_commit in db.read(BUG_FIXING_COMMITS_DB)
             if bug_fixing_commit["type"] in ["r", "d"]
         ]
 
-    def find_bug_introducing_commits(
-        self, bug_fixing_commits, commits_to_ignore, tokenized
-    ):
         if tokenized:
             db_path = TOKENIZED_BUG_INTRODUCING_COMMITS_DB
-            repo_dir = self.tokenized_git_repo_dir
         else:
             db_path = BUG_INTRODUCING_COMMITS_DB
-            repo_dir = self.git_repo_dir
 
         def git_to_mercurial(rev):
             if tokenized:
@@ -492,6 +499,12 @@ class RegressorFinder(object):
 
 
 def evaluate(bug_introducing_commits):
+    logger.info("Downloading commits database...")
+    assert db.download(repository.COMMITS_DB)
+
+    logger.info("Downloading bugs database...")
+    assert db.download(bugzilla.BUGS_DB)
+
     logger.info("Building bug -> commits map...")
     bug_to_commits_map = defaultdict(list)
     for commit in tqdm(repository.get_commits()):
@@ -586,45 +599,54 @@ def main():
     description = "Find bug-introducing commits from bug-fixing commits"
     parser = argparse.ArgumentParser(description=description)
 
-    parser.add_argument("cache_root", help="Cache for repository clones.")
+    parser.add_argument("what", choices=["to_ignore", "bug_fixing", "bug_introducing"])
     parser.add_argument(
-        "git_repo_url", help="URL to the git repository on which to run SZZ."
+        "--repo_dir",
+        help="Path to a Gecko repository. If no repository exists, it will be cloned to this location.",
     )
     parser.add_argument(
-        "git_repo_dir", help="Path where the git repository will be cloned."
+        "--git_repo_url", help="URL to the git repository on which to run SZZ."
     )
     parser.add_argument(
-        "tokenized_git_repo_url",
+        "--git_repo_dir", help="Path where the git repository will be cloned."
+    )
+    parser.add_argument(
+        "--tokenized_git_repo_url",
         help="URL to the tokenized git repository on which to run SZZ.",
     )
     parser.add_argument(
-        "tokenized_git_repo_dir",
+        "--tokenized_git_repo_dir",
         help="Path where the tokenized git repository will be cloned.",
     )
 
     args = parser.parse_args()
 
     regressor_finder = RegressorFinder(
-        args.cache_root,
+        args.repo_dir,
         args.git_repo_url,
         args.git_repo_dir,
         args.tokenized_git_repo_url,
         args.tokenized_git_repo_dir,
     )
 
-    commits_to_ignore = regressor_finder.get_commits_to_ignore()
+    if args.what == "to_ignore":
+        regressor_finder.get_commits_to_ignore()
+    elif args.what == "bug_fixing":
+        regressor_finder.find_bug_fixing_commits()
+    elif args.what == "bug_introducing":
+        assert args.git_repo_url or args.tokenized_git_repo_url
 
-    bug_fixing_commits = regressor_finder.find_bug_fixing_commits()
+        if args.git_repo_url:
+            assert not args.tokenized_git_repo_url
+            regressor_finder.find_bug_introducing_commits(args.git_repo_dir, False)
+            evaluate(db.read(BUG_INTRODUCING_COMMITS_DB))
 
-    regressor_finder.find_bug_introducing_commits(
-        bug_fixing_commits, commits_to_ignore, True
-    )
-    evaluate(db.read(TOKENIZED_BUG_INTRODUCING_COMMITS_DB))
-
-    regressor_finder.find_bug_introducing_commits(
-        bug_fixing_commits, commits_to_ignore, False
-    )
-    evaluate(db.read(BUG_INTRODUCING_COMMITS_DB))
+        if args.tokenized_git_repo_url:
+            assert not args.git_repo_url
+            regressor_finder.find_bug_introducing_commits(
+                args.tokenized_git_repo_dir, True
+            )
+            evaluate(db.read(TOKENIZED_BUG_INTRODUCING_COMMITS_DB))
 
 
 if __name__ == "__main__":

infra/data-pipeline.yml
@@ -464,30 +464,21 @@ tasks:
         owner: release-mgmt-analysis@mozilla.com
         source: ${repository}/raw/master/data-pipeline.yml
 
-    - ID: regressor-finder
+    - ID: regressor-finder-commits-to-ignore
       created: {$fromNow: ''}
-      deadline: {$fromNow: '5 days'}
+      deadline: {$fromNow: '1 day'}
       expires: {$fromNow: '1 month'}
       provisionerId: proj-relman
-      workerType: compute-large
+      workerType: compute-small
       dependencies:
-        - commit-retrieval
-        - microannotate-generate-tokenize
-        - microannotate-generate-remove-comments
-        - microannotate-generate-tokenize-and-remove-comments
-        - bugs-retrieval
-        - train-defectenhancementtask
-        - train-regression
+        - commits-retrieval
       payload:
-        maxRunTime: 432000
+        maxRunTime: 25200
         image: mozilla/bugbug-commit-retrieval:${version}
         command:
           - bugbug-regressor-finder
-          - /cache/
-          - https://github.com/mozilla/gecko-dev
-          - gecko-dev
-          - https://github.com/marco-c/gecko-dev-wordified-and-comments-removed
-          - gecko-dev-wordified-and-comments-removed
+          - to_ignore
+          - --repo_dir=/cache/mozilla-central/
         cache:
           bugbug-mercurial-repository: /cache
         features:
@@ -499,23 +490,129 @@ tasks:
       routes:
         - notify.email.release-mgmt-analysis@mozilla.com.on-failed
         - notify.irc-channel.#bugbug.on-failed
-        - index.project.relman.bugbug_annotate.regressor_finder.latest
+        - index.project.relman.bugbug_annotate.regressor_finder_commits_to_ignore.latest
       metadata:
-        name: bugbug regressor finder
-        description: bugbug regressor finder
+        name: bugbug regressor finder (commits to ignore)
+        description: bugbug regressor finder (commits to ignore)
+        owner: release-mgmt-analysis@mozilla.com
+        source: ${repository}/raw/master/data-pipeline.yml
+
+    - ID: regressor-finder-bug-fixing-commits
+      created: {$fromNow: ''}
+      deadline: {$fromNow: '1 day'}
+      expires: {$fromNow: '1 month'}
+      provisionerId: proj-relman
+      workerType: compute-small
+      dependencies:
+        - commit-retrieval
+        - bugs-retrieval
+        - train-defectenhancementtask
+        - train-regression
+      payload:
+        maxRunTime: 25200
+        image: mozilla/bugbug-base:${version}
+        command:
+          - bugbug-regressor-finder
+          - bug_fixing
+        features:
+          taskclusterProxy:
+            true
+      scopes:
+        - "auth:aws-s3:read-write:communitytc-bugbug/*"
+      routes:
+        - notify.email.release-mgmt-analysis@mozilla.com.on-failed
+        - notify.irc-channel.#bugbug.on-failed
+        - index.project.relman.bugbug_annotate.regressor_finder_bug_fixing_commits.latest
+      metadata:
+        name: bugbug regressor finder (bug-fixing commits)
+        description: bugbug regressor finder (bug-fixing commits)
+        owner: release-mgmt-analysis@mozilla.com
+        source: ${repository}/raw/master/data-pipeline.yml
+
+    - ID: regressor-finder-tokenized-bug-introducing
+      created: {$fromNow: ''}
+      deadline: {$fromNow: '5 days'}
+      expires: {$fromNow: '1 month'}
+      provisionerId: proj-relman
+      workerType: compute-large
+      dependencies:
+        - commit-retrieval
+        - microannotate-generate-tokenize
+        - microannotate-generate-remove-comments
+        - microannotate-generate-tokenize-and-remove-comments
+        - bugs-retrieval
+        - regressor-finder-commits-to-ignore
+        - regressor-finder-bug-fixing-commits
+      payload:
+        maxRunTime: 432000
+        image: mozilla/bugbug-base:${version}
+        command:
+          - bugbug-regressor-finder
+          - bug_introducing
+          - --tokenized_git_repo_url=https://github.com/marco-c/gecko-dev-wordified-and-comments-removed
+          - --tokenized_git_repo_dir=gecko-dev-wordified-and-comments-removed
+        features:
+          taskclusterProxy:
+            true
+      scopes:
+        - "auth:aws-s3:read-write:communitytc-bugbug/*"
+      routes:
+        - notify.email.release-mgmt-analysis@mozilla.com.on-failed
+        - notify.irc-channel.#bugbug.on-failed
+        - index.project.relman.bugbug_annotate.regressor_finder_tokenized_bug_introducing_commits.latest
+      metadata:
+        name: bugbug regressor finder (tokenized bug-introducing commits)
+        description: bugbug regressor finder (tokenized bug-introducing commits)
+        owner: release-mgmt-analysis@mozilla.com
+        source: ${repository}/raw/master/data-pipeline.yml
+
+    - ID: regressor-finder-bug-introducing
+      created: {$fromNow: ''}
+      deadline: {$fromNow: '5 days'}
+      expires: {$fromNow: '1 month'}
+      provisionerId: proj-relman
+      workerType: compute-large
+      dependencies:
+        - commit-retrieval
+        - microannotate-generate-tokenize
+        - microannotate-generate-remove-comments
+        - microannotate-generate-tokenize-and-remove-comments
+        - bugs-retrieval
+        - regressor-finder-commits-to-ignore
+        - regressor-finder-bug-fixing-commits
+      payload:
+        maxRunTime: 432000
+        image: mozilla/bugbug-base:${version}
+        command:
+          - bugbug-regressor-finder
+          - bug_introducing
+          - --git_repo_url=https://github.com/mozilla/gecko-dev
+          - --git_repo_dir=gecko-dev
+        features:
+          taskclusterProxy:
+            true
+      scopes:
+        - "auth:aws-s3:read-write:communitytc-bugbug/*"
+      routes:
+        - notify.email.release-mgmt-analysis@mozilla.com.on-failed
+        - notify.irc-channel.#bugbug.on-failed
+        - index.project.relman.bugbug_annotate.regressor_finder_bug_introducing_commits.latest
+      metadata:
+        name: bugbug regressor finder (bug-introducing commits)
+        description: bugbug regressor finder (bug-introducing commits)
         owner: release-mgmt-analysis@mozilla.com
         source: ${repository}/raw/master/data-pipeline.yml
 
     - ID: past-bugs-by-function
       created: {$fromNow: ''}
-      deadline: {$fromNow: '5 days'}
+      deadline: {$fromNow: '1 day'}
       expires: {$fromNow: '1 month'}
       provisionerId: proj-relman
       workerType: batch
       dependencies:
         - commit-retrieval
         - bugs-retrieval
-        - regressor-finder
+        - regressor-finder-bug-fixing
       payload:
         maxRunTime: 7200
         image: mozilla/bugbug-base:${version}
@@ -545,7 +642,7 @@ tasks:
       workerType: compute-small
       dependencies:
         - commit-retrieval
-        - regressor-finder
+        - regressor-finder-bug-introducing
       payload:
         maxRunTime: 25200
         image: mozilla/bugbug-base:${version}

commit 89ecb8f527791d3aa47e6720d327e10cdd66b12b

Import pydriller only for finding bug-introducing commits
To avoid issues in the bug-fixing commits task where git is not installed.

Regression from 7c81a5ece9b60e5cb1a8efef705b572c53935d7c

.isort.cfg
@@ -4,4 +4,4 @@ include_trailing_comma=True
 force_grid_wrap=0
 use_parentheses=True
 line_length=88
-known_third_party = apispec,apispec_webframeworks,boto3,cerberus,dateutil,flask,flask_cors,hglib,imblearn,joblib,jsone,jsonschema,libmozdata,lmdb,marshmallow,matplotlib,microannotate,models,numpy,orjson,pandas,pkg_resources,pydriller,pyemd,pytest,redis,requests,responses,rq,scipy,setuptools,shap,sklearn,tabulate,taskcluster,tenacity,tqdm,xgboost,yaml,zstandard
+known_third_party = apispec,apispec_webframeworks,boto3,cerberus,dateutil,flask,flask_cors,hglib,imblearn,joblib,jsone,jsonschema,libmozdata,lmdb,marshmallow,matplotlib,microannotate,models,numpy,orjson,pandas,pkg_resources,pyemd,pytest,redis,requests,responses,rq,scipy,setuptools,shap,sklearn,tabulate,taskcluster,tenacity,tqdm,xgboost,yaml,zstandard

scripts/regressor_finder.py
@@ -18,8 +18,6 @@ import tenacity
 from dateutil.relativedelta import relativedelta
 from libmozdata import vcs_map
 from microannotate import utils as microannotate_utils
-from pydriller import GitRepository
-from pydriller.domain.commit import ModificationType
 from tqdm import tqdm
 
 from bugbug import bugzilla, db, repository
@@ -296,6 +294,9 @@ class RegressorFinder(object):
         db.upload(BUG_FIXING_COMMITS_DB)
 
     def find_bug_introducing_commits(self, repo_dir, tokenized):
+        from pydriller import GitRepository
+        from pydriller.domain.commit import ModificationType
+
         logger.info("Download commits to ignore...")
         assert db.download(IGNORED_COMMITS_DB)
         commits_to_ignore = list(db.read(IGNORED_COMMITS_DB))