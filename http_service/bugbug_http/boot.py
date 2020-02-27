cfd82a8ad3635ab0214818c461b5220dccf76aa8
Bastien Abadie <bastien@nextcairn.com>
2020-02-27 12:34:05 +0100

    Clone mozilla-central and load the test scheduling and commits DBs when the HTTP service's workers start
    Fixes #1299

---
+++ http_service/bugbug_http/boot.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import os
+import tempfile
+
+from bugbug import db, repository, test_scheduling
+
+
+def boot_worker():
+
+    # Clone mozilla central
+    repo_dir = os.path.join(tempfile.gettempdir(), "bugbug-hg")
+    repository.clone(repo_dir)
+
+    # Download databases
+    assert db.download_support_file(
+        test_scheduling.TEST_LABEL_SCHEDULING_DB, test_scheduling.PAST_FAILURES_LABEL_DB
+    )
+
+    # Download commits DB
+    assert db.download(repository.COMMITS_DB, support_files_too=True)
+
+    # And update it
+    for commit in repository.get_commits():
+        pass
+
+    rev_start = "children({})".format(commit["node"])
+    repository.download_commits(repo_dir, rev_start)
