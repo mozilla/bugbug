mozilla/bugbug

commit 2a484732fc2c232e9ce65b6546c537071b073fb5

Clone mozilla autoland in the HTTP service worker (#1372)

bugbug/repository.py
@@ -973,19 +973,19 @@ def clean(repo_dir):
                 raise
 
         # Pull and update.
-        logger.info("Pulling and updating mozilla-central")
+        logger.info(f"Pulling and updating {repo_dir}")
         hg.pull(update=True)
-        logger.info("mozilla-central pulled and updated")
+        logger.info("{repo_dir} pulled and updated")
 
 
-def clone(repo_dir):
+def clone(repo_dir, url="https://hg.mozilla.org/mozilla-central"):
     if os.path.exists(repo_dir):
         clean(repo_dir)
         return
 
     cmd = hglib.util.cmdbuilder(
         "robustcheckout",
-        "https://hg.mozilla.org/mozilla-central",
+        url,
         repo_dir,
         purge=True,
         sharebase=repo_dir + "-shared",
@@ -1000,7 +1000,7 @@ def clone(repo_dir):
     if proc.returncode:
         raise hglib.error.CommandError(cmd, proc.returncode, out, err)
 
-    logger.info("mozilla-central cloned")
+    logger.info(f"{repo_dir} cloned")
 
     # Remove pushlog DB to make sure it's regenerated.
     try:

http_service/bugbug_http/boot.py
@@ -13,9 +13,9 @@ logger = logging.getLogger(__name__)
 
 
 def boot_worker():
-    # Clone mozilla central
-    logger.info(f"Cloning mozilla-central in {REPO_DIR}...")
-    repository.clone(REPO_DIR)
+    # Clone autoland
+    logger.info(f"Cloning mozilla autoland in {REPO_DIR}...")
+    repository.clone(REPO_DIR, "https://hg.mozilla.org/integration/autoland")
 
     # Download test scheduling DB support files.
     logger.info("Downloading test scheduling DB support files...")

commit c8426b9a2acd5c18c8df4c857084271298cea387

Add missing 'f' in a f-string
Regression from 2a484732fc2c232e9ce65b6546c537071b073fb5

bugbug/repository.py
@@ -975,7 +975,7 @@ def clean(repo_dir):
         # Pull and update.
         logger.info(f"Pulling and updating {repo_dir}")
         hg.pull(update=True)
-        logger.info("{repo_dir} pulled and updated")
+        logger.info(f"{repo_dir} pulled and updated")
 
 
 def clone(repo_dir, url="https://hg.mozilla.org/mozilla-central"):