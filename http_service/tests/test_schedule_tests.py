05781aa32b3d320f68a221372bd2a4bdadb98844
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-11 02:49:11 +0100

    Extend the schedule tests tests to cover scheduling tests

--- http_service/tests/test_schedule_tests.py
+++ http_service/tests/test_schedule_tests.py
@@ -3,14 +3,36 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import json
+
 import hglib
 import pytest
 
-from bugbug_http.models import schedule_tests
+from bugbug_http import models
 
 
-def test_simple_schedule(patch_resources, mock_hgmo, mock_repo):
-
+@pytest.mark.parametrize(
+    "labels_to_choose, groups_to_choose",
+    [
+        # one from label, one from group
+        (["test-label1"], ["test-group2"],),
+        # one from label, none from group
+        (["test-label1"], [],),
+        # none from label, one from group
+        ([], ["test-group1"],),
+        # two from label, one from group
+        (["test-label1", "test-label2"], ["test-group2"],),
+    ],
+)
+def test_simple_schedule(
+    labels_to_choose,
+    groups_to_choose,
+    monkeypatch,
+    mock_hgmo,
+    mock_repo,
+    mock_component_taskcluster_artifact,
+    mock_schedule_tests_classify,
+):
     # The repo should be almost empty at first
     repo_dir, _ = mock_repo
     repo = hglib.open(str(repo_dir))
@@ -19,12 +41,21 @@ def test_simple_schedule(patch_resources, mock_hgmo, mock_repo):
     assert test_txt.exists()
     assert test_txt.read_text("utf-8") == "Version 3"
 
+    mock_schedule_tests_classify(labels_to_choose, groups_to_choose)
+
     # Scheduling a test on a revision should apply changes in the repo
-    assert schedule_tests("mozilla-central", "12345deadbeef") == "OK"
+    assert models.schedule_tests("mozilla-central", "12345deadbeef") == "OK"
 
     # Check changes have been applied
     assert len(repo.log()) == 5
     assert test_txt.read_text("utf-8") == "Version 3\nThis is a new line\n"
+
+    # Assert the test selection result is stored in Redis.
+    assert json.loads(
+        models.redis.get(
+            "bugbug:job_result:schedule_tests:mozilla-central_12345deadbeef"
+        )
+    ) == {"tasks": labels_to_choose, "groups": groups_to_choose,}
 
 
 @pytest.mark.parametrize(
@@ -44,7 +75,7 @@ def test_simple_schedule(patch_resources, mock_hgmo, mock_repo):
             "orphan123",
             "OK",
             [
-                "Orphan 123",
+                "Bug 123 - Orphan 123",
                 "Base history 3",
                 "Base history 2",
                 "Base history 1",
@@ -57,8 +88,8 @@ def test_simple_schedule(patch_resources, mock_hgmo, mock_repo):
             "needRemote",
             "OK",
             [
-                "On top of remote + local",
-                "Based on remote",
+                "Bug 123 - On top of remote + local",
+                "Bug 123 - Based on remote",
                 "Pulled from remote",
                 "Base history 3",
                 "Base history 2",
@@ -107,7 +138,7 @@ def test_simple_schedule(patch_resources, mock_hgmo, mock_repo):
             "needRemote",
             "OK",
             [
-                "Depends on remote",
+                "Bug 123 - Depends on remote",
                 "Pulled from remote",
                 "Base history 3",
                 "Base history 2",
@@ -118,7 +149,15 @@ def test_simple_schedule(patch_resources, mock_hgmo, mock_repo):
     ],
 )
 def test_schedule(
-    branch, revision, result, final_log, patch_resources, mock_hgmo, mock_repo
+    branch,
+    revision,
+    result,
+    final_log,
+    patch_resources,
+    mock_hgmo,
+    mock_repo,
+    mock_component_taskcluster_artifact,
+    mock_schedule_tests_classify,
 ):
 
     # The repo should only have the base commits
@@ -133,8 +172,16 @@ def test_schedule(
         "Base history 0",
     ]
 
+    mock_schedule_tests_classify(["test-label1"], ["test-group2"])
+
     # Schedule tests for parametrized revision
-    assert schedule_tests(branch, revision) == result
+    assert models.schedule_tests(branch, revision) == result
 
     # Now check the log has evolved
     assert final_log == [l.desc.decode("utf-8") for l in repo.log(follow=True)]
+
+    if result == "OK":
+        # Assert the test selection result is stored in Redis.
+        assert json.loads(
+            models.redis.get(f"bugbug:job_result:schedule_tests:{branch}_{revision}")
+        ) == {"tasks": ["test-label1"], "groups": ["test-group2"],}
