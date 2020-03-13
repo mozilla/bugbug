mozilla/bugbug

commit b74bf96400d0e845705ae41ec308119125fa31ab

Use a single process for mining commits in the HTTP service
Since we need to mine only a few commits per call, it doesn't make sense
to pay the overhead of starting many threads and processes.

This should help with #1374

http_service/bugbug_http/boot.py
@@ -45,7 +45,9 @@ def boot_worker():
 
     # Download commits DB
     logger.info("Downloading commits DB...")
-    commits_db_downloaded = db.download(repository.COMMITS_DB, support_files_too=True)
+    commits_db_downloaded = db.download(
+        repository.COMMITS_DB, support_files_too=True, use_single_process=True
+    )
     if not ALLOW_MISSING_MODELS:
         assert commits_db_downloaded
 

http_service/bugbug_http/models.py
@@ -167,7 +167,9 @@ def schedule_tests(branch, rev):
     )
 
     # Analyze patches.
-    commits = repository.download_commits(REPO_DIR, rev_start=first_rev, save=False)
+    commits = repository.download_commits(
+        REPO_DIR, rev_start=first_rev, save=False, use_single_process=True
+    )
 
     commit_data = commit_features.merge_commits(commits)
 

commit 2ac0bc4c12bf0bd37abcbd631ef4f503a46aeee5

Move the use_single_process parameter to the right function call...
Regression from b74bf96400d0e845705ae41ec308119125fa31ab

http_service/bugbug_http/boot.py
@@ -45,9 +45,7 @@ def boot_worker():
 
     # Download commits DB
     logger.info("Downloading commits DB...")
-    commits_db_downloaded = db.download(
-        repository.COMMITS_DB, support_files_too=True, use_single_process=True
-    )
+    commits_db_downloaded = db.download(repository.COMMITS_DB, support_files_too=True)
     if not ALLOW_MISSING_MODELS:
         assert commits_db_downloaded
 
@@ -59,7 +57,7 @@ def boot_worker():
 
         rev_start = "children({})".format(commit["node"])
         logger.info("Updating commits DB...")
-        repository.download_commits(REPO_DIR, rev_start)
+        repository.download_commits(REPO_DIR, rev_start, use_single_process=True)
 
     # Preload models
     bugbug_http.models.preload_models()