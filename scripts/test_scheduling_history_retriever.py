6088fcc7542324093a25fe9433a944a7ffb01ff9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-13 20:09:34 +0100

    Increase group-level test scheduling history to retrieve
    And drop comment related to ActiveData which no longer applies.

--- scripts/test_scheduling_history_retriever.py
+++ scripts/test_scheduling_history_retriever.py
@@ -39,11 +39,7 @@ PUSH_DATA_URL = "https://community-tc.services.mozilla.com/api/index/v1/task/pro
 
 TRAINING_MONTHS = {
     "label": 6,
-    # For groups, we only have 12 weeks in ActiveData. Getting previous data
-    # from task artifacts is slow, so for now we only get a bit more of what
-    # we can get from ActiveData and we'll see if it's enough to train a
-    # satisfying model.
-    "group": 4,
+    "group": 5,
 }
 
 
