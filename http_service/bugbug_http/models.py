mozilla/bugbug

commit ececf8a58a262679ef3388215211ca38cd2e0427

Cache models in the HTTP service workers only if they are used more than once in two hours
Part of #1342

http_service/bugbug_http/models.py
@@ -6,10 +6,12 @@
 import json
 import logging
 import os
+from datetime import datetime
 from typing import Dict
 from urllib.request import urlretrieve
 
 import requests
+from dateutil.relativedelta import relativedelta
 from redis import Redis
 
 from bugbug import bugzilla, get_bugbug_version
@@ -34,6 +36,7 @@ BASE_URL = "https://community-tc.services.mozilla.com/api/index/v1/task/project.
 DEFAULT_EXPIRATION_TTL = 7 * 24 * 3600  # A week
 
 
+MODEL_LAST_LOADED: Dict[str, datetime] = {}
 MODEL_CACHE: Dict[str, Model] = {}
 
 
@@ -60,10 +63,16 @@ def get_model(model_name):
             else:
                 raise
 
-        MODEL_CACHE[model_name] = model
-        return model
+        # Cache the model only if it was last used less than two hours ago.
+        if model_name in MODEL_LAST_LOADED and MODEL_LAST_LOADED[
+            model_name
+        ] > datetime.now() - relativedelta(hours=2):
+            MODEL_CACHE[model_name] = model
+    else:
+        model = MODEL_CACHE[model_name]
 
-    return MODEL_CACHE[model_name]
+    MODEL_LAST_LOADED[model_name] = datetime.now()
+    return model
 
 
 def preload_models():

commit 227b02e88080cc6bf524c6c6bef249949e374781

Force caching models that we want to preload
Regression from ececf8a58a262679ef3388215211ca38cd2e0427

http_service/bugbug_http/models.py
@@ -43,7 +43,7 @@ MODEL_CACHE: Dict[str, Model] = {}
 redis = Redis.from_url(os.environ.get("REDIS_URL", "redis://localhost/0"))
 
 
-def get_model(model_name):
+def get_model(model_name, force_cache=False):
     if model_name not in MODEL_CACHE:
         LOGGER.info("Recreating the %r model in cache" % model_name)
         try:
@@ -59,9 +59,10 @@ def get_model(model_name):
                 raise
 
         # Cache the model only if it was last used less than two hours ago.
-        if model_name in MODEL_LAST_LOADED and MODEL_LAST_LOADED[
-            model_name
-        ] > datetime.now() - relativedelta(hours=2):
+        if force_cache or (
+            model_name in MODEL_LAST_LOADED
+            and MODEL_LAST_LOADED[model_name] > datetime.now() - relativedelta(hours=2)
+        ):
             MODEL_CACHE[model_name] = model
     else:
         model = MODEL_CACHE[model_name]
@@ -72,7 +73,7 @@ def get_model(model_name):
 
 def preload_models():
     for model in MODELS_TO_PRELOAD:
-        get_model(model)
+        get_model(model, force_cache=True)
 
 
 def setkey(key, value, expiration=DEFAULT_EXPIRATION_TTL):