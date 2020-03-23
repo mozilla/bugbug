97f4545d22b3568be3bee7ae541d246c41bbf6b9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-23 13:25:22 +0100

    Download required DBs directly in the HTTP service worker Docker image
    Fixes #1344

--- http_service/bugbug_http/download_models.py
+++ http_service/bugbug_http/download_models.py
@@ -3,13 +3,37 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from bugbug import utils
+from bugbug import db, repository, test_scheduling, utils
 from bugbug_http.models import MODELS_NAMES
 
 
 def preload_models():
     for model_name in MODELS_NAMES:
         utils.download_model(model_name)
+
+    db.download_support_file(
+        test_scheduling.TEST_LABEL_SCHEDULING_DB,
+        test_scheduling.PAST_FAILURES_LABEL_DB,
+        extract=False,
+    )
+
+    db.download_support_file(
+        test_scheduling.TEST_GROUP_SCHEDULING_DB,
+        test_scheduling.PAST_FAILURES_GROUP_DB,
+        extract=False,
+    )
+
+    db.download_support_file(
+        test_scheduling.TEST_GROUP_SCHEDULING_DB,
+        test_scheduling.TOUCHED_TOGETHER_DB,
+        extract=False,
+    )
+
+    db.download_support_file(
+        repository.COMMITS_DB, repository.COMMIT_EXPERIENCES_DB, extract=False
+    )
+
+    db.download(repository.COMMITS_DB, extract=False)
 
 
 if __name__ == "__main__":
