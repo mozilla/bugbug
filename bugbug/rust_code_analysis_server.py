34fb012dd015ee24feabbbd0cf4517275486d7cc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-23 14:44:52 +0100

    Don't use a connection pool to connect to rust-code-analysis
    rust-code-analysis sometimes fails when using a keep-alive connection.
    Partial revert of 942e19ff709b560bc02d0ceb02f55692d714d2b8

--- bugbug/rust_code_analysis_server.py
+++ bugbug/rust_code_analysis_server.py
@@ -59,7 +59,7 @@ class RustCodeAnalysisServer:
 
     def ping(self):
         try:
-            r = utils.get_session("code_analysis").get(f"{self.base_url}/ping")
+            r = requests.get(f"{self.base_url}/ping")
             return r.ok
         except requests.exceptions.ConnectionError:
             return False
@@ -71,7 +71,7 @@ class RustCodeAnalysisServer:
         """
         unit = 1 if unit else 0
         url = f"{self.base_url}/metrics?file_name={filename}&unit={unit}"
-        r = utils.get_session("code_analysis").post(url, data=code, headers=HEADERS)
+        r = requests.post(url, data=code, headers=HEADERS)
 
         if not r.ok:
             return {}
@@ -80,7 +80,7 @@ class RustCodeAnalysisServer:
 
     def function(self, filename, code):
         url = f"{self.base_url}/function?file_name={filename}"
-        r = utils.get_session("code_analysis").post(url, data=code, headers=HEADERS)
+        r = requests.post(url, data=code, headers=HEADERS)
 
         if not r.ok:
             return {}
