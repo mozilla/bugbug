3784a0980b2467d8b5e1db48e016a2421a93e12a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-05-03 17:27:47 +0200

    Bump version of the DB of commits to ignore for finding regressors

--- scripts/regressor_finder.py
+++ scripts/regressor_finder.py
@@ -46,7 +46,7 @@ IGNORED_COMMITS_DB = "data/ignored_commits.json"
 db.register(
     IGNORED_COMMITS_DB,
     "https://s3-us-west-2.amazonaws.com/communitytc-bugbug/data/ignored_commits.json.zst",
-    4,
+    5,
 )
 
 
