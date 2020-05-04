2a91bc11256eb4779153d211dc81b377f6b7eddc
Armen Zambrano G <armenzg@mozilla.com>
2020-05-04 15:16:07 -0400

    Bug 1622336 - Initialize schedules service when Hg push happens
    This change adds a Pulse listener for Hg pushes as a background process
    to `bugbug-http-service`.
    
    This listener works if `PULSE_USER` and `PULSE_PASSWORD` are set before starting
    up the Docker containers.
    
    The listener only pays attention to autoland and try pushes. When these happen
    we do a GET request from the bugbug deployment. This will get the schedules service
    ready for when the Gecko decision task will query it.

---
+++ http_service/pulse.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Generate your credentials in https://pulseguardian.mozilla.org
+
+Call this script like:
+  export PULSE_USER=generated_username
+  export PULSE_PASSWORD=generated_username
+  ./pulse.py
+"""
+import logging
+import os
+
+from pulse.consumers import ConsumerFactory
+
+logging.basicConfig()
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def _on_message(body, message):
+    logger.info(body)
+    message.ack()
+
+
+def main():
+    # Generate user/password in https://pulseguardian.mozilla.org/
+    # Set PULSE_USER and PULSE_PASSWORD as env variables
+    user = os.environ["PULSE_USER"]
+    password = os.environ["PULSE_PASSWORD"]
+    connection, consumer = ConsumerFactory.hg_pushes(user, password, _on_message)
+    with connection as conn:  # noqa
+        consumer.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ http_service/pulse/consumers.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Library with Pulse factory that can generate Pulse consumers.
+
+Import the ConsumerFactory and generate the connection & consumer you need.
+
+```python
+    # Righ now the factory only has hg_pushes to generate a connection & consumer
+    connection, consumer = ConsumerFactory.hg_pushes(user, password, _on_message)
+    with connection as conn:  # noqa
+        consumer.run()
+```
+"""
+from kombu import Connection, Exchange, Queue
+from kombu.mixins import ConsumerMixin
+
+_CONNECTION_URL = "amqp://{}:{}@pulse.mozilla.org:5671/?ssl=1"
+
+
+def _generate_hg_pushes_queue(user, auto_delete=True):
+    return Queue(
+        name="queue/{}/pushes".format(user),
+        exchange=Exchange("exchange/hgpushes/v2", type="topic", no_declare=True,),
+        routing_key="#",
+        durable=True,
+        auto_delete=auto_delete,
+    )
+
+
+class _GenericConsumer(ConsumerMixin):
+    def __init__(self, connection, queues, callback):
+        self.connection = connection
+        self.queues = queues
+        self.callback = callback
+
+    def get_consumers(self, Consumer, channel):
+        return [Consumer(queues=self.queues, callbacks=[self.callback])]
+
+
+class ConsumerFactory:
+    @staticmethod
+    def hg_pushes(user, password, callback):
+        connection = Connection(_CONNECTION_URL.format(user, password))
+        queues = [_generate_hg_pushes_queue(user)]
+        consumer = _GenericConsumer(connection, queues, callback)
+        return connection, consumer
