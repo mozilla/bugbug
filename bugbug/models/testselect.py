mozilla/bugbug

commit ed9b2113f9bbe975624783c65eed978c9074aaef

When selecting tests, skip non-test tasks directly instead of first needlessly generating data for them

bugbug/models/testselect.py
@@ -205,13 +205,13 @@ class TestSelectModel(Model):
             push_num = past_failures_data["push_num"] + 1
         all_runnables = past_failures_data["all_runnables"]
 
+        if self.granularity == "label":
+            all_runnables = tuple(r for r in all_runnables if not r.startswith("test-"))
+
         commit_tests = []
         for data in test_scheduling.generate_data(
-            past_failures_data, commit_data, push_num, all_runnables, [], []
+            past_failures_data, commit_data, push_num, all_runnables, tuple(), tuple()
         ):
-            if self.granularity == "label" and not data["name"].startswith("test-"):
-                continue
-
             commit_test = commit_data.copy()
             commit_test["test_job"] = data
             commit_tests.append(commit_test)

commit 656d87f5373f5c105e11e63d9eb9e49c4a247225

Skip tasks not starting with 'test-', not the opposite
Fixes a regression from ed9b2113f9bbe975624783c65eed978c9074aaef

bugbug/models/testselect.py
@@ -206,7 +206,7 @@ class TestSelectModel(Model):
         all_runnables = past_failures_data["all_runnables"]
 
         if self.granularity == "label":
-            all_runnables = tuple(r for r in all_runnables if not r.startswith("test-"))
+            all_runnables = tuple(r for r in all_runnables if r.startswith("test-"))
 
         commit_tests = []
         for data in test_scheduling.generate_data(