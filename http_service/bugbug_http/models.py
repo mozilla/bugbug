mozilla/bugbug

commit b93a56494cf2636132bfb9c2319c5edddcdeedc0

Don't store bugs' last change times forever

http_service/bugbug_http/models.py
@@ -41,12 +41,10 @@ MODEL_CACHE: ReadthroughTTLCache[str, Model] = ReadthroughTTLCache(
 MODEL_CACHE.start_ttl_thread()
 
 
-def setkey(key, value, expiration=DEFAULT_EXPIRATION_TTL):
-    LOGGER.debug(f"Storing data at {key}: {value}")
+def setkey(key: str, value: bytes) -> None:
+    LOGGER.debug(f"Storing data at {key}: {value.decode()}")
     redis.set(key, value)
-
-    if expiration > 0:
-        redis.expire(key, expiration)
+    redis.expire(key, DEFAULT_EXPIRATION_TTL)
 
 
 def classify_bug(model_name, bug_ids, bugzilla_token):
@@ -64,8 +62,7 @@ def classify_bug(model_name, bug_ids, bugzilla_token):
         job = JobInfo(classify_bug, model_name, bug_id)
 
         # TODO: Find a better error format
-        encoded_data = orjson.dumps({"available": False})
-        setkey(job.result_key, encoded_data)
+        setkey(job.result_key, orjson.dumps({"available": False}))
 
     if not bugs:
         return "NOK"
@@ -100,7 +97,7 @@ def classify_bug(model_name, bug_ids, bugzilla_token):
         setkey(job.result_key, orjson.dumps(data))
 
         # Save the bug last change
-        setkey(job.change_time_key, bugs[bug_id]["last_change_time"], expiration=0)
+        setkey(job.change_time_key, bugs[bug_id]["last_change_time"])
 
     return "OK"
 

commit a271b0c7b668d3c650b20bb5f0e4548a674e12e3

Make sure the 'value' parameter of the 'setkey' function is always a bytes
Don't fail if it isn't.

Fixes a regression from b93a56494cf2636132bfb9c2319c5edddcdeedc0

http_service/bugbug_http/models.py
@@ -7,7 +7,7 @@ import logging
 import os
 from datetime import timedelta
 from functools import lru_cache
-from typing import Tuple
+from typing import Iterable, Tuple
 
 import orjson
 import requests
@@ -42,12 +42,12 @@ MODEL_CACHE.start_ttl_thread()
 
 
 def setkey(key: str, value: bytes) -> None:
-    LOGGER.debug(f"Storing data at {key}: {value.decode()}")
+    LOGGER.debug(f"Storing data at {key}: {value!r}")
     redis.set(key, value)
     redis.expire(key, DEFAULT_EXPIRATION_TTL)
 
 
-def classify_bug(model_name, bug_ids, bugzilla_token):
+def classify_bug(model_name: str, bug_ids: Iterable[int], bugzilla_token: str) -> str:
     from bugbug_http.app import JobInfo
 
     # This should be called in a process worker so it should be safe to set
@@ -97,7 +97,7 @@ def classify_bug(model_name, bug_ids, bugzilla_token):
         setkey(job.result_key, orjson.dumps(data))
 
         # Save the bug last change
-        setkey(job.change_time_key, bugs[bug_id]["last_change_time"])
+        setkey(job.change_time_key, bugs[bug_id]["last_change_time"].encode())
 
     return "OK"
 