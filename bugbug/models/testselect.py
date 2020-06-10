mozilla/bugbug

commit 0e68e98a54dd82772d03f6eb7e384bf58fedcf82

Return configs on which to run groups as part of the HTTP service's test scheduling response
Part of #1117

bugbug/models/testselect.py
@@ -269,7 +269,7 @@ class TestSelectModel(Model):
 
     def _generate_equivalence_sets(
         self,
-        tasks: Set[str],
+        tasks: Iterable[str],
         min_redundancy_confidence: float,
         load_failing_together: Callable[[str], Dict[str, Tuple[float, float]]],
         assume_redundant: bool,
@@ -368,7 +368,9 @@ class TestSelectModel(Model):
         elif status == pywraplp.Solver.NOT_SOLVED:
             raise Exception("Problem unsolved")
 
-    def reduce(self, tasks: Set[str], min_redundancy_confidence: float) -> Set[str]:
+    def reduce(
+        self, tasks: Iterable[str], min_redundancy_confidence: float
+    ) -> Set[str]:
         failing_together = test_scheduling.get_failing_together_db(self.granularity)
 
         def load_failing_together(task: str) -> Dict[str, Tuple[float, float]]:
@@ -418,8 +420,8 @@ class TestSelectModel(Model):
         }
 
     def select_configs(
-        self, groups: Set[str], min_redundancy_confidence: float
-    ) -> Set[Tuple[str, str]]:
+        self, groups: Iterable[str], min_redundancy_confidence: float
+    ) -> Dict[str, List[str]]:
         failing_together = test_scheduling.get_failing_together_db("config_group")
 
         all_configs = pickle.loads(failing_together[b"$ALL_CONFIGS$"])
@@ -480,11 +482,15 @@ class TestSelectModel(Model):
 
         self._solve_optimization(solver)
 
-        return {
-            (config, group)
-            for (config, group), config_group_var in config_group_vars.items()
-            if config_group_var.solution_value() == 1
-        }
+        configs_by_group: Dict[str, List[str]] = {}
+        for group in groups:
+            configs_by_group[group] = []
+
+        for (config, group), config_group_var in config_group_vars.items():
+            if config_group_var.solution_value() == 1:
+                configs_by_group[group].append(config)
+
+        return configs_by_group
 
     def evaluation(self) -> None:
         # Get a test set of pushes on which to test the model.

.isort.cfg
@@ -3,4 +3,4 @@ multi_line_output=3
 include_trailing_comma=True
 line_length=88
 known_first_party = bugbug,bugbug_http
-known_third_party = adr,apispec,apispec_webframeworks,boto3,cerberus,dateutil,flask,flask_cors,hglib,hypothesis,igraph,imblearn,joblib,jsone,jsonschema,kombu,libmozdata,lmdb,marshmallow,matplotlib,microannotate,mozci,numpy,orjson,ortools,pandas,pkg_resources,psutil,pyemd,pytest,redis,requests,responses,rq,rs_parsepatch,scipy,setuptools,shap,sklearn,tabulate,taskcluster,tenacity,tqdm,xgboost,yaml,zstandard
+known_third_party = _pytest,adr,apispec,apispec_webframeworks,boto3,cerberus,dateutil,flask,flask_cors,hglib,hypothesis,igraph,imblearn,joblib,jsone,jsonschema,kombu,libmozdata,lmdb,marshmallow,matplotlib,microannotate,mozci,numpy,orjson,ortools,pandas,pkg_resources,psutil,py,pyemd,pytest,redis,requests,responses,rq,rs_parsepatch,scipy,setuptools,shap,sklearn,tabulate,taskcluster,tenacity,tqdm,xgboost,yaml,zstandard

http_service/bugbug_http/models.py
@@ -139,29 +139,32 @@ def schedule_tests(branch: str, rev: str) -> str:
     )
 
     if len(commits) > 0:
-        tasks = MODEL_CACHE.get("testlabelselect").select_tests(
-            commits, test_selection_threshold
-        )
+        testlabelselect_model = MODEL_CACHE.get("testlabelselect")
+        testgroupselect_model = MODEL_CACHE.get("testgroupselect")
 
-        reduced = MODEL_CACHE.get("testlabelselect").reduce(
+        tasks = testlabelselect_model.select_tests(commits, test_selection_threshold)
+
+        reduced = testlabelselect_model.reduce(
             set(t for t, c in tasks.items() if c >= 0.8), 1.0
         )
 
-        reduced_higher = MODEL_CACHE.get("testlabelselect").reduce(
+        reduced_higher = testlabelselect_model.reduce(
             set(t for t, c in tasks.items() if c >= 0.9), 1.0
         )
 
-        groups = MODEL_CACHE.get("testgroupselect").select_tests(
-            commits, test_selection_threshold
-        )
+        groups = testgroupselect_model.select_tests(commits, test_selection_threshold)
+
+        config_groups = testgroupselect_model.select_configs(groups.keys(), 1.0)
     else:
         tasks = {}
         reduced = {}
         groups = {}
+        config_groups = {}
 
     data = {
         "tasks": tasks,
         "groups": groups,
+        "config_groups": config_groups,
         "reduced_tasks": {t: c for t, c in tasks.items() if t in reduced},
         "reduced_tasks_higher": {t: c for t, c in tasks.items() if t in reduced_higher},
         "known_tasks": get_known_tasks(),

http_service/tests/conftest.py
@@ -10,11 +10,14 @@ import pickle
 import re
 from collections import defaultdict
 from datetime import datetime
+from typing import Callable, Dict, Tuple
 
 import hglib
 import numpy as np
+import py.path
 import pytest
 import responses
+from _pytest.monkeypatch import MonkeyPatch
 from rq.exceptions import NoSuchJobError
 
 import bugbug.models
@@ -151,7 +154,7 @@ def add_change_time():
 
 
 @pytest.fixture
-def mock_hgmo(mock_repo):
+def mock_hgmo(mock_repo: Tuple[str, str]) -> None:
     """Mock HGMO API to get patches to apply"""
 
     def fake_json_relevance(request):
@@ -187,7 +190,7 @@ def mock_hgmo(mock_repo):
 
 
 @pytest.fixture
-def mock_repo(tmpdir, monkeypatch):
+def mock_repo(tmpdir: py.path.local, monkeypatch: MonkeyPatch) -> Tuple[str, str]:
     """Create an empty mercurial repo"""
     local_dir = tmpdir / "local"
     remote_dir = tmpdir / "remote"
@@ -245,7 +248,7 @@ def mock_data(tmp_path):
 
 
 @pytest.fixture
-def mock_component_taskcluster_artifact():
+def mock_component_taskcluster_artifact() -> None:
     responses.add(
         responses.HEAD,
         "https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/gecko.v2.mozilla-central.latest.source.source-bugzilla-info/artifacts/public/components.json",
@@ -264,7 +267,9 @@ def mock_component_taskcluster_artifact():
 
 
 @pytest.fixture
-def mock_schedule_tests_classify(monkeypatch):
+def mock_schedule_tests_classify(
+    monkeypatch: MonkeyPatch,
+) -> Callable[[Dict[str, float], Dict[str, float]], None]:
     with open("known_tasks", "w") as f:
         f.write("prova")
 
@@ -287,6 +292,21 @@ def mock_schedule_tests_classify(monkeypatch):
         {"test-windows10/opt": (0.1, 1.0),}
     )
     test_scheduling.close_failing_together_db("label")
+
+    failing_together = test_scheduling.get_failing_together_db("config_group")
+    failing_together[b"$ALL_CONFIGS$"] = pickle.dumps(
+        ["test-linux1804-64/opt", "test-windows10/debug", "test-windows10/opt"]
+    )
+    failing_together[b"test-group1"] = pickle.dumps(
+        {
+            "test-linux1804-64/opt": {
+                "test-windows10/debug": (1.0, 0.0),
+                "test-windows10/opt": (1.0, 1.0),
+            },
+            "test-windows10/debug": {"test-windows10/opt": (1.0, 0.0),},
+        }
+    )
+    test_scheduling.close_failing_together_db("config_group")
 
     def do_mock(labels_to_choose, groups_to_choose):
         # Add a mock test selection model.

http_service/tests/test_schedule_tests.py
@@ -4,6 +4,7 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import json
+from typing import Callable, Dict, List, Tuple
 
 import hglib
 import pytest
@@ -12,46 +13,55 @@ from bugbug_http import models
 
 
 @pytest.mark.parametrize(
-    "labels_to_choose, groups_to_choose, reduced_labels",
+    "labels_to_choose, groups_to_choose, reduced_labels, config_groups",
     [
         # one from label, one from group
         (
             {"test-linux1804-64-opt-label1": 0.9},
             {"test-group2": 0.9},
             {"test-linux1804-64-opt-label1": 0.9},
+            {"test-group2": ["test-linux1804-64/opt"]},
         ),
         # one from label, none from group
         (
             {"test-linux1804-64-opt-label1": 0.9},
             {"test-group2": 0.9},
             {"test-linux1804-64-opt-label1": 0.9},
+            {"test-group2": ["test-linux1804-64/opt"]},
         ),
         # none from label, one from group
-        ({}, {"test-group1": 0.9}, {}),
+        (
+            {},
+            {"test-group1": 0.9},
+            {},
+            {"test-group1": ["test-linux1804-64/opt", "test-windows10/debug"]},
+        ),
         # two from label, one from group
         (
             {"test-linux1804-64-opt-label1": 0.9, "test-linux1804-64-opt-label2": 0.4},
             {"test-group2": 0.9},
             {"test-linux1804-64-opt-label1": 0.9},
+            {"test-group2": ["test-linux1804-64/opt"]},
         ),
         # two redundant from label, one from group
         (
             {"test-linux1804-64/opt": 0.9, "test-windows10/opt": 0.8},
             {"test-group1": 0.9},
             {"test-linux1804-64/opt": 0.9},
+            {"test-group1": ["test-linux1804-64/opt", "test-windows10/debug"]},
         ),
     ],
 )
 def test_simple_schedule(
-    labels_to_choose,
-    groups_to_choose,
-    reduced_labels,
-    monkeypatch,
-    mock_hgmo,
-    mock_repo,
-    mock_component_taskcluster_artifact,
-    mock_schedule_tests_classify,
-):
+    labels_to_choose: Dict[str, float],
+    groups_to_choose: Dict[str, float],
+    reduced_labels: Dict[str, float],
+    config_groups: Dict[str, List[str]],
+    mock_hgmo: None,
+    mock_repo: Tuple[str, str],
+    mock_component_taskcluster_artifact: None,
+    mock_schedule_tests_classify: Callable[[Dict[str, float], Dict[str, float]], None],
+) -> None:
     # The repo should be almost empty at first
     repo_dir, remote_repo_dir = mock_repo
     with hglib.open(str(repo_dir)) as hg:
@@ -91,4 +101,5 @@ def test_simple_schedule(
         "reduced_tasks": reduced_labels,
         "reduced_tasks_higher": reduced_labels,
         "known_tasks": ["prova"],
+        "config_groups": config_groups,
     }

tests/test_testselect.py
@@ -6,6 +6,7 @@
 import itertools
 import math
 import pickle
+from typing import Dict, Iterator, Tuple
 
 import hypothesis
 import hypothesis.strategies as st
@@ -14,21 +15,22 @@ from igraph import Graph
 
 from bugbug import test_scheduling
 from bugbug.models.testselect import TestGroupSelectModel, TestLabelSelectModel
+from bugbug.utils import LMDBDict
 
 
 @pytest.fixture
-def failing_together():
+def failing_together() -> Iterator[LMDBDict]:
     yield test_scheduling.get_failing_together_db("label")
     test_scheduling.close_failing_together_db("label")
 
 
 @pytest.fixture
-def failing_together_config_group():
+def failing_together_config_group() -> Iterator[LMDBDict]:
     yield test_scheduling.get_failing_together_db("config_group")
     test_scheduling.close_failing_together_db("config_group")
 
 
-def test_reduce1(failing_together):
+def test_reduce1(failing_together: LMDBDict) -> None:
     failing_together[b"test-linux1804-64/debug"] = pickle.dumps(
         {
             "test-windows10/debug": (0.1, 1.0),
@@ -77,8 +79,7 @@ def test_reduce1(failing_together):
     }
 
 
-def test_reduce2():
-    failing_together = test_scheduling.get_failing_together_db("label")
+def test_reduce2(failing_together: LMDBDict) -> None:
     failing_together[b"windows10/opt-a"] = pickle.dumps(
         {
             "windows10/opt-b": (0.1, 1.0),
@@ -97,10 +98,8 @@ def test_reduce2():
         1.0,
     ) == {"windows10/opt-b",}
 
-    test_scheduling.close_failing_together_db("label")
 
-
-def test_reduce3(failing_together):
+def test_reduce3(failing_together: LMDBDict) -> None:
     test_scheduling.remove_failing_together_db("label")
     failing_together = test_scheduling.get_failing_together_db("label")
     failing_together[b"windows10/opt-a"] = pickle.dumps(
@@ -130,7 +129,7 @@ def test_reduce3(failing_together):
     )
 
 
-def test_reduce4(failing_together):
+def test_reduce4(failing_together: LMDBDict) -> None:
     failing_together[b"windows10/opt-a"] = pickle.dumps(
         {
             "windows10/opt-b": (0.1, 1.0),
@@ -162,7 +161,7 @@ def test_reduce4(failing_together):
     }
 
 
-def test_reduce5(failing_together):
+def test_reduce5(failing_together: LMDBDict) -> None:
     failing_together[b"linux1804-64/opt-a"] = pickle.dumps(
         {"windows10/opt-d": (0.1, 1.0),}
     )
@@ -179,7 +178,7 @@ def test_reduce5(failing_together):
     }
 
 
-def test_reduce6(failing_together):
+def test_reduce6(failing_together: LMDBDict) -> None:
     failing_together[b"windows10/opt-a"] = pickle.dumps(
         {"windows10/opt-d": (0.1, 1.0),}
     )
@@ -206,7 +205,7 @@ def test_reduce6(failing_together):
 
 
 @st.composite
-def equivalence_graph(draw):
+def equivalence_graph(draw) -> Graph:
     NODES = 7
 
     n = draw(st.integers(min_value=1, max_value=NODES))
@@ -233,14 +232,14 @@ def equivalence_graph(draw):
 @pytest.mark.xfail
 @hypothesis.settings(max_examples=7777)
 @hypothesis.given(g=equivalence_graph())
-def test_all(g):
+def test_all(g: Graph) -> None:
     tasks = [f"windows10/opt-{chr(i)}" for i in range(len(g.vs))]
 
     test_scheduling.close_failing_together_db("label")
     test_scheduling.remove_failing_together_db("label")
 
     # TODO: Also add some couples that are *not* failing together.
-    ft = {}
+    ft: Dict[str, Dict[str, Tuple[float, float]]] = {}
 
     for edge in g.es:
         task1 = tasks[edge.tuple[0]]
@@ -262,7 +261,7 @@ def test_all(g):
     assert len(result) == len(g.components())
 
 
-def test_select_configs(failing_together_config_group):
+def test_select_configs(failing_together_config_group: LMDBDict) -> None:
     failing_together_config_group[b"group1"] = pickle.dumps(
         {
             "linux1804-64-asan/debug": {
@@ -315,10 +314,10 @@ def test_select_configs(failing_together_config_group):
 
     model = TestGroupSelectModel()
     result = model.select_configs({"group1", "group2",}, 1.0,)
-    assert result == {
-        ("linux1804-64-asan/debug", "group1"),
-        ("linux1804-64/opt", "group2"),
-        ("mac/debug", "group2"),
-        ("linux1804-64/opt", "group1"),
-        ("linux1804-64/debug", "group2"),
+    assert len(result) == 2
+    assert set(result["group1"]) == {"linux1804-64-asan/debug", "linux1804-64/opt"}
+    assert set(result["group2"]) == {
+        "linux1804-64/opt",
+        "mac/debug",
+        "linux1804-64/debug",
     }

commit f62caea57b0352f4d52ca29287c950d1efb0c21a

Update TestSelect model evaluation code to take into account the changes to the select_configs return type
Fixes a regression from 0e68e98a54dd82772d03f6eb7e384bf58fedcf82

bugbug/models/testselect.py
@@ -559,7 +559,12 @@ class TestSelectModel(Model):
 
         reductions: List[Optional[float]] = [None, 0.9, 1.0]
 
-        def do_eval(confidence_threshold, reduction, cap, minimum):
+        def do_eval(
+            confidence_threshold: float,
+            reduction: Optional[float],
+            cap: Optional[int],
+            minimum: Optional[int],
+        ) -> None:
             for rev, push in test_pushes.items():
                 selected = set(
                     name
@@ -586,9 +591,9 @@ class TestSelectModel(Model):
                     elif self.granularity == "group":
                         push["number_configs"] = len(
                             set(
-                                config
-                                for config, group in self.select_configs(
-                                    selected, reduction
+                                sum(
+                                    self.select_configs(selected, reduction).values(),
+                                    [],
                                 )
                             )
                         )