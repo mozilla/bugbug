mozilla/bugbug

commit 4e9f29c4da867b5a530934b161c999da4bc45996

Store available configurations by group
Some groups might be running only on a subset of all possible configurations.

bugbug/models/testselect.py
@@ -402,6 +402,7 @@ class TestSelectModel(Model):
                 "config_group", True
             )
             all_configs = pickle.loads(failing_together[b"$ALL_CONFIGS$"])
+            configs_by_group = pickle.loads(failing_together[b"$CONFIGS_BY_GROUP$"])
             for group in all_runnables:
                 key = test_scheduling.failing_together_key(group)
                 try:
@@ -414,8 +415,14 @@ class TestSelectModel(Model):
                 ) -> Dict[str, Tuple[float, float]]:
                     return failing_together_stats[config]
 
+                configs = (
+                    configs_by_group[group]
+                    if group in configs_by_group
+                    else all_configs
+                )
+
                 equivalence_sets[group] = self._generate_equivalence_sets(
-                    all_configs, min_redundancy_confidence, load_failing_together, True
+                    configs, min_redundancy_confidence, load_failing_together, True
                 )
 
             with open(
@@ -500,6 +507,7 @@ class TestSelectModel(Model):
         failing_together = test_scheduling.get_failing_together_db("config_group", True)
 
         all_configs = pickle.loads(failing_together[b"$ALL_CONFIGS$"])
+        all_configs_by_group = pickle.loads(failing_together[b"$CONFIGS_BY_GROUP$"])
         config_costs = {config: self._get_cost(config) for config in all_configs}
 
         solver = pywraplp.Solver(
@@ -509,7 +517,11 @@ class TestSelectModel(Model):
         config_group_vars = {
             (config, group): solver.BoolVar(f"{group}@{config}")
             for group in groups
-            for config in all_configs
+            for config in (
+                all_configs_by_group[group]
+                if group in all_configs_by_group
+                else all_configs
+            )
         }
 
         equivalence_sets = self._get_equivalence_sets(min_redundancy_confidence)

bugbug/test_scheduling.py
@@ -16,6 +16,7 @@ from typing import (
     Any,
     Callable,
     Deque,
+    Dict,
     Generator,
     Iterable,
     Iterator,
@@ -356,6 +357,7 @@ def generate_failing_together_probabilities(
                 count_single_failures[(task1, task2)] += 1
 
     all_available_configs: Set[str] = set()
+    available_configs_by_group: Dict[Group, Set[str]] = collections.defaultdict(set)
 
     for revisions, tasks, likely_regressions, candidate_regressions in tqdm(
         push_data, total=push_data_count
@@ -368,6 +370,8 @@ def generate_failing_together_probabilities(
         # on different configurations, and not between manifests too.
         if granularity == "config_group":
             all_available_configs.update(config for config, group in all_tasks)
+            for config, group in all_tasks:
+                available_configs_by_group[group].add(config)
 
             groups = itertools.groupby(
                 sorted(all_tasks, key=lambda x: x[1]), key=lambda x: x[1]
@@ -479,6 +483,11 @@ def generate_failing_together_probabilities(
     failing_together_db = get_failing_together_db(granularity, False)
 
     failing_together_db[b"$ALL_CONFIGS$"] = pickle.dumps(list(all_available_configs))
+
+    if granularity == "config_group":
+        failing_together_db[b"$CONFIGS_BY_GROUP$"] = pickle.dumps(
+            dict(available_configs_by_group)
+        )
 
     for key, value in failing_together.items():
         failing_together_db[failing_together_key(key)] = pickle.dumps(value)

commit bfa22c74f628fbedcde6a47f9a07761fd47b7881

Store available configs by group in the mock failing together DB created for tests
Fixes a regression from 4e9f29c4da867b5a530934b161c999da4bc45996

tests/test_testselect.py
@@ -313,6 +313,24 @@ def test_select_configs(failing_together_config_group: LMDBDict) -> None:
             "windows10/debug",
         ]
     )
+    failing_together_config_group[b"$CONFIGS_BY_GROUP$"] = pickle.dumps(
+        {
+            "group1": {
+                "linux1804-64-asan/debug",
+                "linux1804-64/debug",
+                "linux1804-64/opt",
+                "mac/debug",
+                "windows10/debug",
+            },
+            "group2": {
+                "linux1804-64-asan/debug",
+                "linux1804-64/debug",
+                "linux1804-64/opt",
+                "mac/debug",
+                "windows10/debug",
+            },
+        }
+    )
     test_scheduling.close_failing_together_db("config_group")
 
     model = TestGroupSelectModel()