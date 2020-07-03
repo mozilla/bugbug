mozilla/bugbug

commit 121922fcd7ac6cb6609acdb22c1797f718567d06

Store the hash of the commit which backed-out or was the bustage-fix for each push
So we can consider all passing tasks until the backout as passing tasks of the push,
rather than just those which run on the push itself.

scripts/test_scheduling_history_retriever.py
@@ -120,7 +120,8 @@ class Retriever(object):
                             runnables = push.config_group_summaries.keys()
 
                         value = (
-                            push.revs,
+                            tuple(push.revs),
+                            push.backedoutby or push.bustage_fixed_by,
                             tuple(runnables),
                             tuple(push.get_possible_regressions(granularity)),
                             tuple(push.get_likely_regressions(granularity)),

bugbug/test_scheduling.py
@@ -42,7 +42,11 @@ Group = NewType("Group", str)
 ConfigGroup = NewType("ConfigGroup", Tuple[str, Group])
 Runnable = Union[Task, Group, ConfigGroup]
 PushResult = Tuple[
-    List[Revision], Tuple[Runnable, ...], Tuple[Runnable, ...], Tuple[Runnable, ...]
+    Tuple[Revision],
+    Revision,
+    Tuple[Runnable, ...],
+    Tuple[Runnable, ...],
+    Tuple[Runnable, ...],
 ]
 
 TEST_LABEL_SCHEDULING_DB = "data/test_label_scheduling_history.pickle"
@@ -182,11 +186,12 @@ def get_push_data(
     push_data = [
         (
             revisions,
+            fix_revision,
             rename_runnables(granularity, push_tasks),
             rename_runnables(granularity, possible_regressions),
             rename_runnables(granularity, likely_regressions),
         )
-        for revisions, push_tasks, possible_regressions, likely_regressions in push_data_queue
+        for revisions, fix_revision, push_tasks, possible_regressions, likely_regressions in push_data_queue
     ]
 
     if granularity == "config_group":
@@ -194,7 +199,7 @@ def get_push_data(
             sum(
                 (
                     [Group(r[1]) for r in push_runnables]
-                    for _, push_runnables, _, _ in push_data
+                    for _, _, push_runnables, _, _ in push_data
                 ),
                 [],
             )
@@ -207,7 +212,7 @@ def get_push_data(
         logger.info(f"{len(all_groups_set)} manifests run in the last 28 pushes")
 
     all_runnables_set = set(
-        sum((list(push_runnables) for _, push_runnables, _, _ in push_data), [])
+        sum((list(push_runnables) for _, _, push_runnables, _, _ in push_data), [])
     )
     # Filter runnables we don't need.
     all_runnables = filter_runnables(
@@ -220,6 +225,7 @@ def get_push_data(
         return (
             (
                 revisions,
+                fix_revision,
                 filter_runnables(
                     rename_runnables(granularity, push_tasks),
                     all_runnables_set,
@@ -236,7 +242,7 @@ def get_push_data(
                     granularity,
                 ),
             )
-            for revisions, push_tasks, possible_regressions, likely_regressions in db.read(
+            for revisions, fix_revision, push_tasks, possible_regressions, likely_regressions in db.read(
                 push_data_db
             )
         )

commit 4693e98b862c17c2b15904d9b9bbf96da46b4c7a

Fix another instance of wrong unpacking of PushResult items
Fixes a regression from 121922fcd7ac6cb6609acdb22c1797f718567d06

scripts/test_scheduling_history_retriever.py
@@ -222,7 +222,13 @@ class Retriever(object):
 
             for (
                 i,
-                (revisions, push_runnables, possible_regressions, likely_regressions),
+                (
+                    revisions,
+                    fix_revision,
+                    push_runnables,
+                    possible_regressions,
+                    likely_regressions,
+                ),
             ) in enumerate(tqdm(push_data_iter(), total=push_data_count)):
                 push_num += 1
 