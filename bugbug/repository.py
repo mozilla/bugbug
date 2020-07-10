mozilla/bugbug

commit e5416ce3116efc409b8d14addef0d8423903d143

Provide a custom Mercurial pull implementation instead of using hgclient's pull
This way we don't capture the command's stdout and stderr and we can see what it's doing.

Needed to investigate #1673

bugbug/repository.py
@@ -1122,18 +1122,17 @@ def clone(repo_dir, url="https://hg.mozilla.org/mozilla-central", update=False):
     logger.info(f"{repo_dir} cloned")
 
 
-def apply_stack(
-    repo_dir: str, stack: List[dict], branch: str, revision: str
-) -> List[bytes]:
-    """Apply a stack of patches on a repository"""
-    assert len(stack) > 0, "Empty stack"
+def pull(repo_dir: str, branch: str, revision: str) -> None:
+    """Pull a revision from a branch of a remote repository into a local repository"""
+    cmd = hglib.util.cmdbuilder(
+        "pull",
+        f"https://hg.mozilla.org/{branch}/".encode("ascii"),
+        r=revision.encode("ascii"),
+    )
 
-    with hglib.open(repo_dir) as hg:
-        hg.pull(
-            source=f"https://hg.mozilla.org/{branch}/".encode("ascii"),
-            rev=revision.encode("ascii"),
-        )
-        return [rev["node"].encode("ascii") for rev in stack]
+    cmd.insert(0, hglib.HGPATH)
+
+    subprocess.run(cmd, check=True, cwd=repo_dir)
 
 
 if __name__ == "__main__":

bugbug/utils.py
@@ -14,7 +14,7 @@ import tarfile
 from collections import deque
 from contextlib import contextmanager
 from functools import lru_cache
-from typing import Any
+from typing import Any, List
 
 import boto3
 import dateutil.parser
@@ -440,7 +440,7 @@ def get_session(name):
     return session
 
 
-def get_hgmo_stack(branch: str, revision: str) -> list:
+def get_hgmo_stack(branch: str, revision: str) -> List[bytes]:
     """Load descriptions of patches in the stack for a given revision"""
     url = f"https://hg.mozilla.org/{branch}/json-automationrelevance/{revision}"
     r = get_session("hgmo").get(url)
@@ -466,7 +466,9 @@ def get_hgmo_stack(branch: str, revision: str) -> list:
 
         return False
 
-    return [c for c in r.json()["changesets"] if not should_skip(c)]
+    return [
+        c["node"].encode("ascii") for c in r.json()["changesets"] if not should_skip(c)
+    ]
 
 
 def get_physical_cpu_count() -> int:

http_service/bugbug_http/models.py
@@ -118,17 +118,17 @@ def schedule_tests(branch: str, rev: str) -> str:
     # Load the full stack of patches leading to that revision
     LOGGER.info("Loading commits to analyze using automationrelevance...")
     try:
-        stack = get_hgmo_stack(branch, rev)
+        revs = get_hgmo_stack(branch, rev)
     except requests.exceptions.RequestException:
         LOGGER.warning(f"Push not found for {branch} @ {rev}!")
         return "NOK"
 
-    # Apply the stack on the local repository
+    # Pull the revision to the local repository
     LOGGER.info("Pulling commits from the remote repository...")
     try:
-        revs = repository.apply_stack(REPO_DIR, stack, branch, rev)
+        repository.pull(REPO_DIR, branch, rev)
     except Exception as e:
-        LOGGER.warning(f"Failed to apply stack {branch} @ {rev}: {e}")
+        LOGGER.warning(f"Failed to pull {branch} @ {rev}: {e}")
         return "NOK"
 
     test_selection_threshold = float(

http_service/tests/conftest.py
@@ -229,14 +229,16 @@ def mock_repo(tmpdir: py.path.local, monkeypatch: MonkeyPatch) -> Tuple[str, str
     # Allow using the local code analysis server.
     responses.add_passthru("http://127.0.0.1")
 
-    orig_hgclient_pull = hglib.client.hgclient.pull
+    orig_hgutil_cmdbuilder = hglib.util.cmdbuilder
 
-    def hglib_pull(hgclient, source=None, rev=None, update=False):
-        orig_hgclient_pull(
-            hgclient, source=str(remote_dir).encode("ascii"), rev=rev, update=update
-        )
+    def hglib_cmdbuilder(name, *args, **kwargs):
+        if name == "pull":
+            args = list(args)
+            args[0] = str(remote_dir).encode("ascii")
 
-    monkeypatch.setattr(hglib.client.hgclient, "pull", hglib_pull)
+        return orig_hgutil_cmdbuilder(name, *args, **kwargs)
+
+    monkeypatch.setattr(hglib.util, "cmdbuilder", hglib_cmdbuilder)
 
     return local_dir, remote_dir
 

commit 4a3459787f1ba03a8c8ffe4631b1353d2b2d8cb8

Recover Mercurial aborted transactions
A transaction might be aborted if a job is killed by rq while we are pulling.

Fixes a regression from e5416ce3116efc409b8d14addef0d8423903d143

I am not sure why, before that, we were not seeing aborted transactions.

bugbug/repository.py
@@ -1082,7 +1082,19 @@ def clean(hg, repo_dir):
             raise
 
 
-def clone(repo_dir, url="https://hg.mozilla.org/mozilla-central", update=False):
+def _run_hg_cmd(repo_dir, cmd, *args, **kwargs):
+    cmd = hglib.util.cmdbuilder(cmd, *args, **kwargs,)
+
+    cmd.insert(0, hglib.HGPATH)
+
+    subprocess.run(cmd, cwd=repo_dir, check=True)
+
+
+def clone(
+    repo_dir: str,
+    url: str = "https://hg.mozilla.org/mozilla-central",
+    update: bool = False,
+) -> None:
     try:
         with hglib.open(repo_dir) as hg:
             clean(hg, repo_dir)
@@ -1101,10 +1113,11 @@ def clone(repo_dir, url="https://hg.mozilla.org/mozilla-central", update=False):
 
         return
     except hglib.error.ServerError as e:
-        if "abort: repository" not in str(e) and b"not found" not in str(e):
+        if "abort: repository" not in str(e) and "not found" not in str(e):
             raise
 
-    cmd = hglib.util.cmdbuilder(
+    _run_hg_cmd(
+        None,
         "robustcheckout",
         url,
         repo_dir,
@@ -1115,24 +1128,25 @@ def clone(repo_dir, url="https://hg.mozilla.org/mozilla-central", update=False):
         noupdate=not update,
     )
 
-    cmd.insert(0, hglib.HGPATH)
-
-    subprocess.run(cmd, check=True)
-
     logger.info(f"{repo_dir} cloned")
 
 
 def pull(repo_dir: str, branch: str, revision: str) -> None:
     """Pull a revision from a branch of a remote repository into a local repository"""
-    cmd = hglib.util.cmdbuilder(
-        "pull",
-        f"https://hg.mozilla.org/{branch}/".encode("ascii"),
-        r=revision.encode("ascii"),
-    )
 
-    cmd.insert(0, hglib.HGPATH)
+    def do_pull() -> None:
+        _run_hg_cmd(
+            repo_dir,
+            "pull",
+            f"https://hg.mozilla.org/{branch}/".encode("ascii"),
+            r=revision.encode("ascii"),
+        )
 
-    subprocess.run(cmd, check=True, cwd=repo_dir)
+    try:
+        do_pull()
+    except subprocess.CalledProcessError:
+        _run_hg_cmd(repo_dir, "recover")
+        do_pull()
 
 
 if __name__ == "__main__":