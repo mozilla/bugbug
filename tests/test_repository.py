mozilla/bugbug

commit b5a194a17e39467cf1405193f9baa99dc9783188

Make the commits mining function accept either a start revision or a set of revisions to analyze
This way for test scheduling we can directly pass the revs and avoid
an additional Mercurial exection.

Ref #1422

bugbug/repository.py
@@ -937,12 +937,17 @@ def get_first_pushdate(repo_dir):
         return hg_log(hg, [b"0"])[0].pushdate
 
 
-def download_commits(repo_dir, rev_start=0, save=True, use_single_process=False):
-    with hglib.open(repo_dir) as hg:
-        revs = get_revs(hg, rev_start)
-        if len(revs) == 0:
-            logger.info("No commits to analyze")
-            return []
+def download_commits(
+    repo_dir, rev_start=None, revs=None, save=True, use_single_process=False
+):
+    assert revs is not None or rev_start is not None
+
+    if revs is None:
+        with hglib.open(repo_dir) as hg:
+            revs = get_revs(hg, rev_start)
+            if len(revs) == 0:
+                logger.info("No commits to analyze")
+                return []
 
     first_pushdate = get_first_pushdate(repo_dir)
 
@@ -1086,8 +1091,8 @@ def apply_stack(repo_dir, stack, branch):
 
         return "tip"
 
-    def first_in_stack():
-        return hg.log(revrange=f"-{len(stack)}")[0].node.decode("utf-8")
+    def stack_nodes():
+        return get_revs(hg, f"-{len(stack)}")
 
     # Start by cleaning the repo, without pulling
     clean(repo_dir, pull=False)
@@ -1103,7 +1108,7 @@ def apply_stack(repo_dir, stack, branch):
         # Apply all the patches in the stack on current base
         if apply_patches(base, patches):
             logger.info(f"Stack applied successfully on {base}")
-            return first_in_stack()
+            return stack_nodes()
 
         # We tried to apply on the valid parent and failed: cannot try another revision
         if base != "tip":
@@ -1122,7 +1127,7 @@ def apply_stack(repo_dir, stack, branch):
 
         logger.info(f"Stack applied successfully on {new_base}")
 
-        return first_in_stack()
+        return stack_nodes()
 
 
 if __name__ == "__main__":

tests/test_repository.py
@@ -435,7 +435,9 @@ def test_download_commits(fake_hg_repo, use_single_process):
     hg.push(dest=bytes(remote, "ascii"))
     copy_pushlog_database(remote, local)
 
-    commits = repository.download_commits(local, use_single_process=use_single_process)
+    commits = repository.download_commits(
+        local, rev_start=0, use_single_process=use_single_process
+    )
     assert len(commits) == 0
     commits = list(repository.get_commits())
     assert len(commits) == 0
@@ -448,7 +450,9 @@ def test_download_commits(fake_hg_repo, use_single_process):
     hg.push(dest=bytes(remote, "ascii"))
     copy_pushlog_database(remote, local)
 
-    commits = repository.download_commits(local, use_single_process=use_single_process)
+    commits = repository.download_commits(
+        local, rev_start=0, use_single_process=use_single_process
+    )
     assert len(commits) == 1
     commits = list(repository.get_commits())
     assert len(commits) == 1
@@ -465,7 +469,7 @@ def test_download_commits(fake_hg_repo, use_single_process):
     copy_pushlog_database(remote, local)
 
     commits = repository.download_commits(
-        local, revision3, use_single_process=use_single_process
+        local, rev_start=revision3, use_single_process=use_single_process
     )
     assert len(commits) == 1
     commits = list(repository.get_commits())
@@ -480,14 +484,26 @@ def test_download_commits(fake_hg_repo, use_single_process):
     os.remove("data/commits.json")
     shutil.rmtree("data/commit_experiences.lmdb")
     commits = repository.download_commits(
-        local, f"children({revision2})", use_single_process=use_single_process
+        local, rev_start=f"children({revision2})", use_single_process=use_single_process
     )
     assert len(commits) == 1
     assert len(list(repository.get_commits())) == 1
 
     os.remove("data/commits.json")
     shutil.rmtree("data/commit_experiences.lmdb")
-    commits = repository.download_commits(local, use_single_process=use_single_process)
+    commits = repository.download_commits(
+        local,
+        revs=[revision2.encode("ascii"), revision3.encode("ascii")],
+        use_single_process=use_single_process,
+    )
+    assert len(commits) == 2
+    assert len(list(repository.get_commits())) == 2
+
+    os.remove("data/commits.json")
+    shutil.rmtree("data/commit_experiences.lmdb")
+    commits = repository.download_commits(
+        local, rev_start=0, use_single_process=use_single_process
+    )
     assert len(list(repository.get_commits())) == 2
 
 

http_service/bugbug_http/models.py
@@ -151,7 +151,7 @@ def schedule_tests(branch, rev):
 
     # Apply the stack on the local repository
     try:
-        first_rev = repository.apply_stack(REPO_DIR, stack, branch)
+        revs = repository.apply_stack(REPO_DIR, stack, branch)
     except Exception as e:
         LOGGER.warning(f"Failed to apply stack {branch} @ {rev}: {e}")
         return "NOK"
@@ -162,7 +162,7 @@ def schedule_tests(branch, rev):
 
     # Analyze patches.
     commits = repository.download_commits(
-        REPO_DIR, rev_start=first_rev, save=False, use_single_process=True
+        REPO_DIR, revs=revs, save=False, use_single_process=True
     )
 
     commit_data = commit_features.merge_commits(commits)

scripts/commit_classifier.py
@@ -225,9 +225,9 @@ class CommitClassifier(object):
         for commit in repository.get_commits():
             pass
 
-        rev_start = "children({})".format(commit["node"])
-
-        repository.download_commits(self.repo_dir, rev_start)
+        repository.download_commits(
+            self.repo_dir, rev_start="children({})".format(commit["node"])
+        )
 
     def has_revision(self, hg, revision):
         if not revision:

scripts/commit_retriever.py
@@ -31,7 +31,7 @@ class Retriever(object):
             for commit in repository.get_commits():
                 rev_start = f"children({commit['node']})"
 
-        repository.download_commits(self.repo_dir, rev_start)
+        repository.download_commits(self.repo_dir, rev_start=rev_start)
 
         logger.info("commit data extracted from repository")
 

commit 5461bb594397008d3c8b82c53b97ec0b2a84a40d

Make download_commits not analyze all commits when the list of commits to analyze is empty
Fixes #1690
Fixes a regression from b5a194a17e39467cf1405193f9baa99dc9783188

bugbug/repository.py
@@ -1005,9 +1005,10 @@ def download_commits(
     with hglib.open(repo_dir) as hg:
         if revs is None:
             revs = get_revs(hg, rev_start)
-            if len(revs) == 0:
-                logger.info("No commits to analyze")
-                return tuple()
+
+        if len(revs) == 0:
+            logger.info("No commits to analyze")
+            return tuple()
 
         first_pushdate = get_first_pushdate(repo_dir)
 

tests/test_repository.py
@@ -557,6 +557,14 @@ def test_download_commits(fake_hg_repo, use_single_process):
     )
     assert len(list(repository.get_commits())) == 2
 
+    os.remove("data/commits.json")
+    shutil.rmtree("data/commit_experiences.lmdb")
+    commits = repository.download_commits(
+        local, revs=[], use_single_process=use_single_process,
+    )
+    assert len(commits) == 0
+    assert len(list(repository.get_commits())) == 0
+
 
 def test_get_directories():
     assert repository.get_directories("") == []