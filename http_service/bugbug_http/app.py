bd68f03e3e89af895297bdcb77eacb578fc9d711
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-17 00:28:03 +0200

    Temporarily allow a longer timeout for jobs
    While #1673 is investigated.

--- http_service/bugbug_http/app.py
+++ http_service/bugbug_http/app.py
@@ -53,8 +53,8 @@ application = Flask(__name__)
 redis_url = os.environ.get("REDIS_URL", "redis://localhost/0")
 redis_conn = Redis.from_url(redis_url)
 
-# Kill jobs which don't finish within 5 minutes.
-JOB_TIMEOUT = 7 * 60
+# Kill jobs which don't finish within 21 minutes.
+JOB_TIMEOUT = 21 * 60
 # Remove jobs from the queue if they haven't started within 5 minutes.
 QUEUE_TIMEOUT = 5 * 60
 # Store the information that a job failed for 30 minutes.
