mozilla/bugbug

commit 3edea4a33e7cb434f93513e7b6d568caf32e1009

Fix usage of known defect/regression labels when identifying bug-fixing commits

bugbug/bugzilla.py
@@ -5,6 +5,7 @@
 
 import csv
 from datetime import datetime
+from typing import NewType
 
 import tenacity
 from dateutil.relativedelta import relativedelta
@@ -12,6 +13,8 @@ from libmozdata.bugzilla import Bugzilla
 from tqdm import tqdm
 
 from bugbug import db, utils
+
+BugID = NewType("BugID", int)
 
 BUGS_DB = "data/bugs.json"
 db.register(

bugbug/models/defect.py
@@ -4,6 +4,7 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import itertools
+from typing import Any, Dict, List, Tuple
 
 import xgboost
 from imblearn.over_sampling import BorderlineSMOTE
@@ -12,6 +13,7 @@ from sklearn.feature_extraction import DictVectorizer
 from sklearn.pipeline import Pipeline
 
 from bugbug import bug_features, bugzilla, feature_cleanup, labels, utils
+from bugbug.bugzilla import BugID
 from bugbug.model import BugModel
 
 
@@ -84,21 +86,21 @@ class DefectModel(BugModel):
         self.clf = xgboost.XGBClassifier(n_jobs=utils.get_physical_cpu_count())
         self.clf.set_params(predictor="cpu_predictor")
 
-    def get_bugbug_labels(self, kind="bug"):
+    def get_bugbug_labels(self, kind="bug") -> Dict[BugID, Any]:
         assert kind in ["bug", "regression", "defect_enhancement_task"]
 
-        classes = {}
+        classes: Dict[BugID, Any] = {}
 
         for bug_id, category in labels.get_labels("bug_nobug"):
             assert category in ["True", "False"], f"unexpected category {category}"
             if kind == "bug":
-                classes[int(bug_id)] = 1 if category == "True" else 0
+                classes[BugID(bug_id)] = 1 if category == "True" else 0
             elif kind == "regression":
                 if category == "False":
-                    classes[int(bug_id)] = 0
+                    classes[BugID(bug_id)] = 0
             elif kind == "defect_enhancement_task":
                 if category == "True":
-                    classes[int(bug_id)] = "defect"
+                    classes[BugID(bug_id)] = "defect"
 
         for bug_id, category in labels.get_labels("regression_bug_nobug"):
             assert category in [
@@ -108,15 +110,15 @@ class DefectModel(BugModel):
                 "regression",
             ], f"unexpected category {category}"
             if kind == "bug":
-                classes[int(bug_id)] = 1 if category != "nobug" else 0
+                classes[BugID(bug_id)] = 1 if category != "nobug" else 0
             elif kind == "regression":
                 if category == "bug_unknown_regression":
                     continue
 
-                classes[int(bug_id)] = 1 if category == "regression" else 0
+                classes[BugID(bug_id)] = 1 if category == "regression" else 0
             elif kind == "defect_enhancement_task":
                 if category != "nobug":
-                    classes[int(bug_id)] = "defect"
+                    classes[BugID(bug_id)] = "defect"
 
         defect_enhancement_task_e = {
             bug_id: category
@@ -160,18 +162,18 @@ class DefectModel(BugModel):
         ):
             assert category in ["defect", "enhancement", "task"]
             if kind == "bug":
-                classes[int(bug_id)] = 1 if category == "defect" else 0
+                classes[BugID(bug_id)] = 1 if category == "defect" else 0
             elif kind == "regression":
                 if category in ["enhancement", "task"]:
-                    classes[int(bug_id)] = 0
+                    classes[BugID(bug_id)] = 0
             elif kind == "defect_enhancement_task":
-                classes[int(bug_id)] = category
+                classes[BugID(bug_id)] = category
 
         # Augment labes by using bugs marked as 'regression' or 'feature', as they are basically labelled.
         # And also use the new bug type field.
         bug_ids = set()
         for bug in bugzilla.get_bugs():
-            bug_id = int(bug["id"])
+            bug_id = BugID(bug["id"])
 
             bug_ids.add(bug_id)
 
@@ -247,7 +249,7 @@ class DefectModel(BugModel):
         # Remove labels which belong to bugs for which we have no data.
         return {bug_id: label for bug_id, label in classes.items() if bug_id in bug_ids}
 
-    def get_labels(self):
+    def get_labels(self) -> Tuple[Dict[BugID, Any], List[Any]]:
         classes = self.get_bugbug_labels("bug")
 
         print("{} bugs".format(sum(1 for label in classes.values() if label == 1)))

bugbug/models/defect_enhancement_task.py
@@ -3,6 +3,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from typing import Any, Dict, List, Tuple
+
+from bugbug.bugzilla import BugID
 from bugbug.models.defect import DefectModel
 
 
@@ -12,7 +15,7 @@ class DefectEnhancementTaskModel(DefectModel):
 
         self.calculate_importance = False
 
-    def get_labels(self):
+    def get_labels(self) -> Tuple[Dict[BugID, Any], List[Any]]:
         classes = self.get_bugbug_labels("defect_enhancement_task")
 
         print(

bugbug/models/regression.py
@@ -3,6 +3,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from typing import Any, Dict, List, Tuple
+
+from bugbug.bugzilla import BugID
 from bugbug.models.defect import DefectModel
 
 
@@ -11,7 +14,7 @@ class RegressionModel(DefectModel):
         DefectModel.__init__(self, lemmatization, historical)
         self.calculate_importance = False
 
-    def get_labels(self):
+    def get_labels(self) -> Tuple[Dict[BugID, Any], List[Any]]:
         classes = self.get_bugbug_labels("regression")
 
         print(

bugbug/model.py
@@ -4,7 +4,7 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from collections import defaultdict
-from typing import Dict, List, Tuple
+from typing import Any, Dict, List, Tuple
 
 import joblib
 import matplotlib
@@ -334,6 +334,10 @@ class Model:
 
     def evaluation(self):
         """Subclasses can implement their own additional evaluation."""
+        pass
+
+    def get_labels(self) -> Tuple[Dict[Any, Any], List[Any]]:
+        """Subclasses implement their own function to gather labels."""
         pass
 
     def train(self, importance_cutoff=0.15, limit=None):

bugbug/utils.py
@@ -32,6 +32,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OrdinalEncoder
 
 from bugbug import get_bugbug_version
+from bugbug.model import Model
 from bugbug.models import get_model_class
 
 logging.basicConfig(level=logging.INFO)
@@ -229,7 +230,7 @@ def download_model(model_name):
     return path
 
 
-def download_and_load_model(model_name):
+def download_and_load_model(model_name: str) -> Model:
     path = download_model(model_name)
     return get_model_class(model_name).load(path)
 

scripts/regressor_finder.py
@@ -12,6 +12,7 @@ import time
 from collections import defaultdict
 from datetime import datetime
 from logging import INFO, basicConfig, getLogger
+from typing import Any, Dict, cast
 
 import dateutil.parser
 import tenacity
@@ -21,6 +22,9 @@ from microannotate import utils as microannotate_utils
 from tqdm import tqdm
 
 from bugbug import bugzilla, db, repository
+from bugbug.bugzilla import BugID
+from bugbug.models.defect_enhancement_task import DefectEnhancementTaskModel
+from bugbug.models.regression import RegressionModel
 from bugbug.models.regressor import (
     BUG_FIXING_COMMITS_DB,
     BUG_INTRODUCING_COMMITS_DB,
@@ -174,7 +178,7 @@ class RegressorFinder(object):
         zstd_compress(IGNORED_COMMITS_DB)
         db.upload(IGNORED_COMMITS_DB)
 
-    def find_bug_fixing_commits(self):
+    def find_bug_fixing_commits(self) -> None:
         logger.info("Downloading commits database...")
         assert db.download(repository.COMMITS_DB)
 
@@ -193,10 +197,12 @@ class RegressorFinder(object):
 
         # TODO: Switch to the pure Defect model, as it's better in this case.
         logger.info("Downloading defect/enhancement/task model...")
-        defect_model = download_and_load_model("defectenhancementtask")
+        defect_model = cast(
+            DefectEnhancementTaskModel, download_and_load_model("defectenhancementtask")
+        )
 
         logger.info("Downloading regression model...")
-        regression_model = download_and_load_model("regression")
+        regression_model = cast(RegressionModel, download_and_load_model("regression"))
 
         start_date = datetime.now() - RELATIVE_START_DATE
         end_date = datetime.now() - RELATIVE_END_DATE
@@ -227,8 +233,8 @@ class RegressorFinder(object):
             f"{bug_count} bugs in total, {len(commit_map) - bug_count} bugs linked to commits missing"
         )
 
-        known_defect_labels = defect_model.get_labels()
-        known_regression_labels = regression_model.get_labels()
+        known_defect_labels: Dict[BugID, Any] = defect_model.get_labels()[0]
+        known_regression_labels: Dict[BugID, Any] = regression_model.get_labels()[0]
 
         bug_fixing_commits = []
 

commit 457b9de2388dca8a22403191eb02571d561d4042

Drop the BugID type, just use ints as bug IDs
BugID would be nicer, but some tests are failing and I'm afraid something else might rely on bug IDs to be "normal" ints.

Fixes a regression from 3edea4a33e7cb434f93513e7b6d568caf32e1009

bugbug/bugzilla.py
@@ -5,7 +5,6 @@
 
 import csv
 from datetime import datetime
-from typing import NewType
 
 import tenacity
 from dateutil.relativedelta import relativedelta
@@ -13,8 +12,6 @@ from libmozdata.bugzilla import Bugzilla
 from tqdm import tqdm
 
 from bugbug import db, utils
-
-BugID = NewType("BugID", int)
 
 BUGS_DB = "data/bugs.json"
 db.register(

bugbug/models/defect.py
@@ -13,7 +13,6 @@ from sklearn.feature_extraction import DictVectorizer
 from sklearn.pipeline import Pipeline
 
 from bugbug import bug_features, bugzilla, feature_cleanup, labels, utils
-from bugbug.bugzilla import BugID
 from bugbug.model import BugModel
 
 
@@ -86,21 +85,21 @@ class DefectModel(BugModel):
         self.clf = xgboost.XGBClassifier(n_jobs=utils.get_physical_cpu_count())
         self.clf.set_params(predictor="cpu_predictor")
 
-    def get_bugbug_labels(self, kind="bug") -> Dict[BugID, Any]:
+    def get_bugbug_labels(self, kind="bug") -> Dict[int, Any]:
         assert kind in ["bug", "regression", "defect_enhancement_task"]
 
-        classes: Dict[BugID, Any] = {}
+        classes: Dict[int, Any] = {}
 
         for bug_id, category in labels.get_labels("bug_nobug"):
             assert category in ["True", "False"], f"unexpected category {category}"
             if kind == "bug":
-                classes[BugID(bug_id)] = 1 if category == "True" else 0
+                classes[int(bug_id)] = 1 if category == "True" else 0
             elif kind == "regression":
                 if category == "False":
-                    classes[BugID(bug_id)] = 0
+                    classes[int(bug_id)] = 0
             elif kind == "defect_enhancement_task":
                 if category == "True":
-                    classes[BugID(bug_id)] = "defect"
+                    classes[int(bug_id)] = "defect"
 
         for bug_id, category in labels.get_labels("regression_bug_nobug"):
             assert category in [
@@ -110,15 +109,15 @@ class DefectModel(BugModel):
                 "regression",
             ], f"unexpected category {category}"
             if kind == "bug":
-                classes[BugID(bug_id)] = 1 if category != "nobug" else 0
+                classes[int(bug_id)] = 1 if category != "nobug" else 0
             elif kind == "regression":
                 if category == "bug_unknown_regression":
                     continue
 
-                classes[BugID(bug_id)] = 1 if category == "regression" else 0
+                classes[int(bug_id)] = 1 if category == "regression" else 0
             elif kind == "defect_enhancement_task":
                 if category != "nobug":
-                    classes[BugID(bug_id)] = "defect"
+                    classes[int(bug_id)] = "defect"
 
         defect_enhancement_task_e = {
             bug_id: category
@@ -162,18 +161,18 @@ class DefectModel(BugModel):
         ):
             assert category in ["defect", "enhancement", "task"]
             if kind == "bug":
-                classes[BugID(bug_id)] = 1 if category == "defect" else 0
+                classes[int(bug_id)] = 1 if category == "defect" else 0
             elif kind == "regression":
                 if category in ["enhancement", "task"]:
-                    classes[BugID(bug_id)] = 0
+                    classes[int(bug_id)] = 0
             elif kind == "defect_enhancement_task":
-                classes[BugID(bug_id)] = category
+                classes[int(bug_id)] = category
 
         # Augment labes by using bugs marked as 'regression' or 'feature', as they are basically labelled.
         # And also use the new bug type field.
         bug_ids = set()
         for bug in bugzilla.get_bugs():
-            bug_id = BugID(bug["id"])
+            bug_id = int(bug["id"])
 
             bug_ids.add(bug_id)
 
@@ -249,7 +248,7 @@ class DefectModel(BugModel):
         # Remove labels which belong to bugs for which we have no data.
         return {bug_id: label for bug_id, label in classes.items() if bug_id in bug_ids}
 
-    def get_labels(self) -> Tuple[Dict[BugID, Any], List[Any]]:
+    def get_labels(self) -> Tuple[Dict[int, Any], List[Any]]:
         classes = self.get_bugbug_labels("bug")
 
         print("{} bugs".format(sum(1 for label in classes.values() if label == 1)))

bugbug/models/defect_enhancement_task.py
@@ -5,7 +5,6 @@
 
 from typing import Any, Dict, List, Tuple
 
-from bugbug.bugzilla import BugID
 from bugbug.models.defect import DefectModel
 
 
@@ -15,7 +14,7 @@ class DefectEnhancementTaskModel(DefectModel):
 
         self.calculate_importance = False
 
-    def get_labels(self) -> Tuple[Dict[BugID, Any], List[Any]]:
+    def get_labels(self) -> Tuple[Dict[int, Any], List[Any]]:
         classes = self.get_bugbug_labels("defect_enhancement_task")
 
         print(

bugbug/models/regression.py
@@ -5,7 +5,6 @@
 
 from typing import Any, Dict, List, Tuple
 
-from bugbug.bugzilla import BugID
 from bugbug.models.defect import DefectModel
 
 
@@ -14,7 +13,7 @@ class RegressionModel(DefectModel):
         DefectModel.__init__(self, lemmatization, historical)
         self.calculate_importance = False
 
-    def get_labels(self) -> Tuple[Dict[BugID, Any], List[Any]]:
+    def get_labels(self) -> Tuple[Dict[int, Any], List[Any]]:
         classes = self.get_bugbug_labels("regression")
 
         print(