mozilla/bugbug

commit 95ce98b0ba9f822313cab62414cda4865317664b

Chunk query to retrieve last change time for bugs to avoid 'Request-URI Too Large' errors

http_service/bugbug_http/app.py
@@ -249,21 +249,22 @@ def is_pending(job):
 
 def get_bugs_last_change_time(bug_ids):
     query = {
-        "id": ",".join(map(str, bug_ids)),
         "include_fields": ["last_change_time", "id"],
     }
     header = {"X-Bugzilla-API-Key": "", "User-Agent": "bugbug"}
-    response = utils.get_session("bugzilla").get(
-        BUGZILLA_API_URL, params=query, headers=header, verify=True, timeout=30
-    )
-    response.raise_for_status()
-
-    raw_bugs = response.json()
 
     bugs = {}
+    for i in range(0, len(bug_ids), libmozdata.Bugzilla.BUGZILLA_CHUNK_SIZE):
+        query["id"] = bug_ids[i : (i + libmozdata.Bugzilla.BUGZILLA_CHUNK_SIZE)]
+        response = utils.get_session("bugzilla").get(
+            BUGZILLA_API_URL, params=query, headers=header, verify=True, timeout=30
+        )
+        response.raise_for_status()
 
-    for bug in raw_bugs["bugs"]:
-        bugs[bug["id"]] = bug["last_change_time"]
+        raw_bugs = response.json()
+
+        for bug in raw_bugs["bugs"]:
+            bugs[bug["id"]] = bug["last_change_time"]
 
     return bugs
 

commit 64323b6c452d77a034445f1aa410dc5d5f85881b

Fix usage of libmozdata.bugzilla module
Bugzilla is a class in libmozdata.bugzilla, not in libmozdata.

Fixes a regression from 95ce98b0ba9f822313cab62414cda4865317664b.

http_service/bugbug_http/app.py
@@ -20,6 +20,7 @@ from apispec_webframeworks.flask import FlaskPlugin
 from cerberus import Validator
 from flask import Flask, Response, jsonify, render_template, request
 from flask_cors import cross_origin
+from libmozdata.bugzilla import Bugzilla
 from marshmallow import Schema, fields
 from redis import Redis
 from rq import Queue
@@ -254,8 +255,8 @@ def get_bugs_last_change_time(bug_ids):
     header = {"X-Bugzilla-API-Key": BUGZILLA_TOKEN, "User-Agent": "bugbug"}
 
     bugs = {}
-    for i in range(0, len(bug_ids), libmozdata.Bugzilla.BUGZILLA_CHUNK_SIZE):
-        query["id"] = bug_ids[i : (i + libmozdata.Bugzilla.BUGZILLA_CHUNK_SIZE)]
+    for i in range(0, len(bug_ids), Bugzilla.BUGZILLA_CHUNK_SIZE):
+        query["id"] = bug_ids[i : (i + Bugzilla.BUGZILLA_CHUNK_SIZE)]
         response = utils.get_session("bugzilla").get(
             BUGZILLA_API_URL, params=query, headers=header, verify=True, timeout=30
         )