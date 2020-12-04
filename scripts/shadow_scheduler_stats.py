c49b0b70103b6270e45aba559c1a4a06d6d30a8f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-04 14:23:45 +0100

    Periodically upload shadow scheduler stats results

--- scripts/shadow_scheduler_stats.py
+++ scripts/shadow_scheduler_stats.py
@@ -6,7 +6,7 @@
 import argparse
 import logging
 from datetime import datetime
-from typing import Iterator, List
+from typing import Iterator
 
 import mozci.push
 from dateutil.relativedelta import relativedelta
@@ -20,43 +20,42 @@ logger = logging.getLogger(__name__)
 SHADOW_SCHEDULER_STATS_DB = "data/shadow_scheduler_stats.json"
 db.register(
     SHADOW_SCHEDULER_STATS_DB,
-    "https://community-tc.services.mozilla.com/api/index/v1/task/project.bugbug.data_bugs.latest/artifacts/public/shadow_scheduler_stats.json.zst",
+    "https://s3-us-west-2.amazonaws.com/communitytc-bugbug/data/shadow_scheduler_stats.json.zst",
     1,
 )
 
 
-def analyze_shadow_schedulers(pushes: List[mozci.push.Push]) -> Iterator[dict]:
-    for push in tqdm(pushes):
-        schedulers = []
+def analyze_shadow_schedulers(push: mozci.push.Push) -> dict:
+    schedulers = []
 
-        group_regressions = push.get_likely_regressions("group")
-        config_group_regressions = push.get_likely_regressions("config_group")
+    group_regressions = push.get_likely_regressions("group")
+    config_group_regressions = push.get_likely_regressions("config_group")
 
-        for name, config_groups in push.generate_all_shadow_scheduler_config_groups():
-            if isinstance(config_groups, mozci.errors.TaskNotFound):
-                continue
+    for name, config_groups in push.generate_all_shadow_scheduler_config_groups():
+        if isinstance(config_groups, mozci.errors.TaskNotFound):
+            continue
 
-            groups = set(group for config, group in config_groups)
+        groups = set(group for config, group in config_groups)
 
-            schedulers.append(
-                {
-                    "name": name,
-                    "num_group_scheduled": len(groups),
-                    "num_group_regressions": len(group_regressions),
-                    "num_group_caught": len(group_regressions & groups),
-                    "num_config_group_scheduled": len(config_groups),
-                    "num_config_group_regressions": len(config_group_regressions),
-                    "num_config_group_caught": len(
-                        config_group_regressions & config_groups
-                    ),
-                }
-            )
+        schedulers.append(
+            {
+                "name": name,
+                "num_group_scheduled": len(groups),
+                "num_group_regressions": len(group_regressions),
+                "num_group_caught": len(group_regressions & groups),
+                "num_config_group_scheduled": len(config_groups),
+                "num_config_group_regressions": len(config_group_regressions),
+                "num_config_group_caught": len(
+                    config_group_regressions & config_groups
+                ),
+            }
+        )
 
-        yield {
-            "id": push.rev,
-            "date": push.date,
-            "schedulers": schedulers,
-        }
+    return {
+        "id": push.rev,
+        "date": push.date,
+        "schedulers": schedulers,
+    }
 
 
 def go(days: int) -> None:
@@ -83,8 +82,20 @@ def go(days: int) -> None:
 
     logger.info(f"{len(pushes)} left to analyze")
 
-    db.append(SHADOW_SCHEDULER_STATS_DB, analyze_shadow_schedulers(pushes))
-    utils.zstd_compress(SHADOW_SCHEDULER_STATS_DB)
+    def compress_and_upload() -> None:
+        utils.zstd_compress(SHADOW_SCHEDULER_STATS_DB)
+        db.upload(SHADOW_SCHEDULER_STATS_DB)
+
+    def results() -> Iterator[dict]:
+        for i, push in enumerate(tqdm(pushes)):
+            yield analyze_shadow_schedulers(push)
+
+            # Upload every 42 pushes.
+            if i % 42 == 0:
+                compress_and_upload()
+
+    db.append(SHADOW_SCHEDULER_STATS_DB, results())
+    compress_and_upload()
 
 
 def main() -> None:
