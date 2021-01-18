1e713c5a58481237f7b8c7618b27069508da33d6
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-01-18 23:03:35 +0100

    Gather shadow scheduler statistics using multiple threads
    And use regressions gathered as part of test selection dataset rather than re-analyzing pushes.

--- scripts/shadow_scheduler_stats.py
+++ scripts/shadow_scheduler_stats.py
@@ -4,16 +4,19 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import argparse
+import concurrent.futures
 import logging
+import time
 import traceback
 from datetime import datetime
-from typing import Iterator
+from typing import Set
 
 import mozci.push
 from dateutil.relativedelta import relativedelta
 from tqdm import tqdm
 
-from bugbug import db, utils
+from bugbug import db, test_scheduling, utils
+from bugbug.test_scheduling import ConfigGroup, Group
 
 logger = logging.getLogger(__name__)
 
@@ -22,15 +25,16 @@ SHADOW_SCHEDULER_STATS_DB = "data/shadow_scheduler_stats.json"
 db.register(
     SHADOW_SCHEDULER_STATS_DB,
     "https://s3-us-west-2.amazonaws.com/communitytc-bugbug/data/shadow_scheduler_stats.json.zst",
-    2,
+    3,
 )
 
 
-def analyze_shadow_schedulers(push: mozci.push.Push) -> dict:
+def analyze_shadow_schedulers(
+    group_regressions: Set[Group],
+    config_group_regressions: Set[ConfigGroup],
+    push: mozci.push.Push,
+) -> dict:
     schedulers = []
-
-    group_regressions = push.get_likely_regressions("group")
-    config_group_regressions = push.get_likely_regressions("config_group")
 
     for name, config_groups in push.generate_all_shadow_scheduler_config_groups():
         if isinstance(config_groups, mozci.errors.TaskNotFound):
@@ -64,12 +68,11 @@ def go(days: int) -> None:
     db.download(SHADOW_SCHEDULER_STATS_DB)
 
     logger.info("Get previously gathered statistics...")
-    prev_scheduler_stat_revs = set(
-        scheduler_stat["id"] for scheduler_stat in db.read(SHADOW_SCHEDULER_STATS_DB)
-    )
-    logger.info(
-        f"Already gathered statistics for {len(prev_scheduler_stat_revs)} pushes..."
-    )
+    scheduler_stats = {
+        scheduler_stat["id"]: scheduler_stat
+        for scheduler_stat in db.read(SHADOW_SCHEDULER_STATS_DB)
+    }
+    logger.info(f"Already gathered statistics for {len(scheduler_stats)} pushes...")
 
     to_date = datetime.utcnow() - relativedelta(days=3)
     from_date = to_date - relativedelta(days=days)
@@ -79,26 +82,79 @@ def go(days: int) -> None:
         branch="autoland",
     )
 
-    pushes = [push for push in pushes if push.rev not in prev_scheduler_stat_revs]
+    pushes = [push for push in pushes if push.rev not in scheduler_stats]
 
     logger.info(f"{len(pushes)} left to analyze")
 
     def compress_and_upload() -> None:
+        for push in pushes:
+            db.write(
+                SHADOW_SCHEDULER_STATS_DB,
+                (
+                    scheduler_stats[push.rev]
+                    for push in pushes
+                    if push.rev in scheduler_stats
+                ),
+            )
+
         utils.zstd_compress(SHADOW_SCHEDULER_STATS_DB)
         db.upload(SHADOW_SCHEDULER_STATS_DB)
 
-    def results() -> Iterator[dict]:
-        for i, push in enumerate(tqdm(pushes)):
-            try:
-                yield analyze_shadow_schedulers(push)
-            except Exception:
-                traceback.print_exc()
+    assert db.download(test_scheduling.PUSH_DATA_GROUP_DB)
+    group_regressions = {}
+    for revisions, _, _, possible_regressions, likely_regressions in db.read(
+        test_scheduling.PUSH_DATA_GROUP_DB
+    ):
+        group_regressions[revisions[0]] = set(likely_regressions)
 
-            # Upload every 42 pushes.
-            if (i + 1) % 42 == 0:
-                compress_and_upload()
+    assert db.download(test_scheduling.PUSH_DATA_CONFIG_GROUP_DB)
+    config_group_regressions = {}
+    for (
+        revisions,
+        _,
+        _,
+        possible_regressions,
+        likely_regressions,
+    ) in db.read(test_scheduling.PUSH_DATA_CONFIG_GROUP_DB):
+        config_group_regressions[revisions[0]] = set(
+            tuple(r) for r in likely_regressions
+        )
 
-    db.append(SHADOW_SCHEDULER_STATS_DB, results())
+    start_time = time.monotonic()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
+        future_to_push = {
+            executor.submit(
+                analyze_shadow_schedulers,
+                group_regressions[push.rev],
+                config_group_regressions[push.rev],
+                push,
+            ): push
+            for push in pushes
+            if push.rev in group_regressions and push.rev in config_group_regressions
+        }
+
+        try:
+            for future in tqdm(
+                concurrent.futures.as_completed(future_to_push),
+                total=len(future_to_push),
+            ):
+                push = future_to_push[future]
+
+                try:
+                    scheduler_stats[push.rev] = future.result()
+                except Exception:
+                    traceback.print_exc()
+
+                # Upload every 60 minutes.
+                if time.monotonic() - start_time >= 3600:
+                    compress_and_upload()
+                    start_time = time.monotonic()
+        except Exception:
+            for f in future_to_push.keys():
+                f.cancel()
+
+            raise
+
     compress_and_upload()
 
 
