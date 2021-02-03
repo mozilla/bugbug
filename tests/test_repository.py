mozilla/bugbug

commit 43e1ad10ee961bdbfc7f0d796f7512ae6c29f9fd

Add diff in complexity metrics to the features (#2088)
Fixes #1190

bugbug/repository.py
@@ -143,15 +143,18 @@ METRIC_NAMES = [
 ]
 
 
+def get_total_metrics_dict() -> dict:
+    return {f"{metric}_total": 0 for metric in METRIC_NAMES}
+
+
 def get_metrics_dict() -> dict:
-    metrics = {}
+    metrics = get_total_metrics_dict()
     for metric in METRIC_NAMES:
         metrics.update(
             {
                 f"{metric}_avg": 0.0,
                 f"{metric}_max": 0,
                 f"{metric}_min": sys.maxsize,
-                f"{metric}_total": 0,
             }
         )
     return metrics
@@ -206,6 +209,7 @@ class Commit:
         self.minimum_test_file_size = 0
         self.test_files_modified_num = 0
         self.metrics = get_metrics_dict()
+        self.metrics_diff = get_total_metrics_dict()
 
     def __eq__(self, other):
         assert isinstance(other, Commit)
@@ -504,6 +508,63 @@ def get_space_metrics(
             get_summary_metrics(obj, space)
 
 
+def set_commit_metrics(
+    commit: Commit,
+    path: str,
+    deleted_lines: List[int],
+    added_lines: List[int],
+    before_metrics: dict,
+    after_metrics: dict,
+) -> None:
+    try:
+        get_space_metrics(commit.metrics, after_metrics["spaces"])
+    except AnalysisException:
+        logger.debug(f"rust-code-analysis error on commit {commit.node}, path {path}")
+
+    before_metrics_dict = get_total_metrics_dict()
+    try:
+        get_space_metrics(
+            before_metrics_dict, before_metrics["spaces"], calc_summaries=False
+        )
+    except AnalysisException:
+        logger.debug(f"rust-code-analysis error on commit {commit.node}, path {path}")
+
+    commit.metrics_diff = {
+        f"{metric}_total": commit.metrics[f"{metric}_total"]
+        - before_metrics_dict[f"{metric}_total"]
+        for metric in METRIC_NAMES
+    }
+
+    touched_functions = get_touched_functions(
+        after_metrics["spaces"],
+        deleted_lines,
+        added_lines,
+    )
+    if len(touched_functions) > 0:
+        return
+
+    commit.functions[path] = []
+
+    for func in touched_functions:
+        metrics_dict = get_total_metrics_dict()
+
+        try:
+            get_space_metrics(metrics_dict, func, calc_summaries=False)
+        except AnalysisException:
+            logger.debug(
+                f"rust-code-analysis error on commit {commit.node}, path {path}, function {func['name']}"
+            )
+
+        commit.functions[path].append(
+            {
+                "name": func["name"],
+                "start": func["start_line"],
+                "end": func["end_line"],
+                "metrics": metrics_dict.items(),
+            }
+        )
+
+
 def transform(hg: hglib.client, repo_dir: str, commit: Commit):
     hg_modified_files(hg, commit)
 
@@ -568,52 +629,41 @@ def transform(hg: hglib.client, repo_dir: str, commit: Commit):
                 source_code_sizes.append(size)
 
                 if type_ != "IDL/IPDL/WebIDL":
-                    metrics = code_analysis_server.metrics(path, after, unit=False)
-                    if metrics.get("spaces"):
+                    after_metrics = code_analysis_server.metrics(
+                        path, after, unit=False
+                    )
+                    if after_metrics.get("spaces"):
                         metrics_file_count += 1
 
-                        try:
-                            get_space_metrics(commit.metrics, metrics["spaces"])
-                        except AnalysisException:
-                            logger.debug(
-                                f"rust-code-analysis error on commit {commit.node}, path {path}"
-                            )
+                        before = None
+                        if not stats["new"]:
+                            try:
+                                before = hg.cat(
+                                    [os.path.join(repo_dir, path).encode("utf-8")],
+                                    rev=commit.node.encode("ascii"),
+                                )
+                            except hglib.error.CommandError as e:
+                                if b"no such file in rev" not in e.err:
+                                    raise
 
-                        touched_functions = get_touched_functions(
-                            metrics["spaces"],
+                        before_metrics = code_analysis_server.metrics(
+                            path, before, unit=False
+                        )
+
+                        set_commit_metrics(
+                            commit,
+                            path,
                             stats["deleted_lines"],
                             stats["added_lines"],
+                            before_metrics,
+                            after_metrics,
                         )
-                        if len(touched_functions) > 0:
-                            commit.functions[path] = []
-
-                            for func in touched_functions:
-                                metrics_dict = get_metrics_dict()
-
-                                try:
-                                    get_space_metrics(
-                                        metrics_dict, func, calc_summaries=False
-                                    )
-                                except AnalysisException:
-                                    logger.debug(
-                                        f"rust-code-analysis error on commit {commit.node}, path {path}, function {func['name']}"
-                                    )
-
-                                commit.functions[path].append(
-                                    {
-                                        "name": func["name"],
-                                        "start": func["start_line"],
-                                        "end": func["end_line"],
-                                        "metrics": {
-                                            key: value
-                                            for key, value in metrics_dict.items()
-                                            if key.endswith("_total")
-                                        },
-                                    }
-                                )
 
                     # Replace type with "Objective-C/C++" if rust-code-analysis detected this is an Objective-C/C++ file.
-                    if type_ == "C/C++" and metrics.get("language") == "obj-c/c++":
+                    if (
+                        type_ == "C/C++"
+                        and after_metrics.get("language") == "obj-c/c++"
+                    ):
                         type_ = "Objective-C/C++"
 
             commit.types.add(type_)

tests/test_repository.py
@@ -2291,11 +2291,14 @@ def test_commit_dict_matches_expected():
     commit_features.source_code_file_metrics()(commit)
     commit_features.source_code_function_metrics()(commit)
     commit_features.functions_touched_size()(commit)
+    commit_features.source_code_metrics_diff()(commit)
 
     merged_commit = commit_features.merge_commits([commit, commit])
     commit_features.source_code_file_metrics()(merged_commit)
     # TODO: Support merging functions in commit_features.merge_commits
     # commit_features.source_code_function_metrics()(merged_commit)
+    # TODO: Support merging diff metrics in commit_features.merge_commits
+    # commit_features.source_code_metrics_diff()(commit)
 
     commit = repository.Commit(
         node="commit",
@@ -2341,3 +2344,5 @@ def test_commit_dict_matches_expected():
     commit_features.source_code_file_metrics()(merged_commit)
     # TODO: Support merging functions in commit_features.merge_commits
     # commit_features.source_code_function_metrics()(merged_commit)
+    # TODO: Support merging diff metrics in commit_features.merge_commits
+    # commit_features.source_code_metrics_diff()(commit)

bugbug/commit_features.py
@@ -332,6 +332,34 @@ class source_code_function_metrics(object):
         }
 
 
+class source_code_metrics_diff(object):
+    name = "diff in metrics on source code"
+
+    def __call__(self, commit, **kwargs):
+        return {
+            "Diff in cyclomatic": commit["metrics_diff"]["cyclomatic_total"],
+            "Diff in number of unique operands": commit["metrics_diff"][
+                "halstead_n2_total"
+            ],
+            "Diff in number of operands": commit["metrics_diff"]["halstead_N2_total"],
+            "Diff in number of unique operators": commit["metrics_diff"][
+                "halstead_n1_total"
+            ],
+            "Diff in number of operators": commit["metrics_diff"]["halstead_N1_total"],
+            "Diff in number of source loc": commit["metrics_diff"]["sloc_total"],
+            "Diff in number of instruction loc": commit["metrics_diff"]["ploc_total"],
+            "Diff in number of logical loc": commit["metrics_diff"]["lloc_total"],
+            "Diff in number of comment loc": commit["metrics_diff"]["cloc_total"],
+            "Diff in number of function arguments": commit["metrics_diff"][
+                "nargs_total"
+            ],
+            "Diff in number of function exit points": commit["metrics_diff"][
+                "nexits_total"
+            ],
+            "Diff in cognitive": commit["metrics_diff"]["cognitive_total"],
+        }
+
+
 def get_exps(exp_type, commit):
     items_key = f"{exp_type}s" if exp_type != "directory" else "directories"
     items_num = len(commit[items_key])

commit fc2c37849db9087aa48febc487eab62477bdf6bb

Bring back storing functions touched by commits
Fixes a regression from 43e1ad10ee961bdbfc7f0d796f7512ae6c29f9fd

.pre-commit-config.yaml
@@ -37,6 +37,7 @@ repos:
       - id: check-symlinks
       - id: debug-statements
       - id: trailing-whitespace
+        exclude: ^tests/test_repository.py
       - id: check-yaml
       - id: mixed-line-ending
       - id: name-tests-test

bugbug/repository.py
@@ -541,7 +541,7 @@ def set_commit_metrics(
         deleted_lines,
         added_lines,
     )
-    if len(touched_functions) > 0:
+    if len(touched_functions) == 0:
         return
 
     commit.functions[path] = []
@@ -561,7 +561,7 @@ def set_commit_metrics(
                 "name": func["name"],
                 "start": func["start_line"],
                 "end": func["end_line"],
-                "metrics": metrics_dict.items(),
+                "metrics": metrics_dict,
             }
         )
 

tests/test_repository.py
@@ -13,6 +13,7 @@ from datetime import datetime, timezone
 import hglib
 import pytest
 import responses
+import rs_parsepatch
 import zstandard
 from dateutil.relativedelta import relativedelta
 
@@ -2346,3 +2347,312 @@ def test_commit_dict_matches_expected():
     # commit_features.source_code_function_metrics()(merged_commit)
     # TODO: Support merging diff metrics in commit_features.merge_commits
     # commit_features.source_code_metrics_diff()(commit)
+
+
+def test_set_commit_metrics():
+    # Allow using the local code analysis server.
+    responses.add_passthru("http://127.0.0.1")
+
+    commit = repository.Commit(
+        node="commit",
+        author="author",
+        desc="commit",
+        pushdate=datetime(2021, 1, 1),
+        bug_id=123,
+        backsout=[],
+        backedoutby="",
+        author_email="author@mozilla.org",
+        reviewers=["reviewer"],
+    ).set_files([], {})
+
+    before = """#include <iostream>
+
+void main() {
+    cout << "Ciao1" << endl;
+}
+"""
+
+    after = """#include <iostream>
+
+void main() {
+    cout << "Ciao2" << endl;
+}
+"""
+
+    patch = """--- prova.cpp	2021-02-03 11:32:47.398531225 +0100
++++ prova.cpp	2021-02-03 11:32:45.114531901 +0100
+@@ -1,6 +1,6 @@
+ #include <iostream>
+ 
+ void main() {
+-    cout << "Ciao1" << endl;
++    cout << "Ciao2" << endl;
+ }
+ """
+
+    patch_data = rs_parsepatch.get_lines(patch)
+
+    print(patch_data)
+
+    assert len(patch_data) == 1
+
+    code_analysis_server = rust_code_analysis_server.RustCodeAnalysisServer()
+
+    stats = patch_data[0]
+
+    path = stats["filename"]
+
+    after_metrics = code_analysis_server.metrics(path, after, unit=False)
+    before_metrics = code_analysis_server.metrics(path, before, unit=False)
+
+    repository.set_commit_metrics(
+        commit,
+        "prova.cpp",
+        stats["deleted_lines"],
+        stats["added_lines"],
+        before_metrics,
+        after_metrics,
+    )
+
+    assert commit.metrics == {
+        "cloc_avg": 0.0,
+        "cloc_max": 0,
+        "cloc_min": 0.0,
+        "cloc_total": 0.0,
+        "cognitive_avg": 0.0,
+        "cognitive_max": 0,
+        "cognitive_min": 0.0,
+        "cognitive_total": 0.0,
+        "cyclomatic_avg": 0.0,
+        "cyclomatic_max": 1.0,
+        "cyclomatic_min": 1.0,
+        "cyclomatic_total": 2.0,
+        "halstead_N1_avg": 0.0,
+        "halstead_N1_max": 6.0,
+        "halstead_N1_min": 6.0,
+        "halstead_N1_total": 6.0,
+        "halstead_N2_avg": 0.0,
+        "halstead_N2_max": 4.0,
+        "halstead_N2_min": 4.0,
+        "halstead_N2_total": 4.0,
+        "halstead_n1_avg": 0.0,
+        "halstead_n1_max": 5.0,
+        "halstead_n1_min": 5.0,
+        "halstead_n1_total": 5.0,
+        "halstead_n2_avg": 0.0,
+        "halstead_n2_max": 4.0,
+        "halstead_n2_min": 4.0,
+        "halstead_n2_total": 4.0,
+        "lloc_avg": 0.0,
+        "lloc_max": 1.0,
+        "lloc_min": 1.0,
+        "lloc_total": 1.0,
+        "nargs_avg": 0.0,
+        "nargs_max": 0,
+        "nargs_min": 0.0,
+        "nargs_total": 0.0,
+        "nexits_avg": 0.0,
+        "nexits_max": 0,
+        "nexits_min": 0.0,
+        "nexits_total": 0.0,
+        "ploc_avg": 0.0,
+        "ploc_max": 3.0,
+        "ploc_min": 3.0,
+        "ploc_total": 4.0,
+        "sloc_avg": 0.0,
+        "sloc_max": 3.0,
+        "sloc_min": 3.0,
+        "sloc_total": 5.0,
+    }
+    assert commit.metrics_diff == {
+        "cloc_total": 0.0,
+        "cognitive_total": 0.0,
+        "cyclomatic_total": 0.0,
+        "halstead_N1_total": 0.0,
+        "halstead_N2_total": 0.0,
+        "halstead_n1_total": 0.0,
+        "halstead_n2_total": 0.0,
+        "lloc_total": 0.0,
+        "nargs_total": 0.0,
+        "nexits_total": 0.0,
+        "ploc_total": 0.0,
+        "sloc_total": 0.0,
+    }
+    assert commit.functions == {
+        "prova.cpp": [
+            {
+                "end": 5,
+                "metrics": {
+                    "cloc_total": 0.0,
+                    "cognitive_total": 0.0,
+                    "cyclomatic_total": 1.0,
+                    "halstead_N1_total": 6.0,
+                    "halstead_N2_total": 4.0,
+                    "halstead_n1_total": 5.0,
+                    "halstead_n2_total": 4.0,
+                    "lloc_total": 1.0,
+                    "nargs_total": 0.0,
+                    "nexits_total": 0.0,
+                    "ploc_total": 3.0,
+                    "sloc_total": 3.0,
+                },
+                "name": "main",
+                "start": 3,
+            }
+        ]
+    }
+
+
+def test_set_commit_metrics_meaningful_diff():
+    # Allow using the local code analysis server.
+    responses.add_passthru("http://127.0.0.1")
+
+    commit = repository.Commit(
+        node="commit",
+        author="author",
+        desc="commit",
+        pushdate=datetime(2021, 1, 1),
+        bug_id=123,
+        backsout=[],
+        backedoutby="",
+        author_email="author@mozilla.org",
+        reviewers=["reviewer"],
+    ).set_files([], {})
+
+    before = """#include <iostream>
+
+void main() {
+    cout << "Ciao1" << endl;
+}
+"""
+
+    after = """#include <iostream>
+
+void main() {
+    cout << "Ciao1" << endl;
+    cout << "Ciao2" << endl;
+}
+"""
+
+    patch = """--- prova.cpp	2021-02-03 11:32:47.398531225 +0100
++++ prova.cpp	2021-02-03 11:32:45.114531901 +0100
+@@ -1,6 +1,6 @@
+ #include <iostream>
+ 
+ void main() {
+     cout << "Ciao1" << endl;
++    cout << "Ciao2" << endl;
+ }
+ """
+
+    patch_data = rs_parsepatch.get_lines(patch)
+
+    print(patch_data)
+
+    assert len(patch_data) == 1
+
+    code_analysis_server = rust_code_analysis_server.RustCodeAnalysisServer()
+
+    stats = patch_data[0]
+
+    path = stats["filename"]
+
+    after_metrics = code_analysis_server.metrics(path, after, unit=False)
+    before_metrics = code_analysis_server.metrics(path, before, unit=False)
+
+    repository.set_commit_metrics(
+        commit,
+        "prova.cpp",
+        stats["deleted_lines"],
+        stats["added_lines"],
+        before_metrics,
+        after_metrics,
+    )
+
+    assert commit.metrics == {
+        "cloc_avg": 0.0,
+        "cloc_max": 0,
+        "cloc_min": 0.0,
+        "cloc_total": 0.0,
+        "cognitive_avg": 0.0,
+        "cognitive_max": 0,
+        "cognitive_min": 0.0,
+        "cognitive_total": 0.0,
+        "cyclomatic_avg": 0.0,
+        "cyclomatic_max": 1.0,
+        "cyclomatic_min": 1.0,
+        "cyclomatic_total": 2.0,
+        "halstead_N1_avg": 0.0,
+        "halstead_N1_max": 9.0,
+        "halstead_N1_min": 9.0,
+        "halstead_N1_total": 9.0,
+        "halstead_N2_avg": 0.0,
+        "halstead_N2_max": 7.0,
+        "halstead_N2_min": 7.0,
+        "halstead_N2_total": 7.0,
+        "halstead_n1_avg": 0.0,
+        "halstead_n1_max": 5.0,
+        "halstead_n1_min": 5.0,
+        "halstead_n1_total": 5.0,
+        "halstead_n2_avg": 0.0,
+        "halstead_n2_max": 5.0,
+        "halstead_n2_min": 5.0,
+        "halstead_n2_total": 5.0,
+        "lloc_avg": 0.0,
+        "lloc_max": 2.0,
+        "lloc_min": 2.0,
+        "lloc_total": 2.0,
+        "nargs_avg": 0.0,
+        "nargs_max": 0,
+        "nargs_min": 0.0,
+        "nargs_total": 0.0,
+        "nexits_avg": 0.0,
+        "nexits_max": 0,
+        "nexits_min": 0.0,
+        "nexits_total": 0.0,
+        "ploc_avg": 0.0,
+        "ploc_max": 4.0,
+        "ploc_min": 4.0,
+        "ploc_total": 5.0,
+        "sloc_avg": 0.0,
+        "sloc_max": 4.0,
+        "sloc_min": 4.0,
+        "sloc_total": 6.0,
+    }
+    assert commit.metrics_diff == {
+        "cloc_total": 0.0,
+        "cognitive_total": 0.0,
+        "cyclomatic_total": 0.0,
+        "halstead_N1_total": 3.0,
+        "halstead_N2_total": 3.0,
+        "halstead_n1_total": 0.0,
+        "halstead_n2_total": 1.0,
+        "lloc_total": 1.0,
+        "nargs_total": 0.0,
+        "nexits_total": 0.0,
+        "ploc_total": 1.0,
+        "sloc_total": 1.0,
+    }
+    assert commit.functions == {
+        "prova.cpp": [
+            {
+                "end": 6,
+                "metrics": {
+                    "cloc_total": 0.0,
+                    "cognitive_total": 0.0,
+                    "cyclomatic_total": 1.0,
+                    "halstead_N1_total": 9.0,
+                    "halstead_N2_total": 7.0,
+                    "halstead_n1_total": 5.0,
+                    "halstead_n2_total": 5.0,
+                    "lloc_total": 2.0,
+                    "nargs_total": 0.0,
+                    "nexits_total": 0.0,
+                    "ploc_total": 4.0,
+                    "sloc_total": 4.0,
+                },
+                "name": "main",
+                "start": 3,
+            }
+        ]
+    }