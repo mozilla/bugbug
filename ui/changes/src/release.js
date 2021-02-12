bde4ec860071d3e8c1aec0d159d35fa4a042a6a4
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-12 17:16:59 +0100

    Show components which might be most affected by changes in a given release
    Fixes #2078

--- ui/changes/src/release.js
+++ ui/changes/src/release.js
@@ -6,9 +6,38 @@ import {
   setupOptions,
   getOption,
   getFilteredBugSummaries,
+  getComponentRegressionMap,
 } from "./common.js";
 
 let resultGraphs = document.getElementById("result-graphs");
+
+async function renderTreemap(chartEl, title, counter) {
+  let data = Object.entries(counter)
+    .filter(([name, count]) => count > 5)
+    .map(([name, count]) => {
+      return { x: name, y: count };
+    });
+
+  let options = {
+    series: [
+      {
+        data: data,
+      },
+    ],
+    legend: {
+      show: false,
+    },
+    chart: {
+      type: "treemap",
+    },
+    title: {
+      text: title,
+    },
+  };
+
+  let chart = new ApexCharts(chartEl, options);
+  chart.render();
+}
 
 async function renderComponentChangesChart(chartEl, bugSummaries) {
   // Only show fixed bugs.
@@ -25,31 +54,42 @@ async function renderComponentChangesChart(chartEl, bugSummaries) {
     componentCounter[bugSummary[dimension]] += 1;
   }
 
-  let data = Object.entries(componentCounter)
-    .filter(([component, count]) => count > 5)
-    .map(([component, count]) => {
-      return { x: component, y: count };
-    });
+  renderTreemap(
+    chartEl,
+    `${dimension.charAt(0).toUpperCase()}${dimension.slice(1)} changes`,
+    componentCounter
+  );
+}
 
-  let options = {
-    series: [
-      {
-        data: data,
-      },
-    ],
-    legend: {
-      show: false,
-    },
-    chart: {
-      type: "treemap",
-    },
-    title: {
-      text: `${dimension.charAt(0).toUpperCase()}${dimension.slice(1)} changes`,
-    },
-  };
+async function renderAffectedComponentChangesChart(chartEl, bugSummaries) {
+  // Only consider fixed bugs.
+  bugSummaries = bugSummaries.filter((bugSummary) => bugSummary.date !== null);
 
-  let chart = new ApexCharts(chartEl, options);
-  chart.render();
+  if (bugSummaries.length == 0) {
+    return;
+  }
+
+  let componentCounter = new Counter();
+  for (let bugSummary of bugSummaries) {
+    componentCounter[bugSummary["component"]] += 1;
+  }
+
+  let componentConnectionMap = await getComponentRegressionMap();
+
+  let affectedComponentCounter = new Counter();
+  for (let [sourceComponent, count] of Object.entries(componentCounter)) {
+    if (!componentConnectionMap.hasOwnProperty(sourceComponent)) {
+      continue;
+    }
+
+    for (let [targetComponent, percentage] of Object.entries(
+      componentConnectionMap[sourceComponent]
+    )) {
+      affectedComponentCounter[targetComponent] += count * percentage;
+    }
+  }
+
+  renderTreemap(chartEl, "Most affected components", affectedComponentCounter);
 }
 
 async function renderUI() {
@@ -60,6 +100,13 @@ async function renderUI() {
   let componentChangesChartEl = document.createElement("div");
   resultGraphs.append(componentChangesChartEl);
   await renderComponentChangesChart(componentChangesChartEl, bugSummaries);
+
+  let affectedComponentChangesChartEl = document.createElement("div");
+  resultGraphs.append(affectedComponentChangesChartEl);
+  await renderAffectedComponentChangesChart(
+    affectedComponentChangesChartEl,
+    bugSummaries
+  );
 }
 
 (async function init() {
