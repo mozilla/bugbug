37f402794b0230d92cb0576a1784c91ac53c9e5c
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-26 12:20:27 +0100

    Move the dependency heatmap rendering code in common.js
    Fixes #2147

--- ui/changes/src/common.js
+++ ui/changes/src/common.js
@@ -1120,6 +1120,50 @@ export async function renderTreemap(chartEl, title, counter, minimumCount = 5) {
   chart.render();
 }
 
+export async function renderDependencyHeatmap(chartEl) {
+  const map = await getComponentRegressionMap();
+  const series = Object.keys(map[Object.keys(map)[0]]).map((element) => {
+    let values = {};
+    values.data = [];
+    values.name = element;
+    for (const value in map[element]) {
+      let obj = {};
+      obj.x = value;
+      obj.y = Math.trunc(map[element][value] * 100);
+      values.data.push(obj);
+    }
+    return values;
+  });
+
+  const options = {
+    series: [...series],
+    chart: {
+      height: 700,
+      width: 1200,
+      type: "heatmap",
+      animations: {
+        enabled: false,
+      },
+    },
+    plotOptions: {
+      heatmap: {
+        radius: 30,
+        enableShades: true,
+      },
+    },
+    dataLabels: {
+      enabled: true,
+    },
+    colors: ["#008FFB"],
+    xaxis: {
+      type: "category",
+    },
+  };
+
+  const chart = new ApexCharts(chartEl, options);
+  chart.render();
+}
+
 export function summarizeCoverage(bugSummary) {
   let lines_added = 0;
   let lines_covered = 0;
--- ui/changes/src/graph.js
+++ ui/changes/src/graph.js
@@ -1,57 +1,7 @@
-import ApexCharts from "apexcharts";
-
-import { getComponentRegressionMap } from "./common";
-
-async function generateData() {
-  let map = await getComponentRegressionMap();
-  return Object.keys(map[Object.keys(map)[0]]).map((element) => {
-    let values = {};
-    values.data = [];
-    values.name = element;
-    for (const value in map[element]) {
-      let obj = {};
-      obj.x = value;
-      obj.y = Math.trunc(map[element][value] * 100);
-      values.data.push(obj);
-    }
-    return values;
-  });
-}
+import * as common from "./common";
 
 async function rerender(connections) {
-  var options = {
-    series: [...(await generateData())],
-    chart: {
-      height: 700,
-      width: 1200,
-      type: "heatmap",
-      animations: {
-        enabled: false,
-      },
-    },
-    stroke: {
-      width: 1,
-    },
-    plotOptions: {
-      heatmap: {
-        radius: 30,
-        enableShades: true,
-      },
-    },
-    dataLabels: {
-      enabled: true,
-      style: {
-        colors: ["#fff"],
-      },
-    },
-    colors: ["#008FFB"],
-    xaxis: {
-      type: "category",
-    },
-  };
-
-  var chart = new ApexCharts(document.querySelector("#chart"), options);
-  chart.render();
+  common.renderDependencyHeatmap(document.querySelector("#chart"));
 
   document.querySelector("#component-source").textContent = JSON.stringify(
     connections,
@@ -61,7 +11,7 @@ async function rerender(connections) {
 }
 
 (async function () {
-  let connectionsMap = await getComponentRegressionMap();
+  let connectionsMap = await common.getComponentRegressionMap();
   console.log(connectionsMap);
   await rerender(connectionsMap);
 })();
