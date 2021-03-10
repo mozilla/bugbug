mozilla/bugbug

commit 2d7c53f2762c83536979500f82e2be4e8202fbb0

Add Mi metric (#2207)
Fixes #1448.

bugbug/repository.py
@@ -140,6 +140,9 @@ METRIC_NAMES = [
     "nargs",
     "nexits",
     "cognitive",
+    "mi_original",
+    "mi_sei",
+    "mi_visual_studio",
 ]
 
 
@@ -463,6 +466,13 @@ def get_summary_metrics(obj, metrics_space):
         obj["nargs_max"] = max(obj["nargs_max"], metrics["nargs"]["sum"])
         obj["nexits_max"] = max(obj["nexits_max"], metrics["nexits"]["sum"])
         obj["cognitive_max"] = max(obj["cognitive_max"], metrics["cognitive"]["sum"])
+        obj["mi_original_max"] = max(
+            obj["mi_original_max"], metrics["mi"]["mi_original"]
+        )
+        obj["mi_sei_max"] = max(obj["mi_sei_max"], metrics["mi"]["mi_sei"])
+        obj["mi_visual_studio_max"] = max(
+            obj["mi_visual_studio_max"], metrics["mi"]["mi_visual_studio"]
+        )
 
         obj["cyclomatic_min"] = min(obj["cyclomatic_min"], metrics["cyclomatic"]["sum"])
         obj["halstead_n2_min"] = min(obj["halstead_n2_min"], metrics["halstead"]["n2"])
@@ -476,6 +486,13 @@ def get_summary_metrics(obj, metrics_space):
         obj["nargs_min"] = min(obj["nargs_min"], metrics["nargs"]["sum"])
         obj["nexits_min"] = min(obj["nexits_min"], metrics["nexits"]["sum"])
         obj["cognitive_min"] = min(obj["cognitive_min"], metrics["cognitive"]["sum"])
+        obj["mi_original_min"] = min(
+            obj["mi_original_min"], metrics["mi"]["mi_original"]
+        )
+        obj["mi_sei_min"] = min(obj["mi_sei_min"], metrics["mi"]["mi_sei"])
+        obj["mi_visual_studio_min"] = min(
+            obj["mi_visual_studio_min"], metrics["mi"]["mi_visual_studio"]
+        )
 
     for space in metrics_space["spaces"]:
         get_summary_metrics(obj, space)
@@ -502,6 +519,9 @@ def get_space_metrics(
     obj["nargs_total"] += metrics["nargs"]["sum"]
     obj["nexits_total"] += metrics["nexits"]["sum"]
     obj["cognitive_total"] += metrics["cognitive"]["sum"]
+    obj["mi_original_total"] += metrics["mi"]["mi_original"]
+    obj["mi_sei_total"] += metrics["mi"]["mi_sei"]
+    obj["mi_visual_studio_total"] += metrics["mi"]["mi_visual_studio"]
 
     if calc_summaries:
         for space in metrics_space["spaces"]:

bugbug/commit_features.py
@@ -156,6 +156,9 @@ class source_code_file_metrics(object):
                 "nexits_avg"
             ],
             "Average file cognitive": commit["metrics"]["cognitive_avg"],
+            "Average file mi_original": commit["metrics"]["mi_original_avg"],
+            "Average file mi_sei": commit["metrics"]["mi_sei_avg"],
+            "Average file mi_visual_studio": commit["metrics"]["mi_visual_studio_avg"],
             "Maximum file cyclomatic": commit["metrics"]["cyclomatic_max"],
             "Maximum file number of unique operands": commit["metrics"][
                 "halstead_n2_max"
@@ -174,6 +177,9 @@ class source_code_file_metrics(object):
                 "nexits_max"
             ],
             "Maximum file cognitive": commit["metrics"]["cognitive_max"],
+            "Maximum file mi_original": commit["metrics"]["mi_original_max"],
+            "Maximum file mi_sei": commit["metrics"]["mi_sei_max"],
+            "Maximum file mi_visual_studio": commit["metrics"]["mi_visual_studio_max"],
             "Minimum file cyclomatic": commit["metrics"]["cyclomatic_min"],
             "Minimum file number of unique operands": commit["metrics"][
                 "halstead_n2_min"
@@ -192,6 +198,9 @@ class source_code_file_metrics(object):
                 "nexits_min"
             ],
             "Minimum file cognitive": commit["metrics"]["cognitive_min"],
+            "Minimum file mi_original": commit["metrics"]["mi_original_min"],
+            "Minimum file mi_sei": commit["metrics"]["mi_sei_min"],
+            "Minimum file mi_visual_studio": commit["metrics"]["mi_visual_studio_min"],
             "Total file cyclomatic": commit["metrics"]["cyclomatic_total"],
             "Total file number of unique operands": commit["metrics"][
                 "halstead_n2_total"
@@ -210,6 +219,9 @@ class source_code_file_metrics(object):
                 "nexits_total"
             ],
             "Total file cognitive": commit["metrics"]["cognitive_total"],
+            "Total file mi_original": commit["metrics"]["mi_original_total"],
+            "Total file mi_sei": commit["metrics"]["mi_sei_total"],
+            "Total file mi_visual_studio": commit["metrics"]["mi_visual_studio_total"],
         }
 
 
@@ -269,6 +281,9 @@ class source_code_function_metrics(object):
                 "nexits_avg"
             ],
             "Average function cognitive": merged_metrics["cognitive_avg"],
+            "Average function mi_original": merged_metrics["mi_original_avg"],
+            "Average function mi_sei": merged_metrics["mi_sei_avg"],
+            "Average function mi_visual_studio": merged_metrics["mi_visual_studio_avg"],
             "Maximum function cyclomatic": merged_metrics["cyclomatic_max"],
             "Maximum function number of unique operands": merged_metrics[
                 "halstead_n2_max"
@@ -289,6 +304,9 @@ class source_code_function_metrics(object):
                 "nexits_max"
             ],
             "Maximum function cognitive": merged_metrics["cognitive_max"],
+            "Maximum function mi_original": merged_metrics["mi_original_max"],
+            "Maximum function mi_sei": merged_metrics["mi_sei_max"],
+            "Maximum function mi_visual_studio": merged_metrics["mi_visual_studio_max"],
             "Minimum function cyclomatic": merged_metrics["cyclomatic_min"],
             "Minimum function number of unique operands": merged_metrics[
                 "halstead_n2_min"
@@ -309,6 +327,9 @@ class source_code_function_metrics(object):
                 "nexits_min"
             ],
             "Minimum function cognitive": merged_metrics["cognitive_min"],
+            "Minimum function mi_original": merged_metrics["mi_original_min"],
+            "Minimum function mi_sei": merged_metrics["mi_sei_min"],
+            "Minimum function mi_visual_studio": merged_metrics["mi_visual_studio_min"],
             "Total function cyclomatic": merged_metrics["cyclomatic_total"],
             "Total function number of unique operands": merged_metrics[
                 "halstead_n2_total"
@@ -329,6 +350,9 @@ class source_code_function_metrics(object):
                 "nexits_total"
             ],
             "Total function cognitive": merged_metrics["cognitive_total"],
+            "Total function mi_original": merged_metrics["mi_original_total"],
+            "Total function mi_sei": merged_metrics["mi_sei_total"],
+            "Total function mi_visual_studio": merged_metrics["mi_visual_studio_total"],
         }
 
 
@@ -357,6 +381,11 @@ class source_code_metrics_diff(object):
                 "nexits_total"
             ],
             "Diff in cognitive": commit["metrics_diff"]["cognitive_total"],
+            "Diff in mi_original": commit["metrics_diff"]["mi_original_total"],
+            "Diff in mi_sei": commit["metrics_diff"]["mi_sei_total"],
+            "Diff in mi_visual_studio": commit["metrics_diff"][
+                "mi_visual_studio_total"
+            ],
         }
 
 

tests/test_repository.py
@@ -2463,6 +2463,18 @@ void main() {
         "sloc_max": 3.0,
         "sloc_min": 3.0,
         "sloc_total": 5.0,
+        "mi_original_avg": 0.0,
+        "mi_original_max": 134.9997572104644,
+        "mi_original_min": 134.9997572104644,
+        "mi_original_total": 126.49438210545534,
+        "mi_sei_avg": 0.0,
+        "mi_sei_max": 119.16444811614275,
+        "mi_sei_min": 119.16444811614275,
+        "mi_sei_total": 106.9956054906502,
+        "mi_visual_studio_avg": 0.0,
+        "mi_visual_studio_max": 78.94722643886807,
+        "mi_visual_studio_min": 78.94722643886807,
+        "mi_visual_studio_total": 73.97332286868733,
     }
     assert commit.metrics_diff == {
         "cloc_total": 0.0,
@@ -2477,6 +2489,9 @@ void main() {
         "nexits_total": 0.0,
         "ploc_total": 0.0,
         "sloc_total": 0.0,
+        "mi_original_total": 0.0,
+        "mi_sei_total": 0.0,
+        "mi_visual_studio_total": 0.0,
     }
     assert commit.functions == {
         "prova.cpp": [
@@ -2495,6 +2510,9 @@ void main() {
                     "nexits_total": 0.0,
                     "ploc_total": 3.0,
                     "sloc_total": 3.0,
+                    "mi_original_total": 134.9997572104644,
+                    "mi_sei_total": 119.16444811614275,
+                    "mi_visual_studio_total": 78.94722643886807,
                 },
                 "name": "main",
                 "start": 3,
@@ -2618,6 +2636,18 @@ void main() {
         "sloc_max": 4.0,
         "sloc_min": 4.0,
         "sloc_total": 6.0,
+        "mi_original_avg": 0.0,
+        "mi_original_max": 127.65173409189691,
+        "mi_original_min": 127.65173409189691,
+        "mi_original_total": 120.85319934054462,
+        "mi_sei_avg": 0.0,
+        "mi_sei_max": 108.56349160264799,
+        "mi_sei_min": 108.56349160264799,
+        "mi_sei_total": 98.85709909096525,
+        "mi_visual_studio_avg": 0.0,
+        "mi_visual_studio_max": 74.65013689584615,
+        "mi_visual_studio_min": 74.65013689584615,
+        "mi_visual_studio_total": 70.67438557926586,
     }
     assert commit.metrics_diff == {
         "cloc_total": 0.0,
@@ -2632,6 +2662,9 @@ void main() {
         "nexits_total": 0.0,
         "ploc_total": 1.0,
         "sloc_total": 1.0,
+        "mi_original_total": -5.641182764910724,
+        "mi_sei_total": -8.138506399684957,
+        "mi_visual_studio_total": -3.298937289421474,
     }
     assert commit.functions == {
         "prova.cpp": [
@@ -2650,6 +2683,9 @@ void main() {
                     "nexits_total": 0.0,
                     "ploc_total": 4.0,
                     "sloc_total": 4.0,
+                    "mi_original_total": 127.65173409189691,
+                    "mi_sei_total": 108.56349160264799,
+                    "mi_visual_studio_total": 74.65013689584615,
                 },
                 "name": "main",
                 "start": 3,

commit ce0d6fd8318ea7f1bd4a0f1f73989b39a81c6e02

Ignore cases where the Mi metric is missing instead of failing abruptly
Fixes a regression from 2d7c53f2762c83536979500f82e2be4e8202fbb0

bugbug/repository.py
@@ -466,13 +466,17 @@ def get_summary_metrics(obj, metrics_space):
         obj["nargs_max"] = max(obj["nargs_max"], metrics["nargs"]["sum"])
         obj["nexits_max"] = max(obj["nexits_max"], metrics["nexits"]["sum"])
         obj["cognitive_max"] = max(obj["cognitive_max"], metrics["cognitive"]["sum"])
-        obj["mi_original_max"] = max(
-            obj["mi_original_max"], metrics["mi"]["mi_original"]
-        )
-        obj["mi_sei_max"] = max(obj["mi_sei_max"], metrics["mi"]["mi_sei"])
-        obj["mi_visual_studio_max"] = max(
-            obj["mi_visual_studio_max"], metrics["mi"]["mi_visual_studio"]
-        )
+        # TODO: Try removing the None checks after https://github.com/mozilla/rust-code-analysis/issues/528 is fixed.
+        if metrics["mi"]["mi_original"] is not None:
+            obj["mi_original_max"] = max(
+                obj["mi_original_max"], metrics["mi"]["mi_original"]
+            )
+        if metrics["mi"]["mi_sei"] is not None:
+            obj["mi_sei_max"] = max(obj["mi_sei_max"], metrics["mi"]["mi_sei"])
+        if metrics["mi"]["mi_visual_studio"] is not None:
+            obj["mi_visual_studio_max"] = max(
+                obj["mi_visual_studio_max"], metrics["mi"]["mi_visual_studio"]
+            )
 
         obj["cyclomatic_min"] = min(obj["cyclomatic_min"], metrics["cyclomatic"]["sum"])
         obj["halstead_n2_min"] = min(obj["halstead_n2_min"], metrics["halstead"]["n2"])
@@ -486,13 +490,17 @@ def get_summary_metrics(obj, metrics_space):
         obj["nargs_min"] = min(obj["nargs_min"], metrics["nargs"]["sum"])
         obj["nexits_min"] = min(obj["nexits_min"], metrics["nexits"]["sum"])
         obj["cognitive_min"] = min(obj["cognitive_min"], metrics["cognitive"]["sum"])
-        obj["mi_original_min"] = min(
-            obj["mi_original_min"], metrics["mi"]["mi_original"]
-        )
-        obj["mi_sei_min"] = min(obj["mi_sei_min"], metrics["mi"]["mi_sei"])
-        obj["mi_visual_studio_min"] = min(
-            obj["mi_visual_studio_min"], metrics["mi"]["mi_visual_studio"]
-        )
+        # TODO: Try removing the None checks after https://github.com/mozilla/rust-code-analysis/issues/528 is fixed.
+        if metrics["mi"]["mi_original"] is not None:
+            obj["mi_original_min"] = min(
+                obj["mi_original_min"], metrics["mi"]["mi_original"]
+            )
+        if metrics["mi"]["mi_sei"] is not None:
+            obj["mi_sei_min"] = min(obj["mi_sei_min"], metrics["mi"]["mi_sei"])
+        if metrics["mi"]["mi_visual_studio"] is not None:
+            obj["mi_visual_studio_min"] = min(
+                obj["mi_visual_studio_min"], metrics["mi"]["mi_visual_studio"]
+            )
 
     for space in metrics_space["spaces"]:
         get_summary_metrics(obj, space)
@@ -519,9 +527,13 @@ def get_space_metrics(
     obj["nargs_total"] += metrics["nargs"]["sum"]
     obj["nexits_total"] += metrics["nexits"]["sum"]
     obj["cognitive_total"] += metrics["cognitive"]["sum"]
-    obj["mi_original_total"] += metrics["mi"]["mi_original"]
-    obj["mi_sei_total"] += metrics["mi"]["mi_sei"]
-    obj["mi_visual_studio_total"] += metrics["mi"]["mi_visual_studio"]
+    # TODO: Try removing the None checks after https://github.com/mozilla/rust-code-analysis/issues/528 is fixed.
+    if metrics["mi"]["mi_original"] is not None:
+        obj["mi_original_total"] += metrics["mi"]["mi_original"]
+    if metrics["mi"]["mi_sei"] is not None:
+        obj["mi_sei_total"] += metrics["mi"]["mi_sei"]
+    if metrics["mi"]["mi_visual_studio"] is not None:
+        obj["mi_visual_studio_total"] += metrics["mi"]["mi_visual_studio"]
 
     if calc_summaries:
         for space in metrics_space["spaces"]: