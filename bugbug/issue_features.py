5d1fcbd4a36061e96464dd8cf6ccf0ec3b8a1d81
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-04-02 18:07:17 +0200

    Add a module to extract features from GitHub issues
    Fixes #259

---
+++ bugbug/issue_features.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import sys
+
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class comment_count(object):
+    name = "# of comments"
+
+    def __call__(self, issue, **kwargs):
+        return issue["comments"]
+
+
+class IssueExtractor(BaseEstimator, TransformerMixin):
+    def __init__(
+        self,
+        feature_extractors,
+        cleanup_functions,
+    ):
+        assert len(set(type(fe) for fe in feature_extractors)) == len(
+            feature_extractors
+        ), "Duplicate Feature Extractors"
+        self.feature_extractors = feature_extractors
+
+        assert len(set(type(cf) for cf in cleanup_functions)) == len(
+            cleanup_functions
+        ), "Duplicate Cleanup Functions"
+        self.cleanup_functions = cleanup_functions
+
+    def fit(self, x, y=None):
+        for feature in self.feature_extractors:
+            if hasattr(feature, "fit"):
+                feature.fit(x())
+
+        return self
+
+    def transform(self, issues):
+        results = []
+
+        for issue in issues():
+            data = {}
+
+            for feature_extractor in self.feature_extractors:
+                res = feature_extractor(issue)
+
+                if hasattr(feature_extractor, "name"):
+                    feature_extractor_name = feature_extractor.name
+                else:
+                    feature_extractor_name = feature_extractor.__class__.__name__
+
+                if res is None:
+                    continue
+
+                if isinstance(res, (list, set)):
+                    for item in res:
+                        data[sys.intern(f"{item} in {feature_extractor_name}")] = "True"
+                    continue
+
+                if isinstance(res, bool):
+                    res = str(res)
+
+                data[feature_extractor_name] = res
+
+            # TODO: Try simply using all possible fields instead of extracting features manually.
+
+            title = issue["title"]
+            body = issue["body"]
+            for cleanup_function in self.cleanup_functions:
+                title = cleanup_function(title)
+                body = cleanup_function(body)
+
+            results.append(
+                {
+                    "data": data,
+                    "title": title,
+                    "first_comment": body,
+                }
+            )
+
+        return pd.DataFrame(results)
