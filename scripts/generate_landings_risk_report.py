mozilla/bugbug

commit be6dbe278a0f31f40e1a6f166544850cec54180a

Delete bugs that changed from the DB so that they are updated when generating the changes report

scripts/generate_landings_risk_report.py
@@ -758,6 +758,13 @@ class LandingsRiskReportGenerator(object):
         bugs += meta_bugs.keys()
         bugs += sum(meta_bugs.values(), [])
 
+        last_modified = db.last_modified(bugzilla.BUGS_DB)
+        logger.info(f"Deleting bugs modified since the last run on {last_modified}")
+        changed_ids = bugzilla.get_ids(
+            {"f1": "delta_ts", "o1": "greaterthaneq", "v1": last_modified.date()}
+        )
+        bugzilla.delete_bugs(lambda bug: bug["id"] in changed_ids)
+
         bugs = list(set(bugs))
 
         test_infos = self.retrieve_test_info(days)

commit 4de2e1efcc16753e460cc77f7466904ed3e824f1

Make sure the fuzzing metabug is always available when generating the changes report
Since be6dbe278a0f31f40e1a6f166544850cec54180a, it could be deleted when it
was modified.

Fixes a regression from be6dbe278a0f31f40e1a6f166544850cec54180a

scripts/generate_landings_risk_report.py
@@ -44,6 +44,9 @@ PAST_REGRESSION_BLOCKED_BUGS_BY_URL = "https://community-tc.services.mozilla.com
 PAST_FIXED_BUG_BLOCKED_BUGS_BY_URL = "https://community-tc.services.mozilla.com/api/index/v1/task/project.bugbug.past_bugs_by_unit.latest/artifacts/public/past_fixed_bug_blocked_bugs_by_{dimension}.json.zst"
 
 
+FUZZING_METABUG_ID = 316898
+
+
 def _deduplicate(bug_summaries: List[dict]) -> List[dict]:
     seen = set()
     results = []
@@ -420,13 +423,16 @@ class LandingsRiskReportGenerator(object):
             if bug_id in bug_map or bug_id in regressor_bug_ids:
                 bug_to_commits[bug_id].append(commit)
 
-        # All bugs blocking the "fuzz" bug (316898) and its dependent meta bugs are fuzzing bugs.
+        # All bugs blocking the "fuzz" bug and its dependent meta bugs are fuzzing bugs.
         fuzzblocker_bugs = set(
             bug["id"] for bug in bug_map.values() if is_fuzzblocker(bug)
         )
         fuzzing_bugs = (
             set(
-                sum(self.get_blocking_of([316898], meta_only=True).values(), [])
+                sum(
+                    self.get_blocking_of([FUZZING_METABUG_ID], meta_only=True).values(),
+                    [],
+                )
                 + [
                     bug["id"]
                     for bug in bug_map.values()
@@ -771,7 +777,7 @@ class LandingsRiskReportGenerator(object):
         ]
 
         logger.info("Download bugs of interest...")
-        bugzilla.download_bugs(bugs + test_info_bugs)
+        bugzilla.download_bugs(bugs + test_info_bugs + [FUZZING_METABUG_ID])
 
         logger.info(f"{len(bugs)} bugs to analyze.")
 