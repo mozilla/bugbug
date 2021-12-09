28ae611ba76f9b003535978dab9f401b8a54655f
Anoop TP <anooptp6490@gmail.com>
2021-12-09 23:45:41 +0530

    custom-exception - v1

--- bugbug/models/exception.py
+++ bugbug/models/exception.py
@@ -1,7 +1,11 @@
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
 class CustomException(Exception):
     def __init__(self, message):
         self.message = message
 
     def __str__(self):
         return self.message
-
