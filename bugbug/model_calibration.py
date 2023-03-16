e434b33c851067ff03a2f39f0905247622eee735
WhiteWolf47 <bhandari2003anurag@gmail.com>
2023-03-16 18:24:05 +0000

    debugged some methods

--- bugbug/model_calibration.py
+++ bugbug/model_calibration.py
@@ -13,22 +13,21 @@ class IsotonicRegressionCalibrator:
         self.model = model
         self.calibrator = IsotonicRegression()
 
-    def split_data(self, X, y):
-        X_train_val, X_test, y_train_val, y_test = train_test_split(
-            X, y, test_size=0.2, random_state=42
+    def train_test_split(self, X, y, test_size=0.2, random_state=42):
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=test_size, random_state=random_state
         )
-        X_train, X_val, y_train, y_val = train_test_split(
-            X_train_val, y_train_val, test_size=0.2, random_state=42
-        )
-        return X_train, X_val, X_test, y_train, y_val, y_test
+        return X_train, X_test, y_train, y_test
 
-    def fit(self, X_train, y_train, X_val, y_val):
+    def fit(self, X_train, y_train):
+        X_train, X_val, y_train, y_val = self.train_test_split(X_train, y_train)
         self.model.fit(X_train, y_train)
         mse_before = mean_squared_error(y_val, self.model.predict(X_val))
-        print(f"MSE of model before calibration: {mse_before:.4f}")
+        print(f"MSE of model before calibration : {mse_before:.4f}")
+
         self.calibrate(X_val, y_val)
         mse_after = mean_squared_error(y_val, self.predict(X_val))
-        print(f"MSE of model after calibration: {mse_after:.4f}")
+        print(f"MSE of model after calibration : {mse_after:.4f}")
 
     def predict(self, X):
         return self.calibrator.predict(self.model.predict(X))
@@ -40,6 +39,5 @@ class IsotonicRegressionCalibrator:
         self.calibrator.fit(self.model.predict(X_val), y_val)
 
     def train(self, X, y):
-        X_train, X_val, X_test, y_train, y_val, y_test = self.split_data(X, y)
-        self.fit(X_train, y_train, X_val, y_val)
-        return X_train, X_val, X_test, y_train, y_val, y_test
+        X_train, X_test, y_train, y_test = self.train_test_split(X, y)
+        self.fit(X_train, y_train)
