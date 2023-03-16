c2acd97d0bc388696d89750bf2bcb8293950960c
Joshua <cutejosh2@gmail.com>
2023-03-16 16:08:20 +0100

    added docstring to some functions in bugzilla.py

--- bugbug/bugzilla.py
+++ bugbug/bugzilla.py
@@ -96,6 +96,12 @@ def set_token(token):
 
 
 def get_ids(params):
+    """Retrieves a list of bug IDs from Bugzilla based on the given parameters.
+    Args:
+        params: A dictionary of parameters to pass to the Bugzilla API.
+    Returns:
+        list of int: A list of bug IDs.
+    """
     assert "include_fields" not in params or params["include_fields"] == "id"
 
     old_CHUNK_SIZE = Bugzilla.BUGZILLA_CHUNK_SIZE
@@ -117,6 +123,12 @@ def get_ids(params):
 
 
 def get(ids_or_query):
+    """Retrieves information about bugs from Bugzilla based on the given IDs or query.
+    Args:
+        ids_or_query: A query string or a list of bug IDs to retrieve.
+    Returns:
+        A dictionary containing information about the bugs, keyed by bug ID.
+    """
     new_bugs = {}
 
     def bughandler(bug):
@@ -166,6 +178,16 @@ def get(ids_or_query):
 
 
 def get_ids_between(date_from, date_to=None, security=False, resolution=None):
+    """Returns a list of bug IDs created between two dates, filtered by product and resolution.
+
+    Args:
+        date_from: The earliest date from which to include bug IDs.
+        date_to: The latest date to include bug IDs. Defaults to None.
+        security: Whether to include security bugs. Defaults to False.
+        resolution: The resolution status to include. Defaults to None.
+
+    Returns: A list of bug IDs.
+    """
     params = {
         "f1": "creation_ts",
         "o1": "greaterthan",
@@ -189,6 +211,17 @@ def get_ids_between(date_from, date_to=None, security=False, resolution=None):
 
 
 def download_bugs(bug_ids: Iterable[int], security: bool = False) -> list[BugDict]:
+    """The download_bugs function takes a collection of bug IDs and a flag indicating whether or not to include security bugs.
+    It then retrieves information about the bugs from Bugzilla and appends it to a database. Finally,
+    it returns the information as a list of dictionaries.
+
+    Args:
+       bug_ids: Collection of integers representing bug IDs to download.
+       security: A flag indicating whether or not to include security bugs. Defaults to False.
+
+    Returns:
+       A list of dictionaries representing information about the downloaded bugs.
+    """
     old_bug_count = 0
     new_bug_ids_set = set(int(bug_id) for bug_id in bug_ids)
     for bug in get_bugs(include_invalid=True):
@@ -234,6 +267,15 @@ def download_bugs(bug_ids: Iterable[int], security: bool = False) -> list[BugDic
 def _find_linked(
     bug_map: dict[int, BugDict], bug: BugDict, link_type: str
 ) -> list[int]:
+    """Recursively searches through linked bugs for a given bug and returns a list of their ids.
+    Args:
+        bug_map: A dictionary of bug ids and their corresponding bug dictionaries.
+        bug: The bug dictionary for the bug to search through linked bugs for.
+        link_type: The type of link to follow, either "blocks" or "depends_on".
+
+    Returns:
+        A list of bug ids that are linked to the given bug through the specified link type.
+    """
     return sum(
         (
             _find_linked(bug_map, bug_map[b], link_type)
@@ -245,10 +287,30 @@ def _find_linked(
 
 
 def find_blocked_by(bug_map: dict[int, BugDict], bug: BugDict) -> list[int]:
+    """Finds all the bugs in the given bug_map that are blocked by the given bug.
+
+    Args:
+        bug_map: A mapping of bug IDs to their corresponding BugDict objects.
+        bug: The BugDict object representing the bug for which to find all blocked bugs.
+
+    Returns:
+        A list of integers representing the IDs of all the bugs in the bug_map that are blocked by the given bug.
+    """
     return _find_linked(bug_map, bug, "blocks")
 
 
 def find_blocking(bug_map: dict[int, BugDict], bug: BugDict) -> list[int]:
+    """Given a dictionary of bugs and a particular bug, return a list of bugs that the
+    particular bug is blocking.
+
+    Args:
+    bug_map: A dictionary of bugs with their IDs as keys and
+                                  their information as values.
+    bug: A dictionary containing the information for the bug being queried.
+
+    Returns:
+    A list of bug IDs representing the bugs that are blocking the queried bug.
+    """
     return _find_linked(bug_map, bug, "depends_on")
 
 
