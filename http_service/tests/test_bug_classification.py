f91a6fcbaeaf05714dafe1e3b1e8eb76fc7cdd9a
EMEKA ONWUEPE <pascalemy2010@gmail.com>
2023-03-17 07:30:29 +0100

    ran pre-commit run --all-files

--- http_service/tests/test_bug_classification.py
+++ http_service/tests/test_bug_classification.py
@@ -12,7 +12,6 @@ import orjson
 from bugbug_http.app import API_TOKEN
 
 
-
 def retrieve_compressed_reponse(response):
     # Response is of type "<class 'flask.wrappers.Response'>" -  Flask Client's  Response
     # Not applicable for "<class 'requests.models.Response'> "
@@ -166,32 +165,36 @@ def test_model_predict_batch(client, jobs, add_result, add_change_time, response
     # merge the two list
     bug_ids = [*bugs_list, *missing_bugs_list]
     # A call to Buzgilla will return lesser values
-    #Five bug Ids but returns just 2 bug Ids
+    # Five bug Ids but returns just 2 bug Ids
     responses.add(
         responses.GET,
         "https://bugzilla.mozilla.org/rest/bug?id=1566486%2C1598744%2C1602463%2C1615281%2C1619699&include_fields=id&include_fields=last_change_time",
         status=200,
-        json={'bugs': [{'id': 1602463, 'last_change_time': '2022-06-01T17:27:48Z'},
-                        {'last_change_time': '2022-06-01T17:33:36Z', 'id': 1619699}]},
-        )
-    
+        json={
+            "bugs": [
+                {"id": 1602463, "last_change_time": "2022-06-01T17:27:48Z"},
+                {"last_change_time": "2022-06-01T17:33:36Z", "id": 1619699},
+            ]
+        },
+    )
+
     def do_missing_bug_request():
-            return client.post(
+        return client.post(
             "/component/predict/batch",
             data=json.dumps({"bugs": bug_ids}),
             headers={API_TOKEN: "test"},
         )
-          
+
     rv = do_missing_bug_request()
     assert rv.status_code == 202
-    bugs = retrieve_compressed_reponse(rv)['bugs']
-    # /component/predict/batch will return 
+    bugs = retrieve_compressed_reponse(rv)["bugs"]
+    # /component/predict/batch will return
     # the full bugs ids as the call to
-    #Bugzilla is being queued
+    # Bugzilla is being queued
     assert len(bug_ids) == len(bugs.keys())
     # check for the bugs Bugzilla will returned
-    assert '1602463' in list(bugs.keys())
-    assert '1619699' in list(bugs.keys())
+    assert "1602463" in list(bugs.keys())
+    assert "1619699" in list(bugs.keys())
 
 
 def test_empty_batch(client):
