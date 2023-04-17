3d116cf003c97e0f6f083d772e9f498a7e514a55
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 10:08:17 +0200

    Revert part of #3330 to avoid a regression
    The variables logged are only defined in a branch.
    
    Fixes a regression from 8936218ab23cf4b34b9e7293a55278ed90ab87fe

--- bugbug/models/testselect.py
+++ bugbug/models/testselect.py
@@ -834,11 +834,9 @@ class TestSelectModel(Model):
                     and result["caught_percentage_config_group"] is not None
                 )
 
-            logger.info(
-                "In %d%% of pushes we caught at least one config/group failure. On average, we caught %f%% of all seen config/group failures.",
-                percentage_caught_one_config_group,
-                average_caught_percentage_config_group,
-            )
+                message += f" In {percentage_caught_one_config_group}% of pushes we caught at least one config/group failure. On average, we caught {average_caught_percentage_config_group}% of all seen config/group failures."
+
+            logger.info(message)
 
         with concurrent.futures.ProcessPoolExecutor(
             max_workers=utils.get_physical_cpu_count(),
