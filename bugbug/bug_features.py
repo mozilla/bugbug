80abeb9841be92c55df081562590b16ed23760ca
ElusiveEllie <elena@realemail.net>
2023-05-22 12:03:16 -0400

    Added get_time_to_assign_busdays as new function and reverted original function

--- bugbug/bug_features.py
+++ bugbug/bug_features.py
@@ -558,6 +558,21 @@ def get_time_to_assign(bug):
                 and change["removed"] in ("UNCONFIRMED", "NEW")
                 and change["added"] == "ASSIGNED"
             ):
+                return (
+                    dateutil.parser.parse(history["when"])
+                    - dateutil.parser.parse(bug["creation_time"])
+                ).total_seconds() / 86400
+    return None
+
+
+def get_time_to_assign_busdays(bug):
+    for history in bug["history"]:
+        for change in history["changes"]:
+            if (
+                change["field_name"] == "status"
+                and change["removed"] in ("UNCONFIRMED", "NEW")
+                and change["added"] == "ASSIGNED"
+            ):
                 return utils.get_business_days_count(
                     bug["creation_time"],
                     history["when"],
