a3a4e796bab8fd189cedbf4b309af7a8d3f6fb55
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-11-08 12:37:40 +0100

    Don't report 'Incoming vs total open' and 'Closed vs total open' metrics for now

--- scripts/generate_landings_risk_report.py
+++ scripts/generate_landings_risk_report.py
@@ -1787,12 +1787,10 @@ List of revisions that have been waiting for a review for longer than 3 days:
 
             def format_maintenance_effectiveness(period: relativedelta) -> str:
                 me = calculate_maintenance_effectiveness(period)
-                return "ME: {}%, BDTime: {} y, WBDTime: {} y, Incoming vs total open: {} %, Closed vs total open: {} %".format(
+                return "ME: {}%, BDTime: {} y, WBDTime: {} y".format(
                     round(me["ME"], 2),
                     round(me["BDTime"], 2),
                     round(me["WBDTime"], 2),
-                    round(me["Incoming vs total open"], 2),
-                    round(me["Closed vs total open"], 2),
                 )
 
             maintenance_effectiveness_section = f"""<b>[MAINTENANCE EFFECTIVENESS](https://docs.google.com/document/d/1y2dUDZI5U3xvY0jMY1LfIDARc5b_QB9mS2DV7MWrfa0/edit)</b>
