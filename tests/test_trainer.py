b334f1632b4be988080a23d27418c61acfdbe539
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-11-24 20:38:20 +0100

    Allow downloading models from huggingface during tests

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -3,6 +3,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import re
+
 import responses
 
 from bugbug import bugzilla, db
@@ -34,6 +36,8 @@ def test_trainer_simple():
 
 # Test finetuning of transformer model
 def test_trainer_finetuning():
+    responses.add_passthru(re.compile(r"https://.*\.?huggingface.co/\w+"))
+
     # Pretend the DB was already downloaded and no new DB is available.
 
     url = "https://community-tc.services.mozilla.com/api/index/v1/task/project.bugbug.data_bugs.latest/artifacts/public/bugs.json"
@@ -57,6 +61,8 @@ def test_trainer_finetuning():
 
 # Test xgboost model on transformed model's embeddings
 def test_trainer_embedding():
+    responses.add_passthru(re.compile(r"https://.*\.?huggingface.co/\w+"))
+
     # Pretend the DB was already downloaded and no new DB is available.
 
     url = "https://community-tc.services.mozilla.com/api/index/v1/task/project.bugbug.data_bugs.latest/artifacts/public/bugs.json"
