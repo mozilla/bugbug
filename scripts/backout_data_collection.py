c0964688c8d09cfcfdef159eae3b722fdc5a3cb1
Benjamin Mah <benjaminmah2004@gmail.com>
2024-05-03 14:30:18 -0400

    Cleaned up code, restructured dataset to include the inducing, backout, and fixing commit

--- scripts/backout_data_collection.py
+++ scripts/backout_data_collection.py
@@ -1,219 +1,166 @@
-import sys
-import logging
 import json
+import logging
 import os
+import sys
+
 from tqdm import tqdm
 
-sys.path.append('../bugbug')
-from bugbug import repository, db, bugzilla
+sys.path.append("../bugbug")
+from bugbug import bugzilla, db, repository
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def download_commits() -> None:
-    """
-    Download the database of all commits.
 
-    Args:
-        None
-
-    Returns:
-        None
-    """
+def download_databases() -> None:
+    logger.info("Downloading bugs database...")
+    assert db.download(bugzilla.BUGS_DB)
 
     logger.info("Downloading commits database...")
     assert db.download(repository.COMMITS_DB, support_files_too=True)
 
-def download_bugs() -> None:
-    """
-    Download the database of all bugs.
 
-    Args:
-        None
+def preprocess_commits_and_bugs():
+    logger.info("Preprocessing commits")
+    commit_dict = {}
+    bug_to_commit_dict = {}
 
-    Returns:
-        None
-    """
+    # store commits with their hashes and bug IDs as keys
+    for commit in tqdm(
+        repository.get_commits(
+            include_no_bug=True, include_backouts=True, include_ignored=True
+        ),
+        desc="Processing commits",
+    ):
+        commit_dict[commit["node"]] = commit
 
-    logger.info("Downloading bugs database...")
-    assert db.download(bugzilla.BUGS_DB)
+        if commit["bug_id"] not in bug_to_commit_dict:
+            bug_to_commit_dict[commit["bug_id"]] = [commit]
+        else:
+            bug_to_commit_dict[commit["bug_id"]].append(commit)
 
-def is_bug_fixed(bug_id: int) -> bool:
-    """
-    Check if a bug (given its ID) is resolved.
+    logger.info("Preprocessing bugs")
+    bug_dict = {}
 
-    Args:
-        bug_id (int): the ID of the bug from Bugzilla
+    num_lines = sum(1 for line in open(bugzilla.BUGS_DB, "r"))
 
-    Returns:
-        bool: True if the bug is RESOLVED, False otherwise
-    """
+    # store bugs with their bug IDs as keys
+    with open(bugzilla.BUGS_DB, "r") as f:
+        for line in tqdm(f, total=num_lines, desc="Processing bugs"):
+            bug = json.loads(line)
+            bug_dict[bug.get("id")] = bug
 
-    # get bug details
-    bug_details = bugzilla.get([bug_id])
-    bug = bug_details.get(bug_id, {})
+    return commit_dict, bug_to_commit_dict, bug_dict
 
-    # if the bug is fixed, we return True
-    if bug.get("resolution") == 'FIXED':
-        return True
+
+def is_bug_fixed(bug_id: int, bug_dict: dict) -> bool:
+    bug = bug_dict.get(bug_id)
+
+    if bug:
+        return bug.get("resolution") == "FIXED"
 
     return False
 
-def filter_commits(directory_path: str, destination_filepath: str, count_limit: int) -> None:
-    """
-    Filters the commits based on:
-        1. if it is backing out another commit
-        2. if the bug is RESOLVED
-    Saves to a .json file in ../data/raw_backout_data.json
-    
-    Args:
-        directory_path (str): the directory where the dataset will be saved
-        destination_filepath (str): where the raw filtered dataset will be saved
-        count_limit (int): limit of commits that are added to the json
-    
-    Returns:
-        None
 
-    """
-
+def filter_commits(
+    directory_path: str,
+    destination_filepath: str,
+    count_limit: int,
+    bug_dict: dict,
+    commit_dict: dict,
+    bug_to_commit_dict: dict,
+) -> None:
     filtered_list = []
     counter = 0
 
     pbar = tqdm(total=count_limit, desc="Filtering commits")
-    
-    # iterate over all commits
-    for commit in repository.get_commits(include_no_bug=False, include_backouts=True, include_ignored=False):
-        
-        # if the commit is backing out other commits and the bug is fixed
-        if len(commit["backsout"]) > 0 and is_bug_fixed(commit["bug_id"]):
-            filtered_list.append(commit)
+
+    for commit in repository.get_commits(
+        include_no_bug=False, include_backouts=True, include_ignored=False
+    ):
+        # add commit if it was backed out and the bug is fixed
+        if len(commit["backedoutby"]) > 0 and is_bug_fixed(
+            bug_id=commit["bug_id"], bug_dict=bug_dict
+        ):
+            fixing_commit = find_next_commit(
+                commit["bug_id"], bug_to_commit_dict, commit["node"]
+            )
+
+            # if fixing commit could not be found, do not add it to dataset
+            if fixing_commit["node"] == commit["backedoutby"]:
+                counter += 1
+                continue
+
+            # add the hashes of the bug-inducing commit, the back out commit, and the fixing commit
+            # include metadata such as push date and description for further context
+            list_entry = {
+                "bug_id": commit["bug_id"],
+                "inducing_commit": {
+                    "node": commit["node"],
+                    "pushdate": commit["pushdate"],
+                    "desc": commit["desc"],
+                },
+                "backout_commit": {
+                    "node": commit["backedoutby"],
+                    "pushdate": commit_dict[commit["backedoutby"]]["pushdate"],
+                    "desc": commit_dict[commit["backedoutby"]]["desc"],
+                },
+                "fixing_commit": {
+                    "node": fixing_commit["node"],
+                    "pushdate": fixing_commit["pushdate"],
+                    "desc": fixing_commit["desc"],
+                },
+            }
+
+            filtered_list.append(list_entry)
             counter += 1
             pbar.update(1)
 
             if counter >= count_limit:
                 break
 
-    # convert to json
     json_data = json.dumps(filtered_list, indent=4)
 
-    # write to a file
     if not os.path.exists(directory_path):
         os.makedirs(directory_path)
         print(f"Directory {directory_path} created")
 
-    with open(destination_filepath, 'w') as file:
+    with open(destination_filepath, "w") as file:
         file.write(json_data)
 
     logger.info(f"Data successfully saved to {destination_filepath}")
 
-def preprocess_commits():
-    """
-    Preprocess all commit hashes to create a dictionary mapping the first
-    12 characters to the entire hash.
-
-    Args:
-        None
-    
-    Returns:
-        commit_dict (dict): dictionary of commit hashes
-    
-    """
-
-    commit_dict = {}
-    for commit in repository.get_commits(include_no_bug=False, include_backouts=True, include_ignored=False):
-        shortened_hash = commit['node'][:12]
-        commit_dict[shortened_hash] = commit['node']
-    return commit_dict
-
-
-def get_full_hash(shortened_hash: str, commit_dict) -> str:
-    """
-    Given a shortened hash of 12 characters, search in the commit_dict for the full hash and return it.
-
-    Args:
-        shortened_hash (str): 12 character long shortened hash of a commit
-        commit_dict (dict): Dictionary of shortened hashes to full hashes
-    Returns:
-        str: full 40 character long hash of commit, or None if not found
-    """
-
-    return commit_dict.get(shortened_hash)
-
-def find_full_hash(source_filepath: str, destination_filepath: str, commit_dict: dict) -> None:
-    """
-    Convert the first 12 characters of the hash in backsout (list) to the full 40 character long hash using commit_dict.
-
-    Args:
-        source_filepath (str): filepath to .json file containing raw backout data
-        destination_filepath (str): where to save the new .json file containing the full hashesh
-        commit_dict (dict): dictionary mapping shortened hashes to full hashes
-    Returns:
-        None
-    """
-
-    with open(source_filepath, 'r') as file:
-        data = json.load(file)
-
-    for commit in data:
-        for index, backout_commit in enumerate(commit['backsout']):
-            full_hash = get_full_hash(backout_commit, commit_dict)
-
-            # replace shortened hash with full hash
-            if full_hash:
-                commit['backsout'][index] = full_hash
-    
-    # save to a new file
-    with open(destination_filepath, 'w') as file:
-        json.dump(data, file, indent=4)
-        logger.info(f"Updated data has been saved to {destination_filepath}")
-    return data
-
-def clean_dataset(source_filepath: str, destination_filepath: str):
-    """
-    Given a source filepath to a .json file containing the full hashes, remove
-    unnecessary fields and save to destination_filepath.
-
-    Args:
-        source_filepath (str): filepath to .json file containing full hash data
-        destination_filepath (str): where to save new .json file containing cleaned data
-    Returns:
-        None
-    """
-
-    with open(source_filepath, 'r') as file:
-        data = json.load(file)
-    
-    filtered_data = []
-
-    # fields to keep, rest will be discarded
-    fields_to_keep = ['node', 'bug_id', 'desc', 'pushdate', 'backsout']
-
-    for commit in data:
-        filtered_commit = {key: commit[key] for key in fields_to_keep if key in commit}
-        filtered_data.append(filtered_commit)
-
-    with open(destination_filepath, 'w') as file:
-        json.dump(filtered_data, file, indent=4)
-
-    logger.info(f"Filtered data has been saved to {destination_filepath}")
-
     return
 
+
+def find_next_commit(bug_id: int, bug_to_commit_dict: dict, inducing_node: str) -> dict:
+    inducing_commit_found = False
+    for commit in bug_to_commit_dict[bug_id]:
+        # if the inducing commit has been found, find next commit that has not been backed out
+        if inducing_commit_found:
+            if len(commit["backedoutby"]) == 0:
+                return commit
+
+        if commit["node"] == inducing_node:
+            inducing_commit_found = True
+
+    return commit
+
+
+def main():
+    download_databases()
+
+    commit_dict, bug_to_commit_dict, bug_dict = preprocess_commits_and_bugs()
+
+    filter_commits(
+        directory_path="dataset",
+        destination_filepath="dataset/backout_dataset.json",
+        count_limit=500,
+        bug_dict=bug_dict,
+        commit_dict=commit_dict,
+        bug_to_commit_dict=bug_to_commit_dict,
+    )
+
+
 if __name__ == "__main__":
-    # download commits and bugs
-    download_commits()
-    download_bugs()
-
-    # filter commits based on backout and are fixed bugs, save to .json
-    filter_commits(directory_path="dataset", destination_filepath="dataset/raw_backout_data.json", count_limit=500)
-    
-    # create dictionary mapping shortened hashes to their full hashes
-    commit_dict = preprocess_commits()
-
-    # find and replace shortened hashes with full hashes
-    find_full_hash(source_filepath="dataset/raw_backout_data.json", destination_filepath="dataset/processed_backout_data.json", commit_dict=commit_dict)
-    
-    # remove unnecessary fields
-    clean_dataset(source_filepath="dataset/processed_backout_data.json", destination_filepath="dataset/cleaned_backout_data.json")
-
-
+    main()
