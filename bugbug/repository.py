846210fc435eeb8f4438b237fe813fdff71164ac
Benjamin Mah <benjaminmah2004@gmail.com>
2024-06-03 10:08:52 -0400

    Changed standard collections to generic types

--- bugbug/repository.py
+++ bugbug/repository.py
@@ -18,16 +18,10 @@ import shelve
 import subprocess
 import sys
 import threading
+from collections.abc import Collection, Iterable, Iterator
 from datetime import datetime
 from functools import lru_cache
-from typing import (
-    Collection,
-    Iterable,
-    Iterator,
-    NewType,
-    Set,
-    Union,
-)
+from typing import NewType, Union
 
 import hglib
 import lmdb
@@ -203,7 +197,7 @@ class Commit:
         self.source_code_deleted = 0
         self.other_deleted = 0
         self.test_deleted = 0
-        self.types: Set[str] = set()
+        self.types: set[str] = set()
         self.functions: dict[str, list[dict]] = {}
         self.seniority_author = 0.0
         self.total_source_code_file_size = 0
