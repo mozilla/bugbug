66108ad83f3e2579af14ee983758cc3b5ff7fcf9
Benjamin Mah <benjaminmah2004@gmail.com>
2024-06-04 09:40:03 -0400

    Added raise-from when shelving

--- bugbug/repository.py
+++ bugbug/repository.py
@@ -1554,8 +1554,7 @@ def get_diff(repo_path, original_hash, fix_hash) -> bytes:
         if b"nothing changed" in e.out:
             logger.info(f"Nothing to shelve: {e}")
         else:
-            logger.info(f"Error while shelving: {e}")
-            raise
+            raise RuntimeError("Error occurred while shelving") from e
 
     parents = client.parents(rev=fix_hash)
     parent_of_fix = parents[0][1]
