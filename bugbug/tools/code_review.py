3015c954af95306bde4ed39e76d93466395d7fc1
olewicki <doriane.olewicki@gmail.com>
2024-06-06 15:10:11 -0400

    swarm scraper

--- bugbug/tools/code_review.py
+++ bugbug/tools/code_review.py
@@ -19,7 +19,7 @@ from tqdm import tqdm
 from unidiff import Hunk, PatchedFile, PatchSet
 from unidiff.errors import UnidiffParseError
 
-from bugbug import db, phabricator
+from bugbug import db, phabricator, swarm
 from bugbug.generative_model_tool import GenerativeModelTool
 from bugbug.utils import get_secret
 from bugbug.vectordb import VectorDB, VectorPoint
@@ -125,10 +125,12 @@ class ReviewRequest:
 
 class Patch:
     raw_diff: str
+    file_diff: str
 
-    def __init__(self, raw_diff) -> None:
+    def __init__(self, raw_diff, file_diff=None) -> None:
         super().__init__()
         self.raw_diff = raw_diff
+        self.file_diff = file_diff
 
 
 class ReviewData(ABC):
@@ -369,8 +371,50 @@ class PhabricatorReviewData(ReviewData):
                 yield diff_id, comments
 
 
+class SwarmReviewData(ReviewData):
+    def __init__(self):
+        self.auth = {
+            "user": get_secret("SWARM_USER"),
+            "password": get_secret("SWARM_PASS"),
+            "port": get_secret("SWARM_PORT"),
+            "instance": get_secret("INSTANCE"),
+        }
+
+    # return ReviewRequest object with patch_id = revision_id
+    def get_review_request_by_id(self, revision_id: int) -> ReviewRequest:
+        return ReviewRequest(revision_id)
+
+    # return rawdiff from the initial version of the revision with id patch_id
+    def get_patch_by_id(self, patch_id: int) -> Patch:
+        revisions = swarm.get(self.auth, rev_ids=[int(patch_id)], version_l=[0, 1])
+        assert len(revisions) == 1
+        return Patch(revisions[0]["fields"]["diff"])
+
+    # return rawdiff from the initial version of a specific version of the revision_id
+    def get_patch_by_version_fromto(
+        self, revision_id: int, version_before: int = 0, version_num: int = 1
+    ) -> Patch:
+        revisions = swarm.get(
+            self.auth,
+            rev_ids=[int(revision_id)],
+            version_l=[version_before, version_num],
+        )
+        assert len(revisions) == 1
+
+        return Patch(
+            revisions[0]["fields"]["diff"], revisions[0]["fields"]["file_diff"]
+        )
+
+    def get_all_inline_comments(
+        self, comment_filter
+    ) -> Iterable[tuple[int, list[InlineComment]]]:
+        # Todo
+        raise NotImplementedError
+
+
 review_data_classes = {
     "phabricator": PhabricatorReviewData,
+    "swarm": SwarmReviewData,
 }
 
 
