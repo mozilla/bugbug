9adba3f671f1228dad6252f93bf794ef9311ee1a
Benjamin Mah <benjaminmah2004@gmail.com>
2024-06-20 12:17:36 -0400

    Added file path feature extraction

--- bugbug/bug_features.py
+++ bugbug/bug_features.py
@@ -905,3 +905,19 @@ class BugType(SingleBugFeature):
 
     def __call__(self, bug, **kwargs):
         return bug["type"]
+
+class ExtractFilePaths(SingleBugFeature):
+    """Extract file paths (partial and full) from bug data."""
+
+    name = "Extract File Paths"
+
+    def __call__(self, bug: bugzilla.BugDict, **kwargs) -> list[str]:
+        text = (
+            bug.get("summary", "")
+            + " "
+            + " ".join(comment["text"] for comment in bug.get("comments", []))
+        )
+
+        paths = re.findall(r"\b[\w/\\]+/\w+\.\w+\b", text)
+
+        return sorted(set(paths))
