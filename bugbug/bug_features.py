802c9bd859293ad136fdfc3e4facb72a3cf0e486
Benjamin Mah <benjaminmah2004@gmail.com>
2024-07-02 10:28:28 -0400

    Compiled regex using `re.compile` and move to constructor

--- bugbug/bug_features.py
+++ bugbug/bug_features.py
@@ -926,12 +926,16 @@ class ExtractFilePaths(SingleBugFeature):
     """Extract file paths (partial and full) from bug data."""
 
     name = "Extract File Paths"
-    regex = r"\b[a-zA-Z0-9_/.\-]+(?:/[a-zA-Z0-9_/.\-]+)*\.(?!com|org|net|edu|gov)[a-zA-Z]{2,4}\b"
+
+    def __init__(self):
+        self.regex = re.compile(
+            r"\b[a-zA-Z0-9_/.\-]+(?:/[a-zA-Z0-9_/.\-]+)*\.(?!com|org|net|edu|gov)[a-zA-Z]{2,4}\b"
+        )
 
     def __call__(self, bug: bugzilla.BugDict, **kwargs) -> list[str]:
         text = bug.get("summary", "") + " " + bug["comments"][0]["text"]
 
-        file_paths = re.findall(self.regex, text)
+        file_paths = self.regex.findall(text)
 
         all_sub_paths = []
         for path in file_paths:
