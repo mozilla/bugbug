d1abebd0cd014b285b66557c62d718260ecaafd4
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-07-08 07:14:50 +0200

    Implement the initial version of the code search (#4234)
    Co-authored-by: Suhaib Mujahid <smujahid@mozilla.com>

---
+++ bugbug/code_search/mozilla.py
@@ -0,0 +1,166 @@
+from bugbug.code_search.function_search import (
+    Function,
+    FunctionSearch,
+    register_function_search,
+)
+from bugbug.code_search.parser import FunctionSearchParser
+from bugbug.code_search.searchfox_api import FunctionSearchSearchfoxAPI
+from bugbug.code_search.searchfox_data import FunctionSearchSearchfoxData
+from bugbug.tools.code_review import PhabricatorPatch
+
+
+class FunctionSearchMozilla(FunctionSearch):
+    def __init__(self, repo_dir, get_file, fast=False):
+        super().__init__()
+        self.repo_dir = repo_dir
+        self.get_file = get_file
+        self.fast = fast
+
+    def get_function(
+        self, commit_hash, path, function_name=None, line=None, fast=False
+    ):
+        # In fast mode, only use Searchfox website.
+        # In slow mode (for historical data), try Searchfox first and fallback on parsing with rust-code-analysis.
+
+        result = []
+
+        if not fast:
+            function_search_searchfox_data = FunctionSearchSearchfoxData()
+            if function_name is not None:
+                result = function_search_searchfox_data.get_function_by_name(
+                    commit_hash, path, function_name
+                )
+            elif line is not None:
+                result = function_search_searchfox_data.get_function_by_line(
+                    commit_hash, path, line
+                )
+
+            # If we were not able to find the function by using Searchfox, fallback on parsing.
+            if not result:
+                function_search_parser = FunctionSearchParser(
+                    self.repo_dir, self.get_file
+                )
+                if function_name is not None:
+                    result = function_search_parser.get_function_by_name(
+                        commit_hash, path, function_name
+                    )
+                elif line is not None:
+                    result = function_search_parser.get_function_by_line(
+                        commit_hash, path, line
+                    )
+
+        if not result:
+            function_search_searchfox_api = FunctionSearchSearchfoxAPI(self.get_file)
+            if function_name is not None:
+                result = function_search_searchfox_api.get_function_by_name(
+                    commit_hash, path, function_name
+                )
+            elif line is not None:
+                result = function_search_searchfox_api.get_function_by_line(
+                    commit_hash, path, line
+                )
+
+        # Filter out results when there are too many.
+        # TODO: If the function is in a JS file and we found its definition in both JS and C++ files, use the definition in JS file.
+        # TODO: If the function is in a non-test file and we found its definition in both test and non-test files, use the definition in the non-test file (and the opposite for functions in test files)
+
+        found_perfect = None
+        for func in result:
+            if func["name"] == function_name:
+                assert found_perfect is None, "Found two functions with the same name"
+                found_perfect = func
+
+        if found_perfect is not None:
+            return [found_perfect]
+        else:
+            return result
+
+    def get_function_by_line(
+        self, commit_hash: str, path: str, line: int
+    ) -> list[Function]:
+        return self.get_function(commit_hash, path, line=line, fast=self.fast)
+
+    def get_function_by_name(
+        self, commit_hash: str, path: str, function_name: str
+    ) -> list[Function]:
+        return self.get_function(
+            commit_hash, path, function_name=function_name, fast=self.fast
+        )
+
+
+register_function_search("mozilla", FunctionSearchMozilla)
+
+
+if __name__ == "__main__":
+    import sys
+
+    import requests
+    from libmozdata.phabricator import PhabricatorAPI
+
+    from bugbug.utils import get_secret
+
+    phabricator = PhabricatorAPI(
+        get_secret("PHABRICATOR_TOKEN"), get_secret("PHABRICATOR_URL")
+    )
+
+    def get_file(commit_hash, path):
+        r = requests.get(
+            f"https://hg.mozilla.org/mozilla-unified/raw-file/{commit_hash}/{path}"
+        )
+        r.raise_for_status()
+        return r.text
+
+    repo_dir = sys.argv[1]
+
+    function_search_mozilla = FunctionSearchMozilla(repo_dir, get_file, False)
+
+    # https://phabricator.services.mozilla.com/D199272?id=811858
+    patch1 = PhabricatorPatch("811858")
+
+    # In this case, the function was not used before the patch.
+    print(
+        function_search_mozilla.get_function_by_name(
+            patch1.base_commit_hash,
+            "dom/base/nsObjectLoadingContent.cpp",
+            "LowerCaseEqualsASCII",
+        )
+    )
+
+    # In this case, the function was used before the patch.
+    print(
+        function_search_mozilla.get_function_by_name(
+            patch1.base_commit_hash,
+            "dom/base/nsObjectLoadingContent.cpp",
+            "HtmlObjectContentTypeForMIMEType",
+        )
+    )
+
+    # https://phabricator.services.mozilla.com/D199248?id=811740
+    patch2 = PhabricatorPatch("811740")
+
+    # In this case, it is a JS file.
+    print(
+        function_search_mozilla.get_function_by_name(
+            patch2.base_commit_hash,
+            "testing/modules/XPCShellContentUtils.sys.mjs",
+            "registerPathHandler",
+        )
+    )
+
+    patch3 = PhabricatorPatch("721783")
+
+    print(
+        function_search_mozilla.get_function_by_name(
+            patch3.base_commit_hash,
+            "dom/performance/Performance.cpp",
+            "Performance::MemoryPressure",
+        )
+    )
+
+    patch4 = PhabricatorPatch("736446")
+
+    function_search_mozilla.get_function_by_line(
+        patch4.base_commit_hash,
+        "browser/base/content/test/webrtc/browser_devices_select_audio_output.js",
+        180,
+    )
--- scripts/code_review_tool_runner.py
+++ scripts/code_review_tool_runner.py
@@ -7,6 +7,7 @@ import argparse
 import sys
 
 from bugbug import generative_model_tool
+from bugbug.code_search.function_search import function_search_classes
 from bugbug.tools import code_review
 from bugbug.vectordb import QdrantVectorDB
 
@@ -14,9 +15,16 @@ from bugbug.vectordb import QdrantVectorDB
 def run(args) -> None:
     llm = generative_model_tool.create_llm(args.llm)
 
+    function_search = (
+        function_search_classes[args.function_search_type]()
+        if args.function_search_type is not None
+        else None
+    )
     vector_db = QdrantVectorDB("diff_comments")
     review_comments_db = code_review.ReviewCommentsDB(vector_db)
-    code_review_tool = code_review.CodeReviewTool(review_comments_db, llm)
+    code_review_tool = code_review.CodeReviewTool(
+        function_search, review_comments_db, llm
+    )
 
     review_data = code_review.review_data_classes[args.review_platform]()
 
@@ -43,6 +51,11 @@ def parse_args(args):
         "--llm",
         help="LLM",
         choices=["human", "openai", "azureopenai", "llama2"],
+    )
+    parser.add_argument(
+        "--function_search_type",
+        help="Function search tool",
+        choices=list(function_search_classes.keys()),
     )
     return parser.parse_args(args)
 
