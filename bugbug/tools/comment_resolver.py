e1eda17637c24246c0425074526abf27db5d34b8
Benjamin Mah <benjaminmah2004@gmail.com>
2024-11-08 11:15:24 -0500

    Added study-modified prompt

--- bugbug/tools/comment_resolver.py
+++ bugbug/tools/comment_resolver.py
@@ -487,6 +487,33 @@ def generate_prompt(
         FIX:
         """
 
+    if prompt_type == "study-modified":
+        prompt = f"""
+        You are a software developer who has to change the code below by following a given Code Review.
+        The Code Review is attached to the line of code starting with the line number Start_Line and
+        ending with the line number End_Line. There are also characters (- and +) showing where a line
+        of code in the diff hunk has been removed (marked with a - at the beginning of the line) or added
+        (marked with a + at the beginning of the line). The New Code Diff should be in the correct Git diff
+        format, where added lines (on top of the diff hunk) are denoted with the + character. Lines removed
+        from the Diff Hunk should be denoted with the - character. Your output must not contain any trailing
+        tokens/characters. Your output must adhere to the following format: "Short Explanation: [...] \n
+        New Code Diff: [...]"
+
+        Start_Line:
+        {start_line}
+
+        End_Line:
+        {end_line}
+
+        Code Review:
+        {comment_content}
+
+        Diff Hunk:
+        ```
+        {relevant_diff}
+        ```
+        """
+
     return prompt
 
 
