529067c1eb4fce812aff5094cb93d15b3cf307c3
Benjamin Mah <benjaminmah2004@gmail.com>
2024-11-29 13:21:21 -0500

    Refactored code

--- scripts/build_failure_data_collection.py
+++ scripts/build_failure_data_collection.py
@@ -2,15 +2,16 @@ import csv
 import logging
 import os
 from collections import defaultdict
+from datetime import datetime
 
 import requests
 import taskcluster
+from dateutil.relativedelta import relativedelta
+from libmozdata.bugzilla import Bugzilla
 from libmozdata.hgmozilla import Revision
 from tqdm import tqdm
 
 from bugbug import bugzilla, db, phabricator, repository
-
-# from libmozdata.hgmozilla import Revision
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -30,8 +31,40 @@ def download_databases():
     assert db.download(phabricator.REVISIONS_DB, support_files_too=True)
 
 
-def preprocess_commits_and_bugs():
-    logger.info("Preprocessing commits and bugs...")
+def get_bz_params():
+    fields = ["id"]
+    one_year_ago = (datetime.now() - relativedelta(years=1)).strftime("%Y-%m-%d")
+    params = {
+        "include_fields": fields,
+        "resolution": "---",
+        "f1": "creation_ts",
+        "o1": "greaterthan",
+        "v1": one_year_ago,
+        "f2": "longdesc",
+        "o2": "allwordssubstr",
+        "v2": "backed out for causing build",
+    }
+    return params
+
+
+def get_backed_out_build_failure_bugs(date="today", bug_ids=[], chunk_size=None):
+    params = get_bz_params()
+    bugs = {}
+
+    def bug_handler(bug, data):
+        data[bug["id"]] = bug
+
+    Bugzilla(
+        params,
+        bughandler=bug_handler,
+        bugdata=bugs,
+    ).get_data().wait()
+
+    return bugs
+
+
+def map_bugs_to_commit(bug_ids):
+    logger.info("Mapping bugs to their commits...")
     bug_commits = {}
 
     for commit in tqdm(
@@ -39,9 +72,12 @@ def preprocess_commits_and_bugs():
             include_no_bug=True, include_backouts=True, include_ignored=True
         )
     ):
+        if commit["bug_id"] not in bug_ids:
+            continue
+
         commit_data = {
             key: commit[key]
-            for key in ["node", "bug_id", "pushdate", "backedoutby", "backsout"]
+            for key in ["node", "bug_id", "pushdate", "backedoutby", "backsout", "desc"]
         }
 
         bug_commits.setdefault(commit["bug_id"], []).append(commit_data)
@@ -49,50 +85,59 @@ def preprocess_commits_and_bugs():
     return bug_commits
 
 
-def preprocess_revisions():
-    logger.info("Preprocessing revisions...")
-    diff_id_to_phid = {}
+def find_bugs(hg_client, bug_ids, bug_commits):
+    logger.info("Finding bugs...")
+    backed_out_revisions = []
 
-    for revision in phabricator.get_revisions():
-        diff_id_to_phid[revision["id"]] = revision["phid"]
+    for bug_id in bug_ids:
+        bug_id_commits = bug_commits.get(bug_id, None)
+        backing_out_commit = find_backing_out_commit(bug_id_commits, hg_client)
 
-    return diff_id_to_phid
+        if not backing_out_commit:
+            continue
+
+        logger.info("Backing out commit found!")
+
+        commit = {}
+
+        commit["desc"] = next(
+            (
+                c["desc"]
+                for c in bug_id_commits
+                if any(
+                    c["node"].startswith(node)
+                    for node in backing_out_commit["backsout"]
+                )
+            ),
+            None,
+        )
+        if commit["desc"] is None:
+            continue
+
+        revision_id = repository.get_revision_id(commit)
+
+        backed_out_revisions.append(revision_id)
+
+    return backed_out_revisions
 
 
-def find_bugs(bug_commits, hg_client):
-    backed_out_bugs = []
+def find_backing_out_commit(commits, hg_client):
+    logger.info("Finding backing out commit...")
+    if not commits:
+        return None
 
-    for bug in bugzilla.get_bugs(include_invalid=True):
-        if caused_build_failure(bug["comments"]):
-            backing_out_commit = find_backing_out_commit(
-                bug_commits.get(bug["id"], None), hg_client
-            )
-            if not backing_out_commit:
-                continue
+    for commit in commits:
+        if not commit["backsout"]:
+            continue
 
-            commit = {}
-
-            commit["desc"] = hg_client.get_revision(
-                "nightly", backing_out_commit["backsout"]
-            )["desc"]
-
-            revision_id = repository.get_revision_id(commit)
-
-            backed_out_bugs.append((bug, backing_out_commit, revision_id))
-
-    return backed_out_bugs
-
-
-def caused_build_failure(comments):
-    for comment in comments:
+        desc = commit["desc"]
         if (
-            "backed out" in comment["text"]
-            # and "for causing" in comment["text"]
-            and "build" in comment["text"]
-            and "bustages" in comment["text"]
+            "backed out" in desc.lower()
+            and "for causing" in desc.lower()
+            and "build" in desc.lower()
         ):
-            return True
-    return False
+            return commit
+    return None
 
 
 def find_error_lines(index_client, queue_client, commit_node):
@@ -149,61 +194,31 @@ def find_error_lines(index_client, queue_client, commit_node):
     return error_lines
 
 
-def find_backing_out_commit(commits, hg_client):
-    if not commits:
-        return None
-
-    for commit in commits:
-        if not commit["backsout"]:
-            continue
-
-        desc = hg_client.get_revision("nightly", commit["node"])["desc"]
-        if "backed out" in desc.lower() and "build" in desc.lower():
-            return commit
-    return None
-
-
 def main():
+    # 0.
     download_databases()
 
-    bug_commits = preprocess_commits_and_bugs()
+    # 1.
+    bugs = get_backed_out_build_failure_bugs()
+    bug_ids = list(bugs.keys())
 
+    # 2.
+    bug_commits = map_bugs_to_commit(bug_ids)
+
+    # 3.
     hg_client = Revision()
+    backed_out_revisions = find_bugs(hg_client, bug_ids, bug_commits)
 
-    bugs = find_bugs(bug_commits, hg_client)
+    # 4.
+    revisions_to_commits = defaultdict(list)
 
-    backout_revisions = []
-    for bug in bugs:
-        backout_revisions.append(bug[2])
+    for commit in repository.get_commits():
+        revision_id = repository.get_revision_id(commit)
 
-    # backout_revisions = [
-    #     27904,
-    #     30744,
-    #     128537,
-    #     127218,
-    #     153067,
-    #     157855,
-    #     161229,
-    #     164203,
-    #     173115,
-    #     174921,
-    #     174086,
-    #     175742,
-    #     20409,
-    #     58102,
-    #     91663,
-    #     205936,
-    #     178686,
-    #     208953,
-    #     211415,
-    #     211106,
-    #     89590,
-    #     214412,
-    #     216163,
-    #     26390,
-    #     219250,
-    #     215371,
-    # ]
+        if revision_id in backed_out_revisions:
+            revisions_to_commits[revision_id].append(commit["node"])
+
+    # 5. and 6.
 
     client_id = os.getenv("TC_CLIENT_ID")
 
@@ -220,23 +235,6 @@ def main():
         }
     )
 
-    # backout_revisions = [
-    #     153067,
-    #     157855,
-    #     164203,
-    #     178686,
-    #     208953,
-    #     216163,
-    #     215371,
-    # ]
-    revisions_to_commits = defaultdict(list)
-
-    for commit in repository.get_commits():
-        revision_id = repository.get_revision_id(commit)
-
-        if revision_id in backout_revisions:
-            revisions_to_commits[revision_id].append(commit["node"])
-
     with open("revisions.csv", mode="w", newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
 
@@ -245,6 +243,9 @@ def main():
         )
 
         for revision_id, commits in revisions_to_commits.items():
+            if len(commits) < 2:
+                continue
+
             commit_diff = repository.get_diff(
                 repo_path="hg_dir", original_hash=commits[0], fix_hash=commits[1]
             )
@@ -263,116 +264,10 @@ def main():
 if __name__ == "__main__":
     main()
 
-    # # ==== experimental purposes ====
-
-    # index = taskcluster.Index(
-    #     {
-    #         "rootUrl": "https://firefox-ci-tc.services.mozilla.com",
-    #         "credentials": {"clientId": "mozilla-auth0/ad|Mozilla-LDAP|bmah"},
-    #     }
-    # )
-
-    # queue = taskcluster.Queue(
-    #     {
-    #         "rootUrl": "https://firefox-ci-tc.services.mozilla.com",
-    #     }
-    # )
-
-    # print(find_error_lines(index, queue, "448597bce69d9e173e0b6818a513b8dfd86f1765"))
-
-    # tc_test = True
-
-    # if tc_test:
-
-    #     index = taskcluster.Index(
-    #         {
-    #             "rootUrl": "https://firefox-ci-tc.services.mozilla.com",
-    #             "credentials": {"clientId": "mozilla-auth0/ad|Mozilla-LDAP|bmah"},
-    #         }
-    #     )
-
-    #     queue = taskcluster.Queue(
-    #         {
-    #             "rootUrl": "https://firefox-ci-tc.services.mozilla.com",
-    #         }
-    #     )
-
-    #     # FINAL STEPS
-    #     # 1. list the tasks
-    #     # tasks = index.listTasks('gecko.v2.autoland.revision.04d0c38e624dc5fe830b67c0526aafa87d3a63ed.firefox')
-    #     commit_node = "448597bce69d9e173e0b6818a513b8dfd86f1765"
-    #     commit_node = "04d0c38e624dc5fe830b67c0526aafa87d3a63ed"
-    #     commit_node = "759d4948ed8b468dfc03d2ca35e7c8e54b62ae75"
-    #     tasks = index.listTasks(f"gecko.v2.autoland.revision.{commit_node}.firefox")
-    #     print(tasks)
-
-    #     # 2. get the task ID from one of the tasks (I think any is fine)
-    #     first_task_id = tasks["tasks"][0]["taskId"]
-    #     print(first_task_id)
-
-    #     # 3. get the task group ID from the task ID
-    #     first_task = queue.task(first_task_id)
-    #     task_group_id = first_task["taskGroupId"]
-    #     print(task_group_id)
-
-    #     # 4. extract the build task IDs from the task group ID
-    #     # "https://firefoxci.taskcluster-artifacts.net/YHg9SxOFQiKgWd4Cr9TuRw/0/public/label-to-taskid.json"
-    #     url = f"https://firefoxci.taskcluster-artifacts.net/{task_group_id}/0/public/label-to-taskid.json"
-    #     response = requests.get(url)
-    #     response.raise_for_status()
-    #     data = response.json()
-
-    #     build_tasks = set()
-
-    #     for label, taskId in data.items():
-    #         if label[:5] == "build":
-    #             build_tasks.add(taskId)
-
-    #     # 5 get failed tasks
-    #     failed_tasks = set()
-
-    #     for task in queue.listTaskGroup(task_group_id)["tasks"]:
-    #         if task["status"]["state"] == "failed":
-    #             failed_tasks.add(task["status"]["taskId"])
-
-    #     # 6. find intersection between build tasks and failed tasks
-    #     failed_build_tasks = list(build_tasks & failed_tasks)
-    #     print(failed_build_tasks)
-
-    #     # 7. get the url to access the log, load it, and extract the ERROR lines
-    #     for failed_build_task in failed_build_tasks:
-    #         artifact = queue.getArtifact(
-    #             taskId=failed_build_task, runId="0", name="public/logs/live.log"
-    #         )
-    #         print(artifact)
-    #         url = artifact["url"]
-    #         break
-
-    #     response = requests.get(url)
-    #     error_lines = [line for line in response.text.split("\n") if "ERROR - " in line]
-
-    #     for error_line in error_lines:
-    #         print(error_line)
-
-    # print(queue.listTaskGroup('04d0c38e624dc5fe830b67c0526aafa87d3a63ed'))
-    # print(queue.listTaskGroup('eNsrB5djSb6UZipZi3BPAQ'))
-
-# collect bugs with build failures, along with a list of their revisions X
-# identify commit that backs out another commit due to build failure X
-# from above, we can also get the node of the commit that caused the backout X
-# extract the revision ID from initial commit description X
-
-# ---- Find another commit with the same revision mentioned in the commit description
-
-# use this to associate the commit nodes to the diff IDS --> find the commit before and after the backout -- check diff description?
-# or alternatively, check when the reverting change was made (specifically for the build failure backout) --> get the diff before and after this timestamp with a desc with a commit to MOZILLACENTRAL
-
-# once we have the initial and fix patch IDs, we can get the interdiff between them
-# we can also get the error message from the initial patch, to find the exact lines
-# take a look here https://matrix.to/#/!whDRjjSmICCgrhFHsQ:mozilla.org/$H93f5S5LisVMCEeM2-oB97mHXz6usNAJjWAMUSqQEQc?via=mozilla.org&via=matrix.org&via=braak.pro
-
-
-# find the commit that happened most recently after the backout --> this is a fix commit
-# convert commit id to commit phid (this is a thing in the revision object in phabricator.get_revisions)
-# associate commit phid with its revision in phab --> and then associate it with its patch id
-# get interdiff between the initial patch and the fix patch
+# 0. Download databases
+# 1. Identify bugs in Bugzilla that have a backout due to build failures X
+# 2. Map only these bugs' commits to the bug ID in a dict
+# 3. Find the revision from the bug
+# 4. Map the revision to the commits
+# 5. Get the interdiff
+# 6. Find error lines in the interdiff
