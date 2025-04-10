c79890e7345252a8ffcafa3928418f9a53de5b09
Benjamin Mah <benjaminmah2004@gmail.com>
2025-04-10 17:41:39 -0400

    Addressed PR comments

--- functions/release_notes/main.py
+++ functions/release_notes/main.py
@@ -11,6 +11,15 @@ from bugbug.utils import get_secret
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+os.environ["OPENAI_API_KEY"] = get_secret("OPENAI_API_KEY")
+
+llm_cache = {
+    "llm_name": None,
+    "llm": None,
+    "chunk_size": None,
+    "selector": None,
+}
+
 
 @functions_framework.http
 def handle_release_notes(request: flask.Request):
@@ -21,14 +30,25 @@ def handle_release_notes(request: flask.Request):
     if not version:
         return "Missing 'version' query parameter", 400
 
-    os.environ["OPENAI_API_KEY"] = get_secret("OPENAI_API_KEY")
-
     llm_name = request.args.get("llm", "openai")
     chunk_size = int(request.args.get("chunk_size", 100))
-    version = request.args.get("version")
 
-    llm = generative_model_tool.create_llm_from_request(llm_name, request.args)
-    selector = ReleaseNotesCommitsSelector(chunk_size=chunk_size, llm=llm)
+    if (
+        llm_cache["llm"] is None
+        or llm_cache["llm_name"] != llm_name
+        or llm_cache["chunk_size"] != chunk_size
+    ):
+        logger.info("Initializing new LLM and selector...")
+        llm_cache["llm_name"] = llm_name
+        llm_cache["llm"] = generative_model_tool.create_llm_from_request(
+            llm_name, request.args
+        )
+        llm_cache["chunk_size"] = chunk_size
+        llm_cache["selector"] = ReleaseNotesCommitsSelector(
+            chunk_size=chunk_size, llm=llm_cache["llm"]
+        )
+
+    selector = llm_cache["selector"]
     notes = selector.get_final_release_notes_commits(version=version)
 
     if not notes:
