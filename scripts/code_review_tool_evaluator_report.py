6bea293d205823cb4c371ea5b18d792c9fabae84
Suhaib Mujahid <suhaibmujahid@gmail.com>
2025-04-14 16:39:39 -0400

    [review_tool_evaluator_report] Show examples

--- scripts/code_review_tool_evaluator_report.py
+++ scripts/code_review_tool_evaluator_report.py
@@ -5,6 +5,7 @@ import pandas as pd
 from scripts.code_review_tool_evaluator import get_latest_evaluation_results_file
 
 evaluation_results = pd.read_csv(get_latest_evaluation_results_file())
+# evaluation_results = pd.read_csv("/Users/smujahid/repos/mozilla/bugbug/evaluation_results_2025-03-05_08-59-13.csv")
 
 # %%
 
@@ -37,7 +38,9 @@ matched_invalid_comments = df[
     & (df["evaluation"] == "INVALID")
 ]["old_comments_count"].sum()
 
-
+print("--------------------")
+print("Variant Name:", variant_name)
+print("--------------------")
 print("New Comments:", new_comments_count)
 print("New Valid Comments:", new_valid_comments)
 print("New Invalid Comments:", new_invalid_comments)
@@ -65,5 +68,40 @@ print(
     (old_invalid_comments - matched_invalid_comments) / old_invalid_comments * 100,
 )
 
+
+# %%
+
+
+df = evaluation_results[
+    evaluation_results["evaluation"].isin(["VALID", "INVALID"])
+    & ~evaluation_results["new_comment"].isna()
+].sort_values(by=["evaluation", "revision_id", "new_comment"])
+
+
+df["id"] = df["diff_id"].astype(str) + " | " + df["new_comment"].astype(str)
+df_old = df[df["variant_name"] == variant_names[0]]
+df_new = df[df["variant_name"] == variant_names[1]]
+
+in_new_but_not_in_old = df_new[~df_new["id"].isin(df_old["id"])]
+
+print(
+    "Examples of comments that were filtered by the old version but were not filtered by the new version:\n"
+)
+print(
+    in_new_but_not_in_old[["revision_id", "new_comment", "evaluation"]].to_markdown(
+        index=False
+    )
+)
+
+
+in_old_but_not_in_new = df_old[~df_old["id"].isin(df_new["id"])]
+print(
+    "\n\nExamples of comments that were filtered by the new version but were not filtered by the old version:\n"
+)
+print(
+    in_old_but_not_in_new[["revision_id", "new_comment", "evaluation"]].to_markdown(
+        index=False
+    )
+)
 
 # %%
