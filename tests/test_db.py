mozilla/bugbug

commit c0539ec9d5f733c59541c693df0e8b55c019c1af

A few improvements to the DB implementation (#5392)

bugbug/db.py
@@ -6,6 +6,7 @@
 import gzip
 import io
 import logging
+import mmap
 import os
 import pickle
 from contextlib import contextmanager
@@ -137,8 +138,9 @@ def last_modified(path):
 
 
 class Store:
-    def __init__(self, fh):
+    def __init__(self, fh, use_mmap=True):
         self.fh = fh
+        self.use_mmap = use_mmap
 
 
 class JSONStore(Store):
@@ -147,8 +149,23 @@ class JSONStore(Store):
             self.fh.write(orjson.dumps(elem) + b"\n")
 
     def read(self):
-        for line in io.TextIOWrapper(self.fh, encoding="utf-8"):
-            yield orjson.loads(line)
+        if self.use_mmap:
+            with mmap.mmap(self.fh.fileno(), 0, prot=mmap.PROT_READ) as buf:
+                while line := buf.readline():
+                    yield orjson.loads(line)
+        else:
+            for line in io.TextIOWrapper(self.fh, encoding="utf-8"):
+                yield orjson.loads(line)
+
+    def size(self):
+        if self.use_mmap:
+            with mmap.mmap(self.fh.fileno(), 0, prot=mmap.PROT_READ) as buf:
+                count = 0
+                while buf.readline():
+                    count += 1
+                return count
+        else:
+            return sum(1 for _ in io.TextIOWrapper(self.fh, encoding="utf-8"))
 
 
 class PickleStore(Store):
@@ -162,6 +179,9 @@ class PickleStore(Store):
                 yield pickle.load(self.fh)
         except EOFError:
             pass
+
+    def size(self):
+        return sum(1 for _ in self.read())
 
 
 COMPRESSION_FORMATS = ["gz", "zstd"]
@@ -186,21 +206,21 @@ def _db_open(path, mode):
 
     if compression == "gz":
         with gzip.GzipFile(path, mode) as f:
-            yield store_constructor(f)
+            yield store_constructor(f, use_mmap=False)
     elif compression == "zstd":
         if "w" in mode or "a" in mode:
             cctx = zstandard.ZstdCompressor()
             with open(path, mode) as f:
                 with cctx.stream_writer(f) as writer:
-                    yield store_constructor(writer)
+                    yield store_constructor(writer, use_mmap=False)
         else:
             dctx = zstandard.ZstdDecompressor()
             with open(path, mode) as f:
                 with dctx.stream_reader(f) as reader:
-                    yield store_constructor(reader)
+                    yield store_constructor(reader, use_mmap=False)
     else:
         with open(path, mode) as f:
-            yield store_constructor(f)
+            yield store_constructor(f, use_mmap=True)
 
 
 def read(path):
@@ -210,8 +230,17 @@ def read(path):
         return ()
 
     with _db_open(path, "rb") as store:
-        for elem in store.read():
-            yield elem
+        yield from store.read()
+
+
+def size(path):
+    assert path in DATABASES
+
+    if not os.path.exists(path):
+        return ()
+
+    with _db_open(path, "rb") as store:
+        return store.size()
 
 
 def write(path, elems):

tests/test_db.py
@@ -31,12 +31,13 @@ def mock_db(tmp_path):
 
 @pytest.mark.parametrize("db_format", ["json", "pickle"])
 @pytest.mark.parametrize("db_compression", [None, "gz", "zstd"])
-def test_write_read(mock_db, db_format, db_compression):
+def test_write_read_size(mock_db, db_format, db_compression):
     db_path = mock_db(db_format, db_compression)
 
     db.write(db_path, range(1, 8))
 
     assert list(db.read(db_path)) == [1, 2, 3, 4, 5, 6, 7]
+    assert db.size(db_path) == 7
 
 
 @pytest.mark.parametrize("db_format", ["json", "pickle"])

bugbug/phabricator.py
@@ -52,6 +52,10 @@ def get_revisions() -> Iterator[RevisionDict]:
     yield from db.read(REVISIONS_DB)
 
 
+def count_revisions() -> Iterator[RevisionDict]:
+    return db.size(REVISIONS_DB)
+
+
 def set_api_key(url: str, api_key: str) -> None:
     global PHABRICATOR_API
     PHABRICATOR_API = PhabricatorAPI(api_key, url)

bugbug/tools/core/platforms/phabricator.py
@@ -485,8 +485,9 @@ class PhabricatorReviewData(ReviewData):
     ) -> Iterable[tuple[int, list[InlineComment]]]:
         db.download(phabricator.REVISIONS_DB)
 
-        revision_count = sum(1 for _ in phabricator.get_revisions())
-        for revision in tqdm(phabricator.get_revisions(), total=revision_count):
+        for revision in tqdm(
+            phabricator.get_revisions(), total=phabricator.count_revisions()
+        ):
             diff_comments: dict[int, list[InlineComment]] = defaultdict(list)
 
             for transaction in revision["transactions"]:

commit d5a4f6933403454445465a4b8c12338831c69527

Don't fail with a ValueError due to inability to mmap empty files when a DB file is empty
Fixes a regression from c0539ec9d5f733c59541c693df0e8b55c019c1af

bugbug/db.py
@@ -150,6 +150,9 @@ class JSONStore(Store):
 
     def read(self):
         if self.use_mmap:
+            if os.stat(self.fh.fileno()).st_size == 0:
+                return
+
             with mmap.mmap(self.fh.fileno(), 0, prot=mmap.PROT_READ) as buf:
                 while line := buf.readline():
                     yield orjson.loads(line)
@@ -159,6 +162,9 @@ class JSONStore(Store):
 
     def size(self):
         if self.use_mmap:
+            if os.stat(self.fh.fileno()).st_size == 0:
+                return 0
+
             with mmap.mmap(self.fh.fileno(), 0, prot=mmap.PROT_READ) as buf:
                 count = 0
                 while buf.readline():

tests/test_db.py
@@ -48,6 +48,11 @@ def test_read_empty(mock_db, db_format, db_compression):
     assert list(db.read(db_path)) == []
     assert db.size(db_path) == 0
 
+    db.write(db_path, [])
+
+    assert list(db.read(db_path)) == []
+    assert db.size(db_path) == 0
+
 
 @pytest.mark.parametrize("db_format", ["json", "pickle"])
 @pytest.mark.parametrize("db_compression", [None, "gz", "zstd"])