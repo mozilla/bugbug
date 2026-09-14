3b4dbd07f396c546f3b24b7e3c33d6c06c0bc125
Suhaib Mujahid <smujahid@mozilla.com>
2026-09-14 09:00:25 -0400

    Remove Vercel cron monitor instrumentation

--- services/hackbot-ui/next.config.mjs
+++ services/hackbot-ui/next.config.mjs
@@ -30,12 +30,6 @@ export default withSentryConfig(nextConfig, {
   tunnelRoute: "/monitoring",
 
   webpack: {
-    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: true,
-
     // Tree-shaking options for reducing bundle size
     treeshake: {
       // Automatically tree-shake Sentry logger statements to reduce bundle size
